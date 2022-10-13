e1a9f143b09afd0b1feb47a882b81c09316f800c
Phil Cockfield <phil@cockfield.net>
2022-10-14 12:41:58 +1300

    Stub in Svelte version of publish spike

---
+++ code/spikes/publish.svelte/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Counter from './Counter.svelte';
+</script>
+
+<main>
+  <div class="main">
+    <h1>Hello World (Svelte)</h1>
+    <div>
+      <Counter />
+    </div>
+  </div>
+</main>
+
+<style>
+  .main {
+    font-family: sans-serif;
+    padding-top: 30px;
+    padding-left: 45px;
+  }
+  h1 {
+    font-size: 60px;
+    color: red;
+  }
+</style>
