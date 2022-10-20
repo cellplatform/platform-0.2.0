3f3e9d42148ffd76da595ea0b684585e9a3a81b6
Phil Cockfield <phil@cockfield.net>
2022-10-21 11:38:20 +1300

    refactor entry

--- code/spikes/publish.001/src/index.tsx
+++ code/spikes/publish.001/src/index.tsx
@@ -1,10 +1,1 @@
-import { createRoot } from 'react-dom/client';
-import { Root } from './ui.react/Root/index.mjs';
-
-/**
- * User Interface
- */
-
-const el = <Root />;
-const root = createRoot(document.getElementById('root')!);
-root.render(el);
+export {};
