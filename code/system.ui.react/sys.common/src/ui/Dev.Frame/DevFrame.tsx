a7cfea883071c9331de3ba7cb9251ac838a7c4ff
Phil Cockfield <phil@cockfield.net>
2022-11-09 08:13:41 +1300

    refactor

--- code/system.ui.react/sys.common/src/ui/Dev.Frame/DevFrame.tsx
+++ code/system.ui.react/sys.common/src/ui/Dev.Frame/DevFrame.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Color, COLORS, css, t, rx, FC } from '../common';
+import { Color, COLORS, css, t, rx, FC } from '../common.mjs';
 
 export type DevFrameProps = {
   style?: t.CssValue;
@@ -10,9 +10,7 @@ export const DevFrame: React.FC<DevFrameProps> = (props) => {
    * [Render]
    */
   const styles = {
-    base: css({
-      backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
-    }),
+    base: css({ backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */ }),
   };
 
   return (
