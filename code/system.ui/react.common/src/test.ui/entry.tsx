0f97d41ae53a71854b7a39d58a72e432792827e0
Phil Cockfield <phil@cockfield.net>
2022-11-12 15:05:48 +1300

    Dev - Index Listing via Query Router
    (refactor into module: "sys.ui.react.dev")

--- code/system.ui/react.common/src/test.ui/entry.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { Dev } from '.';
-
-const spec = import('../ui.Icon/dev/Icon.SPEC');
-
-const el = <Dev.Harness spec={spec} style={{ Absolute: 0 }} />;
-const root = createRoot(document.getElementById('root')!);
-root.render(el);
