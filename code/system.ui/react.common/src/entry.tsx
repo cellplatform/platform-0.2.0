489f29fa3038c4077de55fa2c3bbc565a09d2506
Phil Cockfield <phil@cockfield.net>
2022-11-14 17:35:48 +1300

    Harden up dev entry render helper

--- code/system.ui/react.common/src/entry.tsx
+++ code/system.ui/react.common/src/entry.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Dev } from './test.ui';
-import { SpecList } from './test.ui/SpecList';
 import { Pkg } from './index.pkg.mjs';
-
-const url = new URL(window.location.href);
-const params = url.searchParams;
 
 const Imports = {
   ['ui.Icon']: () => import('./ui.Icon/dev/Icon.SPEC'),
@@ -12,48 +8,8 @@ const Imports = {
   ['ui.ZoomAndPan']: () => import('./ui.ZoomAndPan/ZoomAndPan.SPEC'),
 };
 
-/**
- * MAIN INIT
- *   Dev.Harness => (Load Router)
- */
 (async () => {
-  let _module: any;
-
-  /**
-   * Interpret URL parameters
-   */
-  const KEY = { DEV: 'dev' };
-  if (params.has(KEY.DEV)) {
-    /**
-     * QUERY: ?dev=<namespace>
-     */
-    const dev = params.get(KEY.DEV) ?? '';
-
-    if (dev && Object.keys(Imports).includes(dev)) {
-      const matches = Object.keys(Imports)
-        .filter((namespace) => namespace === dev)
-        .map((namespace) => ({ namespace, fn: (Imports as any)[namespace] }));
-
-      if (matches[0]) {
-        const res = await matches[0].fn();
-        if (typeof res === 'object') _module = res.default;
-      }
-    }
-  }
-
-  /**
-   * Render UI.
-   */
+  const el = await Dev.render(Pkg, Imports);
   const root = createRoot(document.getElementById('root')!);
-
-  if (_module) {
-    const el = <Dev.Harness spec={_module} style={{ Absolute: 0 }} />;
-    return root.render(el);
-  }
-
-  /**
-   * Default Index List
-   */
-  const el = <SpecList title={Pkg.name} imports={Imports} style={{ Absolute: 0 }} />;
-  return root.render(el);
+  root.render(el);
 })();
--- code/system.ui/react.dev/src/ui.Entry/Entry.tsx
+++ code/system.ui/react.dev/src/ui.Entry/Entry.tsx
@@ -4,21 +4,22 @@ import { Harness } from '../ui.Harness';
 
 export const Entry = {
   /**
-   * Render the default component
+   * Render a harness with the selected `dev=namespace` import or an
+   * index list of available specs.
    */
   async render(
     pkg: { name: string; version: string },
-    imports: t.Imports,
+    specs: t.Imports,
     options: { location?: string | URL | Location; style?: t.CssValue } = {},
   ) {
     const url = Wrangle.location(options.location);
-    const spec = await Wrangle.module(url, imports);
+    const spec = await Wrangle.module(url, specs);
     const style = options.style ?? { Absolute: 0 };
 
     if (spec) {
       return <Harness spec={spec} style={style} />;
     } else {
-      return <SpecList title={pkg.name} imports={imports} style={style} />;
+      return <SpecList title={pkg.name} imports={specs} style={style} />;
     }
   },
 };
@@ -28,25 +29,38 @@ export const Entry = {
  */
 
 const Wrangle = {
+  /**
+   * Convert an input location into a standard [URL] object.
+   */
   location(value?: string | URL | Location): URL {
     if (!value) return new URL(window.location.href);
     return typeof value === 'string' ? new URL(value) : new URL(value.href);
   },
 
-  async module(url: URL, imports: t.Imports) {
+  /**
+   * Derive and load the module from the given URL.
+   */
+  async module(url: URL, specs: t.Imports) {
     const KEY = { DEV: 'dev' };
     const params = url.searchParams;
     if (!params.has(KEY.DEV)) return undefined;
 
     const dev = params.get(KEY.DEV) ?? '';
-    if (dev && Object.keys(imports).includes(dev)) {
-      const matches = Object.keys(imports)
+    if (dev && Object.keys(specs).includes(dev)) {
+      const matches = Object.keys(specs)
         .filter((namespace) => namespace === dev)
-        .map((namespace) => ({ namespace, fn: (imports as any)[namespace] }));
+        .map((namespace) => ({ namespace, fn: (specs as any)[namespace] }))
+        .filter((item) => typeof item.fn === 'function');
 
       if (matches[0]) {
         const res = await matches[0].fn();
-        if (typeof res === 'object') return res.default;
+        if (typeof res === 'object') {
+          if (res.default.kind === 'TestSuite') {
+            return res.default;
+          } else {
+            console.warn(`Imported default is not of kind "TestSuite"`);
+          }
+        }
       }
     }
 
