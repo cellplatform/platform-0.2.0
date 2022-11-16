e8c8155bf3c81ff7df23e1a2f64b413f5dce8a94
Phil Cockfield <phil@cockfield.net>
2022-11-16 13:02:13 +1300

    Update entry.tsx

--- code/spikes/publish.001/src/ui.react/Root/entry.tsx
+++ code/spikes/publish.001/src/ui.react/Root/entry.tsx
@@ -10,7 +10,7 @@ const query = url.searchParams;
 const Imports = {
   'spike.ui.Root.dev': () => import('./dev/Root.DevEnv.SPEC'),
   'spike.ui.Root': () => import('./dev/Root.SPEC'),
-  'spike.ui.Outline': () => import('../Tile.Outline/Tile.Outline.SPEC'),
+  'spike.ui.TileOutline': () => import('../Tile.Outline/TileOutline.SPEC'),
   'spike.ui.VideoDiagram': () => import('../VideoDiagram/VideoDiagram.SPEC'),
 };
 
@@ -20,13 +20,11 @@ const Imports = {
 
 (async () => {
   const root = createRoot(document.getElementById('root')!);
-
   if (query.has('dev')) {
     const { Dev } = await import('../../test.ui');
     const el = await Dev.render(Pkg, Imports);
     root.render(el);
   } else {
-    const el = <Root />;
-    root.render(el);
+    root.render(<Root />);
   }
 })();
