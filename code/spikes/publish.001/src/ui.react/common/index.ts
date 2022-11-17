1854df26e480f97267071b628bbedc12569eb84b
Phil Cockfield <phil@cockfield.net>
2022-11-17 16:19:29 +1300

    reposition [KeyListener] ready for export into System level library

--- code/spikes/publish.001/src/ui.react/common/index.ts
+++ code/spikes/publish.001/src/ui.react/common/index.ts
@@ -2,4 +2,5 @@ import type * as t from './types.mjs';
 
 export { t };
 export * from './libs.mjs';
-export * from './KeyListener.mjs';
+
+export { KeyListener } from '../../ui.react.libs/index.mjs';
