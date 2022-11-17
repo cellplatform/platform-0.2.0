a1a54160f02cbf3cc78e3ecefd4307fcc395c60c
Phil Cockfield <phil@cockfield.net>
2022-11-18 10:25:13 +1300

    Stub in <ProgressBar> to spec

--- code/spikes/publish.001/src/ui.react/Video.ProgressBar/ProgressBar.SPEC.tsx
+++ code/spikes/publish.001/src/ui.react/Video.ProgressBar/ProgressBar.SPEC.tsx
@@ -1,9 +1,10 @@
 import { Spec, expect } from '../../test.ui';
+import { ProgressBar } from './ProgressBar';
 
 export default Spec.describe('Video.ProbressBar', (e) => {
   e.it('init', async (e) => {
     const ctx = Spec.ctx(e);
-    const el = <div>{`Hello 🐷`}</div>;
-    ctx.backgroundColor(1).render(el);
+    const el = <ProgressBar />;
+    ctx.backgroundColor(1).size('fill-x').render(el);
   });
 });
