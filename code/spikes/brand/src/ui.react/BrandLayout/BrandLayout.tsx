9048ce485cdf4cbfce24fdb135824074dee136db
Phil Cockfield <phil@cockfield.net>
2022-12-06 09:29:37 +1300

    brand layout (spike)

--- code/spikes/brand/src/ui.react/BrandLayout/BrandLayout.tsx
+++ code/spikes/brand/src/ui.react/BrandLayout/BrandLayout.tsx
@@ -3,8 +3,10 @@ import { Color, COLORS, css, t, rx } from '../common';
 import { Image } from '../Image';
 
 const URL = {
-  SKETCH_TABLE:
+  SKETCH_1:
     'https://user-images.githubusercontent.com/185555/205549096-48cd9707-bca1-4561-aa68-8df60e61f37f.jpg',
+  SKETCH_2:
+    'https://user-images.githubusercontent.com/185555/205729992-44c68a14-db0f-4c5b-b511-6b6774030166.jpg',
 };
 
 export type BrandLayoutProps = {
@@ -12,16 +14,38 @@ export type BrandLayoutProps = {
 };
 
 export const BrandLayout: React.FC<BrandLayoutProps> = (props) => {
+  const GAP = 60;
   /**
    * [Render]
    */
   const styles = {
-    base: css({ Absolute: 0 }),
-    sketch: css({ Absolute: 0 }),
+    base: css({
+      Absolute: 0,
+      backgroundColor: COLORS.WHITE,
+    }),
+    body: css({
+      Absolute: GAP,
+      display: 'grid',
+      gridTemplateColumns: `repeat(2, 1fr)`,
+      columnGap: GAP,
+    }),
+    outer: css({
+      position: 'relative',
+      border: `solid 1px ${Color.alpha(COLORS.DARK, 0.8)}`,
+    }),
+    image: css({ Absolute: 0 }),
   };
+
   return (
     <div {...css(styles.base, props.style)}>
-      <Image.Cover url={URL.SKETCH_TABLE} style={styles.sketch} />
+      <div {...styles.body}>
+        <div {...styles.outer}>
+          <Image.Cover url={URL.SKETCH_1} style={styles.image} />
+        </div>
+        <div {...styles.outer}>
+          <Image.Cover url={URL.SKETCH_2} style={styles.image} />
+        </div>
+      </div>
     </div>
   );
 };
