2a330e7adbc102b879e966855b86432d94f18a6c
Phil Cockfield <phil@cockfield.net>
2022-12-08 10:35:40 +1300

    folder refactor

--- code/spikes/publish.001/src/ui/common/index.ts
+++ code/spikes/publish.001/src/ui/common/index.ts
@@ -3,4 +3,4 @@ import type * as t from './types.mjs';
 export { t };
 export * from './libs.mjs';
 
-export { KeyListener } from '../../ui.react.libs/index.mjs';
+export { KeyListener } from '../../ui.libs/index.mjs';
