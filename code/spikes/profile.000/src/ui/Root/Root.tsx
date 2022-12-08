fbbb68eb11505413b218e8acb5679af684c5807b
Phil Cockfield <phil@cockfield.net>
2022-12-08 13:48:09 +1300

    spike - spin out profile

--- code/spikes/profile.000/src/ui/Root/Root.tsx
+++ code/spikes/profile.000/src/ui/Root/Root.tsx
@@ -1,7 +1,11 @@
-import { COLORS, css, t } from '../common';
+import { Color, COLORS, css, t } from '../common';
+import { Details } from './Details';
 
 const URL = {
-  FACE: 'https://user-images.githubusercontent.com/185555/206095850-8b561843-50f3-4549-a5e3-dcfc6bae474d.png',
+  FACE_1:
+    'https://user-images.githubusercontent.com/185555/206095850-8b561843-50f3-4549-a5e3-dcfc6bae474d.png',
+  FACE_2:
+    'https://user-images.githubusercontent.com/185555/206325854-f418b496-cb14-4ff2-8f66-1c91d40ecb7a.png',
 };
 
 export type RootProps = {
@@ -17,19 +21,37 @@ export const Root: React.FC<RootProps> = (props) => {
       Absolute: 0,
       backgroundColor: COLORS.WHITE,
       color: COLORS.DARK,
+      fontFamily: 'sans-serif',
+      overflow: 'hidden',
+    }),
+    body: css({
+      Absolute: 0,
       display: 'grid',
       justifyContent: 'center',
       alignContent: 'center',
-      overflow: 'hidden',
+      backgroundColor: Color.alpha(COLORS.DARK, 0.02),
+    }),
+    columns: css({
+      display: 'grid',
+      // gridTemplateColumns: `repeat(2, 1fr)`,
+      // columnGap: 20,
     }),
     logo: css({
       width: 360,
       borderRadius: 20,
     }),
+    details: css({
+      marginTop: 20,
+    }),
   };
   return (
     <div {...css(styles.base, props.style)}>
-      <img src={URL.FACE} {...styles.logo} />
+      <div {...styles.body}>
+        <div {...styles.columns}>
+          <img src={URL.FACE_1} {...styles.logo} />
+          <Details style={styles.details} />
+        </div>
+      </div>
     </div>
   );
 };
