3d5a2e950d86e2b5b61570fe25ebdff8ed7669fd
Phil Cockfield <phil@cockfield.net>
2022-12-14 12:17:15 +1300

    withEvents

--- code/system.ui/react.dev/src/ui.Bus/index.ts
+++ code/system.ui/react.dev/src/ui.Bus/index.ts
@@ -1,11 +1,19 @@
 import { BusEvents as Events } from './Bus.Events.mjs';
 import { BusController as Controller } from './Bus.Controller.mjs';
 import { DEFAULT } from './DEFAULT.mjs';
+import { t, Is } from './common';
 
 export * from './Bus.Events.mjs';
 
 export const DevBus = {
-  Events,
-  Controller,
   DEFAULT,
+  Controller,
+  Events,
+
+  async withEvents(instance: t.DevInstance, handler: (events: t.DevEvents) => any) {
+    const events = DevBus.Events({ instance });
+    const res = handler(events);
+    if (Is.promise(res)) await res;
+    events.dispose();
+  },
 };
