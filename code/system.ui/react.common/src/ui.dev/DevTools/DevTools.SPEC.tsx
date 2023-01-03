3d680c69d9f545d5a2bab7b64c8740c4c11489fa
Phil Cockfield <phil@cockfield.net>
2023-01-03 16:00:40 +1300

    stub in Dev Button

--- code/system.ui/react.common/src/ui.dev/DevTools/DevTools.SPEC.tsx
+++ code/system.ui/react.common/src/ui.dev/DevTools/DevTools.SPEC.tsx
@@ -1,8 +1,12 @@
 import { Spec } from '../../test.ui';
+import { Button } from '../DevTools.Button';
 
 export default Spec.describe('ui.dev.DevTools', (e) => {
   e.it('init', async (e) => {
     const ctx = Spec.ctx(e);
-    ctx.component.render(() => <div>DevTools</div>);
+    ctx.component
+      .display('grid')
+      .size(250, undefined)
+      .render(() => <Button ctx={ctx} label={'MyButton'} />);
   });
 });
