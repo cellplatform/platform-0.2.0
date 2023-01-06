95fa7739942fa410665fa6135d718f7187d3f3ee
Phil Cockfield <phil@cockfield.net>
2023-01-06 17:37:07 +1300

    Empty - in default state

--- code/system.ui/react.dev/src/test.ui/sample.specs/Empty.SPEC.tsx
+++ code/system.ui/react.dev/src/test.ui/sample.specs/Empty.SPEC.tsx
@@ -3,8 +3,7 @@ import { Spec } from '../common';
 export default Spec.describe('Empty', (e) => {
   e.it('init', async (e) => {
     const ctx = Spec.ctx(e);
-
     // ctx.host.backgroundColor(-0.3);
-    ctx.component.backgroundColor(-0.9);
+    // ctx.component.backgroundColor(-0.9);
   });
 });
