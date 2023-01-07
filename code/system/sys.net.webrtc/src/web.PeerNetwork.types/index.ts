52a3ea4f1688de661ad4a8312e622563a23800d3
Phil Cockfield <phil@cockfield.net>
2023-01-08 12:04:01 +1300

    WIP

--- code/system/sys.net.webrtc/src/web.PeerNetwork.types/index.ts
+++ code/system/sys.net.webrtc/src/web.PeerNetwork.types/index.ts
@@ -1,18 +1,18 @@
-export * from './type.peer';
-export * from './type.peer.connection';
+export * from './type.peer.mjs';
+export * from './type.peer.connection.mjs';
 
-export * from './type.event.peer';
-export * from './type.event.group';
-export * from './type.event.peer.local';
-export * from './type.event.peer.conn';
-export * from './type.event.peer.data';
-export * from './type.event.peer.remote';
+export * from './type.event.peer.mjs';
+export * from './type.event.group.mjs';
+export * from './type.event.peer.local.mjs';
+export * from './type.event.peer.conn.mjs';
+export * from './type.event.peer.data.mjs';
+export * from './type.event.peer.remote.mjs';
 
-export * from './type.events.api.peer';
-export * from './type.events.api.group';
+export * from './type.events.api.peer.mjs';
+export * from './type.events.api.group.mjs';
 
-export * from './type.strategy';
-export * from './type.group';
+export * from './type.strategy.mjs';
+export * from './type.group.mjs';
 
-export * from './type.uri';
-export * from './type.network';
+export * from './type.uri.mjs';
+export * from './type.network.mjs';
