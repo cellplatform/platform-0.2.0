98a14ae4e342f208e994eb43e4b8bf607b00de9d
Phil Cockfield <phil@cockfield.net>
2023-01-13 11:58:16 +1300

    PropList SPEC - WIP

--- code/system.ui/react.common/src/ui/PropList/PropList.SPEC.tsx
+++ code/system.ui/react.common/src/ui/PropList/PropList.SPEC.tsx
@@ -1,7 +1,40 @@
-import { Dev, expect } from '../../test.ui';
+import { t, Dev } from '../../test.ui';
+import { PropList } from '.';
+import { sampleItems, BuilderSample } from './dev';
+import type { MyFields } from './dev';
 
-type T = { count: number };
-const initial: T = { count: 0 };
+type SampleKind = 'Samples' | 'Builder';
+type T = {
+  props: t.PropListProps;
+  debug: {
+    source: SampleKind;
+    fields?: MyFields[];
+    fieldSelector: {
+      title: boolean;
+      resettable: boolean;
+      showIndexes: boolean;
+    };
+  };
+};
+
+const initial: T = {
+  props: {
+    title: 'MyTitle',
+    titleEllipsis: true,
+    defaults: { clipboard: false },
+    theme: 'Light',
+    // theme: 'Dark',
+  },
+  debug: {
+    source: 'Samples',
+    // source: 'Builder',
+    fieldSelector: {
+      title: true,
+      resettable: PropList.FieldSelector.DEFAULT.resettable,
+      showIndexes: PropList.FieldSelector.DEFAULT.showIndexes,
+    },
+  },
+};
 
 export default Dev.describe('PropList', (e) => {
   e.it('init', async (e) => {
@@ -11,7 +44,7 @@ export default Dev.describe('PropList', (e) => {
       .display('grid')
       .backgroundColor(1)
       .size(250, null)
-      .render<T>((e) => <div>{`🐷 Hello-${e.state.count}`}</div>);
+      .render<T>((e) => <PropList />);
   });
 
   e.it('debug panel', async (e) => {
@@ -21,6 +54,39 @@ export default Dev.describe('PropList', (e) => {
       .border(-0.1)
       .render<T>((e) => <Dev.ObjectView name={'state'} data={e.state} />);
 
-    dev.button('tmp', (e) => e.change((d) => d.count++));
+    dev
+      .title('Defaults')
+      .boolean((btn) =>
+        btn
+          .label('clipboard')
+          .value((e) => e.state.props.defaults?.clipboard)
+          .onClick((e) => e.change((d) => Dev.toggle(Util.defaults(d.props), 'clipboard'))),
+      )
+      .boolean((btn) =>
+        btn
+          .label('monospace')
+          .value((e) => e.state.props.defaults?.monospace)
+          .onClick((e) => e.change((d) => Dev.toggle(Util.defaults(d.props), 'monospace'))),
+      )
+      .hr();
+
+    dev.title('Title').hr();
   });
 });
+
+/**
+ * [Helpers]
+ */
+
+const Util = {
+  toItems(ctx: T) {
+    const { source, fields } = ctx.debug;
+    if (source === 'Samples') return sampleItems;
+    if (source === 'Builder') return BuilderSample.toItems({ fields });
+    return [];
+  },
+
+  defaults(props: t.PropListProps) {
+    return props.defaults ?? (props.defaults = {});
+  },
+};
