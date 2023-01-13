6bd4ba381fa59da26a156f53cd3ccbc11486dc10
Phil Cockfield <phil@cockfield.net>
2023-01-13 19:07:18 +1300

    spec update

--- code/spikes/dev.000/src/ui/Root/Root.SPEC.tsx
+++ code/spikes/dev.000/src/ui/Root/Root.SPEC.tsx
@@ -1,15 +1,26 @@
-import { Spec } from '../../test.ui';
+import { Dev } from '../../test.ui';
 import { Root } from '.';
 
-export default Spec.describe('Root', (e) => {
+type T = { count: number };
+const initial: T = { count: 0 };
+
+export default Dev.describe('suite', (e) => {
   e.it('init', async (e) => {
-    const ctx = Spec.ctx(e);
+    const ctx = Dev.ctx(e);
+    await ctx.state<T>(initial);
     ctx.component
-      .size('fill')
-      .display('flex')
+      .display('grid')
       .backgroundColor(1)
-      .render(() => {
-        return <Root style={{ flex: 1 }} />;
-      });
+      .size('fill')
+      .render<T>((e) => <Root />);
+  });
+
+  e.it('debug panel', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    dev.footer
+      .border(-0.1)
+      .render<T>((e) => <Dev.Object name={'info'} data={e.state} expand={3} />);
+
+    dev.title('dev.spike: Root (Entry)').hr();
   });
 });
