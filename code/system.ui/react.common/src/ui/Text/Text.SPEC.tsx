04ad8f7b94e09c6ca7076db61235a72cec94ceaa
Phil Cockfield <phil@cockfield.net>
2023-01-13 17:14:19 +1300

    test update: text-to-speech

--- code/system.ui/react.common/src/ui/Text/Text.SPEC.tsx
+++ code/system.ui/react.common/src/ui/Text/Text.SPEC.tsx
@@ -4,11 +4,31 @@ export default Dev.describe('Text', (e) => {
   e.it('debug panel', async (e) => {
     const dev = Dev.tools(e);
 
-    dev.button('text to speech', (e) => {
-      const text = `Hello World.`;
+    dev.section('Text to Speech (W3C)', (dev) => {
       const synth = window.speechSynthesis;
-      const utter = new SpeechSynthesisUtterance(text);
-      synth.speak(utter);
+
+      const utter = (text: string) => {
+        const utter = new SpeechSynthesisUtterance(text);
+        synth.cancel();
+        synth.speak(utter);
+      };
+
+      dev.button('short: "Hello World!"', (e) => {
+        utter(`Hello World!`);
+      });
+
+      dev.button('long: Orwell (extract - "Why I Write")', async (e) => {
+        const { Essays } = await import('./dev/SAMPLE.essay.mjs');
+        const text = Essays.Orwell.extract;
+        const words = text.split(' ');
+
+        utter(words.join(' '));
+        e.label(`long: ${words.length} words`);
+      });
+
+      dev.hr();
+      dev.button('cancel', () => synth.cancel());
+      dev.hr();
     });
   });
 });
