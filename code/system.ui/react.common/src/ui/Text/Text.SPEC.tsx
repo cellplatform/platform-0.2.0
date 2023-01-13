aeeea1227eab26c34ef9c23300077f2c394d6b3b
Phil Cockfield <phil@cockfield.net>
2023-01-13 17:58:25 +1300

    test tweak: text to speech

--- code/system.ui/react.common/src/ui/Text/Text.SPEC.tsx
+++ code/system.ui/react.common/src/ui/Text/Text.SPEC.tsx
@@ -1,34 +1,61 @@
 import { Dev } from '../../test.ui';
 
+type T = { pitch: number };
+const initial: T = { pitch: 1 };
+
 export default Dev.describe('Text', (e) => {
+  e.it('init', async (e) => {});
+
   e.it('debug panel', async (e) => {
-    const dev = Dev.tools(e);
+    const dev = Dev.tools<T>(e);
+    const state = await dev.ctx.state<T>(initial);
 
-    dev.section('Text to Speech (W3C)', (dev) => {
+    dev.footer
+      .border(-0.1)
+      .render<T>((e) => <Dev.Object name={'info'} data={e.state} expand={3} />);
+
+    dev.section('Speech Synthesis Utterance 🤖 (W3C)', (dev) => {
       const synth = window.speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance();
 
-      const utter = (text: string) => {
-        const utter = new SpeechSynthesisUtterance(text);
+      const utter = async (text: string) => {
+        const utter = utterance;
+        utter.text = text;
+        utter.pitch = state.current.pitch;
         synth.cancel();
         synth.speak(utter);
       };
 
-      dev.button('short: "Hello World!"', (e) => {
-        utter(`Hello World!`);
+      dev.hr();
+
+      dev.button('short: "Hello World!"', async (e) => {
+        await utter(`Hello World!`);
       });
 
-      dev.button('long: Orwell (extract - "Why I Write")', async (e) => {
+      dev.button('long: Orwell (essay extract - "Why I Write", 1946)', async (e) => {
         const { Essays } = await import('./dev/SAMPLE.essay.mjs');
         const text = Essays.Orwell.extract;
         const words = text.split(' ');
 
-        utter(words.join(' '));
         e.label(`long: ${words.length} words`);
+        await utter(words.join(' '));
       });
 
       dev.hr();
       dev.button('cancel', () => synth.cancel());
       dev.hr();
+
+      dev.section('Pitch', (dev) => {
+        const adjustPitch = async (amount: number) => {
+          const pitch = state.current.pitch + amount;
+          await state.change((d) => (d.pitch = pitch));
+          if (utterance.text) await utter(utterance.text);
+        };
+
+        const BY = 0.25;
+        dev.button('increment (+)', (e) => adjustPitch(BY));
+        dev.button('increment (-)', (e) => adjustPitch(0 - BY));
+      });
     });
   });
 });
