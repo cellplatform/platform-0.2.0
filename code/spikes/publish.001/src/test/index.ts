3846d47933e96cd8a9431418f9cf4a2e9abea3d5
Phil Cockfield <phil@cockfield.net>
2023-01-18 11:42:15 +1300

    refactor

--- code/spikes/publish.001/src/test/index.ts
+++ code/spikes/publish.001/src/test/index.ts
@@ -2,4 +2,4 @@ export { describe, it } from 'vitest';
 export { expect, expectError } from 'sys.test';
 export { TestFilesystem } from 'sys.fs';
 
-export * from '../common.mjs';
+export * from '../common';
