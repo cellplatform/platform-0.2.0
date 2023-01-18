ca9fdcc85f42cf605ac6300e81fe4f9bff59a3a6
Phil Cockfield <phil@cockfield.net>
2023-01-18 17:41:32 +1300

    CI badge

--- code/spikes/publish.001/src/test.ui/entry.tsx
+++ code/spikes/publish.001/src/test.ui/entry.tsx
@@ -9,6 +9,11 @@ const url = new URL(location.href);
 const params = url.searchParams;
 const isDev = params.has('dev') || params.has('d');
 
+const BADGE_CI = {
+  image: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml/badge.svg',
+  href: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml',
+};
+
 /**
  * User Interface
  */
@@ -17,7 +22,7 @@ const isDev = params.has('dev') || params.has('d');
   const root = createRoot(document.getElementById('root')!);
   if (isDev) {
     const { Dev } = await import('./index');
-    const el = await Dev.render(Pkg, Spec);
+    const el = await Dev.render(Pkg, Spec, { badge: BADGE_CI, hrDepth: 2 });
     root.render(el);
   } else {
     root.render(<Root />);
