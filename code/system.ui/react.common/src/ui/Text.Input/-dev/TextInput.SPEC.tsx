c118948165504d77d2d45ae655b163bd312a0ed5
Phil Cockfield <phil@cockfield.net>
2023-01-26 08:13:27 +1300

    stub in EventBus tests (not working)

--- code/system.ui/react.common/src/ui/Text.Input/-dev/TextInput.SPEC.tsx
+++ code/system.ui/react.common/src/ui/Text.Input/-dev/TextInput.SPEC.tsx
@@ -1,5 +1,5 @@
 import { TextInput } from '..';
-import { Dev, t } from '../../../test.ui';
+import { Dev, t, rx, slug } from '../../../test.ui';
 import { DevSample } from './DEV.Sample';
 
 type T = {
@@ -11,6 +11,7 @@ type T = {
     hint: boolean;
     updateHandlerEnabled: boolean;
   };
+  output: Record<string, any>;
 };
 
 const initial: T = {
@@ -25,19 +26,28 @@ const initial: T = {
     isNumericMask: false,
     updateHandlerEnabled: true,
   },
+  output: {},
 };
 
 export default Dev.describe('TextInput', (e) => {
+  const bus = rx.bus();
+  const instance = { bus, id: `foo.${slug()}` };
+
   e.it('init', async (e) => {
     const ctx = Dev.ctx(e);
+    const state = await ctx.state<T>(initial);
+    await state.change((d) => (d.props.instance = instance));
 
-    await ctx.state<T>(initial);
     ctx.subject
       .display('grid')
-      .size(250, null)
+      .size(300, null)
       .render<T>((e) => {
         const { debug } = e.state;
-        if (!debug.render) return null;
+        if (!debug.render) return;
+
+        const autoSize = e.state.props.autoSize;
+        if (autoSize) ctx.subject.size('fill-x');
+        if (!autoSize) ctx.subject.size(300, null);
 
         const mask = debug.isNumericMask ? TextInput.Masks.isNumeric : undefined;
         const props = { ...e.state.props, mask };
@@ -49,14 +59,18 @@ export default Dev.describe('TextInput', (e) => {
     const dev = Dev.tools<T>(e, initial);
     dev.footer
       .border(-0.1)
-      .render<T>((e) => <Dev.Object name={'spec.TextInput'} data={e.state} expand={1} />);
+      .render<T>((e) => (
+        <Dev.Object
+          name={'spec.TextInput'}
+          data={e.state}
+          expand={{ level: 1, paths: ['$', '$.output', '$.output.status'] }}
+        />
+      ));
 
     dev.section('Configurations', (dev) => {
-      //
       const value = (value: string, label?: string) => {
         dev.button(`text: ${label ?? value}`, (e) => e.change((d) => (d.props.value = value)));
       };
-
       value('hello 👋');
       value(dev.lorem(50), 'long (lorem)');
     });
@@ -76,11 +90,78 @@ export default Dev.describe('TextInput', (e) => {
       boolean('isEnabled');
       boolean('isReadOnly');
       boolean('isPassword');
+      dev.hr();
       boolean('autoCapitalize');
       boolean('autoCorrect');
       boolean('autoComplete');
       boolean('spellCheck');
+      dev.hr();
+      boolean('autoSize');
       boolean('focusOnLoad');
     });
+
+    dev.TODO(`focusActions: ${TextInput.DEFAULTS.focusActions.join()}`);
+
+    dev.hr();
+
+    dev.section('Events', (dev) => {
+      const events = TextInput.Events({ instance });
+
+      dev.button('⚡️ Status', async (e) => {
+        console.log('⚡️ Status | events:', events.instance);
+        const status = await events.status.get();
+        await e.change((d) => (d.output.status = status));
+
+        console.log('status', status);
+      });
+
+      dev.hr();
+
+      dev.button('⚡️ Focus', (e) => events.focus.fire());
+      dev.button('⚡️ Blur', (e) => events.focus.fire(false));
+
+      dev.hr();
+
+      dev.button('⚡️ Select (All)', (e) => events.select.fire());
+      dev.button('⚡️ Cursor: Start', (e) => events.cursor.start());
+      dev.button('⚡️ Cursor: End', (e) => events.cursor.end());
+
+      dev.hr();
+    });
+
+    dev.hr();
+
+    dev.section('Debug', (dev) => {
+      dev.boolean((btn) =>
+        btn
+          .label('render')
+          .value((e) => e.state.debug.render)
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'render'))),
+      );
+
+      dev.boolean((btn) =>
+        btn
+          .label((e) => `mask: isNumeric`)
+          .value((e) => e.state.debug.isNumericMask)
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'isNumericMask'))),
+      );
+
+      dev.boolean((btn) =>
+        btn
+          .label((e) => `hint list (auto-complete)`)
+          .value((e) => e.state.debug.hint)
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'hint'))),
+      );
+
+      dev.boolean((btn) => {
+        const current = (state: T) => (state.debug.updateHandlerEnabled ? 'enabled' : 'disabled');
+        btn
+          .label((e) => `update handler: ${current(e.state)}`)
+          .value((e) => e.state.debug.updateHandlerEnabled)
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'updateHandlerEnabled')));
+      });
+    });
+
+    dev.hr();
   });
 });
