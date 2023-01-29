3db0274bcd4432405c32bde0616ff9de50c31b7b
Phil Cockfield <phil@cockfield.net>
2023-01-30 10:50:40 +1300

    refactor

--- code/system.ui/react.common/src/ui/IFrame/IFrame.SPEC.tsx
+++ code/system.ui/react.common/src/ui/IFrame/IFrame.SPEC.tsx
@@ -3,19 +3,27 @@ import { IFrame, IFrameProps } from './IFrame';
 
 const DEFAULTS = IFrame.DEFAULTS;
 
+const backgroundImage = {
+  url: 'https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+  opacity: 0.3,
+};
+
 type T = { props: IFrameProps; debug: { backgroundImage: boolean } };
 const initial: T = {
   props: {
     src: 'https://en.wikipedia.org/wiki/World_Wide_Web_Consortium',
     allow: 'camera; microphone',
   },
-  debug: { backgroundImage: false },
+  debug: { backgroundImage: true },
 };
 
 export default Dev.describe('IFrame', (e) => {
   e.it('init', async (e) => {
     const ctx = Dev.ctx(e);
     await ctx.state<T>(initial);
+
+    ctx.host.backgroundImage(backgroundImage);
+
     ctx.subject
       .display('grid')
       .size('fill')
@@ -34,10 +42,8 @@ export default Dev.describe('IFrame', (e) => {
         .value((e) => e.state.debug.backgroundImage)
         .onClick((e) => {
           const next = !e.state.current.debug.backgroundImage;
-          const url =
-            'https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80';
           e.change((d) => (d.debug.backgroundImage = next));
-          e.ctx.host.backgroundImage(next ? { url, opacity: 0.3 } : null);
+          e.ctx.host.backgroundImage(next ? backgroundImage : null);
         }),
     );
 
@@ -48,7 +54,8 @@ export default Dev.describe('IFrame', (e) => {
         dev.button(`src: ${label}`, (e) => e.change((d) => (d.props.src = url)));
       };
 
-      load('Wikipedia', 'https://en.wikipedia.org/wiki/World_Wide_Web_Consortium');
+      load('Wikipedia: "W3C"', 'https://en.wikipedia.org/wiki/World_Wide_Web_Consortium');
+      load('Wikipedia: "Foobar" mobile format', 'https://en.m.wikipedia.org/wiki/Foobar');
       load('Google (← blocked)', 'https://google.com');
       dev.hr();
       dev.button('srcDoc (← <html>)', (e) =>
