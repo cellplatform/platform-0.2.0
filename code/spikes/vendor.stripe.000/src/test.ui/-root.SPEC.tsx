50a91c5e9a3cb8aaa623f128f1863db0efc97861
Phil Cockfield <phil@cockfield.net>
2023-01-30 20:02:24 +1300

    strip root spec (stub)

---
+++ code/spikes/vendor.stripe.000/src/test.ui/-root.SPEC.tsx
@@ -0,0 +1,29 @@
+import { Dev } from '../test.ui';
+
+type T = { count: number };
+const initial: T = { count: 0 };
+
+export default Dev.describe('Root', (e) => {
+  e.it('init', async (e) => {
+    const ctx = Dev.ctx(e);
+    await ctx.state<T>(initial);
+    ctx.subject
+      .display('grid')
+      .backgroundColor(1)
+      .size(250, null)
+      .render<T>((e) => {
+        return <div>{`🐷 Hello-${'Stripe'}`}</div>;
+      });
+  });
+
+  e.it('debug panel', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    dev.footer
+      .border(-0.1)
+      .render<T>((e) => <Dev.Object name={'spec.Stripe'} data={e.state} expand={1} />);
+
+    dev.title('Stripe API (Wrapper)');
+    dev.TODO('Take a donation (digital Koha) - simple payment UI (Credit Card)');
+    dev.TODO('Make a markeplate transaction');
+  });
+});
