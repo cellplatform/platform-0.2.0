72380bdd38d4f485b7c11be35f4f686edb29cd73
Phil Cockfield <phil@cockfield.net>
2023-02-03 17:00:41 +1300

    sort into dev.sections

--- code/system.ui/react.common/src/ui.dev/DevTools.Button/DevTools.Button.SPEC.tsx
+++ code/system.ui/react.common/src/ui.dev/DevTools.Button/DevTools.Button.SPEC.tsx
@@ -28,76 +28,86 @@ export default Dev.describe('Button', (e) => {
       .border(-0.1)
       .render<T>((e) => <Dev.Object name={'props'} data={e.state.props} expand={1} />);
 
-    dev
-      .button('state: increment count', async (e) => {
-        await e.change((d) => d.count++);
-        e.label(`state: count-${e.state.current.count}`);
-      })
-      .button((btn) => {
-        let count = 0;
-        return btn.label('rename (self)').onClick((e) => {
-          count++;
-          e.label(`renamed-${count} (within closure)`);
-        });
-      })
-      .button((btn) => {
-        return btn
-          .label((e) => {
-            const bg = e.dev.subject.backgroundColor ?? 0;
-            const count = e.state.count;
-            return `change props: (props-${bg}) / state-${count}`;
+    dev.section((dev) => {
+      dev
+        .button('state: increment count', async (e) => {
+          await e.change((d) => d.count++);
+          e.label(`state: count-${e.state.current.count}`);
+        })
+        .button((btn) => {
+          let count = 0;
+          return btn.label('rename (self)').onClick((e) => {
+            count++;
+            e.label(`renamed-${count} (within closure)`);
+          });
+        })
+        .button((btn) => {
+          return btn
+            .label((e) => {
+              const bg = e.dev.subject.backgroundColor ?? 0;
+              const count = e.state.count;
+              return `change props: (props-${bg}) / state-${count}`;
+            })
+            .onClick((e) => e.ctx.subject.backgroundColor(1));
+        })
+        /**
+         * Shorthand: "label", "onClick" parameter declaration.
+         */
+        .button('no `onClick`');
+    });
+
+    dev.hr();
+
+    dev.section((dev) => {
+      dev
+        .button('right: clear', (e) => e.change(({ props }) => (props.rightElement = undefined)))
+        .button('right: `<Switch>`', (e) =>
+          e.change(({ props }) => (props.rightElement = <Switch height={16} />)),
+        )
+        .button('right: `<div>123</div>`', (e) =>
+          e.change(({ props }) => (props.rightElement = <div>123</div>)),
+        );
+    });
+
+    dev.hr();
+
+    dev.section((dev) => {
+      dev.button((btn) =>
+        btn
+          .label((e) => `sample left`)
+          .right((e) => {
+            const style = css({
+              PaddingX: 10,
+              PaddingY: 2,
+              borderRadius: 10,
+              backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
+            });
+            return <div {...style}>right element</div>;
           })
-          .onClick((e) => e.ctx.subject.backgroundColor(1));
-      })
-      /**
-       * Shorthand: "label", "onClick" parameter declaration.
-       */
-      .button('no `onClick`')
-      .hr()
-      .button('right: clear', (e) => e.change(({ props }) => (props.rightElement = undefined)))
-      .button('right: `<Switch>`', (e) =>
-        e.change(({ props }) => (props.rightElement = <Switch height={16} />)),
-      )
-      .button('right: `<div>123</div>`', (e) =>
-        e.change(({ props }) => (props.rightElement = <div>123</div>)),
+          .onClick((e) => {
+            console.log('click');
+          }),
       );
 
+      dev.button((btn) => btn.label('sample left').right('"right string"'));
+    });
     dev.hr();
 
-    dev.button((btn) =>
-      btn
-        .label((e) => `sample left`)
-        .right((e) => {
-          const style = css({
-            PaddingX: 10,
-            PaddingY: 2,
-            borderRadius: 10,
-            backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
-          });
-          return <div {...style}>right element</div>;
-        })
-        .onClick((e) => {
-          console.log('click');
-        }),
-    );
+    dev.section((dev) => {
+      dev.boolean((btn) =>
+        btn
+          .label('debug.enabled')
+          .value((e) => e.state.debug.enabled)
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'enabled'))),
+      );
 
-    dev.button((btn) => btn.label('sample left').right('"right string"'));
-
-    dev.hr();
-
-    dev.boolean((btn) =>
-      btn
-        .label('debug.enabled')
-        .value((e) => e.state.debug.enabled)
-        .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'enabled'))),
-    );
-
-    dev.button((btn) =>
-      btn
-        .label((e) => `my button (${e.state.debug.enabled ? 'enabled' : 'disabled'})`)
-        .enabled((e) => e.state.debug.enabled)
-        .onClick((e) => console.info('click')),
-    );
+      dev.button((btn) =>
+        btn
+          .label((e) => `my button (${e.state.debug.enabled ? 'enabled' : 'disabled'})`)
+          .enabled((e) => e.state.debug.enabled)
+          .onClick((e) => console.info('click')),
+      );
+    });
 
     dev.hr();
   });
