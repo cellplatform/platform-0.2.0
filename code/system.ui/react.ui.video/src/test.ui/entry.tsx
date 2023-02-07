2f5abc442b7828d442de7f088b93b491469b7e03
Phil Cockfield <phil@cockfield.net>
2023-02-08 10:38:14 +1300

    video - building and running with 'vite-plugin-top-level-await'

--- code/system.ui/react.ui.video/src/test.ui/entry.tsx
+++ code/system.ui/react.ui.video/src/test.ui/entry.tsx
@@ -1,12 +1,13 @@
 import 'symbol-observable';
 
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Dev } from '.';
 import { Pkg } from '../index.pkg.mjs';
-import { Specs } from './entry.Specs.mjs';
+import { Dev } from 'sys.ui.react.common';
 
-(async () => {
-  const el = await Dev.render(Pkg, Specs);
-  const root = createRoot(document.getElementById('root')!);
-  root.render(el);
-})();
+const { Specs } = await import('./entry.Specs.mjs');
+
+const el = await Dev.render(Pkg, Specs);
+const root = createRoot(document.getElementById('root')!);
+root.render(<StrictMode>{el}</StrictMode>);
+console.info(`Pkg:`, Pkg);
