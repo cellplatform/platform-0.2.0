ef337346b2ba5278e601acc0c00feffe2f9d6779
Phil Cockfield <phil@cockfield.net>
2023-02-15 08:08:12 +1300

    delete old

--- code/system/sys.data.crdt.__legacy/src/Crdt.Path/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './Crdt.Path.mjs';
--- code/system/sys.data.crdt.__legacy/src/common/index.ts
+++
@@ -1,6 +0,0 @@
-import type * as t from './types.mjs';
-
-export { t };
-export * from '../index.pkg.mjs';
-export * from './libs.mjs';
-export * from './Is.mjs';
