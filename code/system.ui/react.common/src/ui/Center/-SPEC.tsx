d6240b1e35710dbb6715d0ed771457108a36bc2d
Phil Cockfield <phil@cockfield.net>
2023-02-28 08:52:58 +1300

    delete old code

--- code/system.ui/react.common/src/ui/Center/-SPEC.tsx
+++ code/system.ui/react.common/src/ui/Center/-SPEC.tsx
@@ -65,34 +65,3 @@ export default Dev.describe('Center', (e) => {
     });
   });
 });
-
-// export default Dev.describe('Center', (e) => {
-//   e.it('init', async (e) => {
-//     const ctx = Dev.ctx(e);
-//
-//     const styles = {
-//       base: css({
-//         flex: 1,
-//         overflow: 'hidden',
-//         backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
-//       }),
-//       child: css({ fontSize: 120 }),
-//     };
-//
-//     const el = (
-//       <Center style={styles.base}>
-//         <div {...styles.child}>🐷</div>
-//       </Center>
-//     );
-//
-//     ctx.subject
-//       .size('fill')
-//       .display('flex')
-//       .render(() => el);
-//   });
-//
-//   e.it('debug panel', async (e) => {
-//     const dev = Dev.tools(e);
-//     dev.title('Center');
-//   });
-// });
