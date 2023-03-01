d11bf5e6644995f0b16b5646cd3048a378a0bfd3
Phil Cockfield <phil@cockfield.net>
2023-03-01 13:06:03 +1300

    export Automerge

---
+++ code/system.ui/react.ui.monaco/src/test.ui/common.ts
@@ -0,0 +1,11 @@
+/**
+ * @system
+ */
+export { expect, expectError } from 'sys.test';
+export { Dev } from 'sys.ui.react.common';
+export { Automerge, Crdt } from 'sys.data.crdt';
+
+/**
+ * @local
+ */
+export * from '../common';
