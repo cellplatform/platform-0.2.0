f1bc6d0bcaff31c7c68d0a0c958e2ea86aecea68
Phil Cockfield <phil@cockfield.net>
2023-03-09 10:33:22 +1300

    tweak template

--- code/templates/esm/test.ui/entry.tsx
+++ code/templates/esm/test.ui/entry.tsx
@@ -3,12 +3,15 @@ import 'symbol-observable';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Pkg } from '../index.pkg.mjs';
+import { Dev } from 'sys.ui.react.common';
 
-const common = await import('sys.ui.react.common');
-const { Specs } = await import('./entry.Specs.mjs');
-const { Dev } = common;
+async function render() {
+  const { Specs } = await import('./entry.Specs.mjs');
 
-const el = await Dev.render(Pkg, Specs, { hrDepth: 3 });
-const root = createRoot(document.getElementById('root')!);
-root.render(<StrictMode>{el}</StrictMode>);
-console.info(`Pkg:`, Pkg);
+  const el = await Dev.render(Pkg, Specs, { hrDepth: 3 });
+  const root = createRoot(document.getElementById('root')!);
+  root.render(<StrictMode>{el}</StrictMode>);
+  console.info(`Pkg:`, Pkg);
+}
+
+render();
