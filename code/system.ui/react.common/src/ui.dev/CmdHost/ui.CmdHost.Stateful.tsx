9365cc304c7c6d273e48a8badd796bd1a4ab535f
Phil Cockfield <phil@cockfield.net>
2023-03-23 08:53:26 +1300

    reset selection on filter change

--- code/system.ui/react.common/src/ui.dev/CmdHost/ui.CmdHost.Stateful.tsx
+++ code/system.ui/react.common/src/ui.dev/CmdHost/ui.CmdHost.Stateful.tsx
@@ -12,30 +12,32 @@ type T = t.Subject<t.SpecListScrollTarget>;
  * A version of <CmdHost> that manages state interanally.
  */
 export const CmdHostStateful: React.FC<CmdHostStatefulProps> = (props) => {
-  const { mutateUrl = true, specs } = props;
-  const total = specs ? Object.keys(specs).length : -1;
+  const { mutateUrl = true } = props;
 
   const [command, setCommand] = useState(Wrangle.url().filter);
   const [isFocused, setFocused] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
 
+  const specs = SpecList.Filter.specs(props.specs, command);
+  const total = Object.keys(specs).length;
   const hintKeys = Wrangle.hintKey({ isFocused, selectedIndex, specs, command });
-  const filteredSpecs = SpecList.Filter.specs(specs, command);
 
   const [childItems, setChildItems] = useState<t.SpecListChildVisibility[]>([]);
   const selectionChangeTrigger = childItems.map((item) => item.isOnScreen).join(',');
-  const scrollToRef = useRef<T>(new rx.Subject<t.SpecListScrollTarget>());
+  const scrollToRef$ = useRef<T>(new rx.Subject<t.SpecListScrollTarget>());
 
   /**
-   * Effects
+   * [Effects]
    */
   useEffect(() => {
     const child = childItems[selectedIndex];
-    if (child && !child.isOnScreen) {
-      const index = child.index;
-      scrollToRef.current.next({ index });
-    }
+    const scrollTo$ = scrollToRef$.current;
+    if (child && !child.isOnScreen) scrollTo$.next({ index: child.index });
   }, [selectedIndex, selectionChangeTrigger]);
+
+  useEffect(() => {
+    setSelectedIndex(0); // Reset the selection when the filter changes.
+  }, [total, command]);
 
   /**
    * [Handlers]
@@ -50,26 +52,26 @@ export const CmdHostStateful: React.FC<CmdHostStatefulProps> = (props) => {
     const done = () => e.preventDefault();
 
     if (e.key === 'Home' || (e.key === 'ArrowUp' && e.metaKey)) {
-      setSelectedIndex(Wrangle.selected(filteredSpecs, 0));
+      setSelectedIndex(Wrangle.selected(specs, 0));
       return done();
     }
     if (e.key === 'End' || (e.key === 'ArrowDown' && e.metaKey)) {
-      setSelectedIndex(Wrangle.selected(filteredSpecs, total - 1));
+      setSelectedIndex(Wrangle.selected(specs, total - 1));
       return done();
     }
 
     if (e.key === 'ArrowUp') {
       const next = selectedIndex - (e.altKey ? 5 : 1);
-      setSelectedIndex(Wrangle.selected(filteredSpecs, next));
+      setSelectedIndex(Wrangle.selected(specs, next));
       return done();
     }
     if (e.key === 'ArrowDown') {
       const next = selectedIndex + (e.altKey ? 5 : 1);
-      setSelectedIndex(Wrangle.selected(filteredSpecs, next));
+      setSelectedIndex(Wrangle.selected(specs, next));
       return done();
     }
     if (e.key === 'Enter' && mutateUrl) {
-      Url.mutateSelected(selectedIndex, filteredSpecs);
+      Url.mutateSelected(selectedIndex, specs);
 
       /**
        * NB: forced page reload here
@@ -93,7 +95,7 @@ export const CmdHostStateful: React.FC<CmdHostStatefulProps> = (props) => {
       command={command}
       selectedIndex={isFocused ? selectedIndex : undefined}
       hintKey={hintKeys}
-      scrollTo$={scrollToRef.current}
+      scrollTo$={scrollToRef$.current}
       onChanged={handleFilterChanged}
       onCmdFocusChange={(e) => setFocused(e.isFocused)}
       onKeyDown={handleKeyboard}
