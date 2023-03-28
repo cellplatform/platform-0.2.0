62ef332973f1ed794b8e7667af7688b1c35f251d
Phil Cockfield <phil@cockfield.net>
2023-03-28 18:59:19 +1300

    CRDT: text change (diff) sample

---
+++ code/system/sys.data.crdt/src/test.ui/-SPEC.Text.tsx
@@ -0,0 +1,58 @@
+import { css, Dev, Crdt, t, Automerge } from '../test.ui';
+
+type T = { count: number };
+const initial: T = { count: 0 };
+
+export default Dev.describe('Text', (e) => {
+  type D = { text: t.AutomergeText };
+  const doc = Crdt.Doc.ref<D>({ text: new Automerge.Text() });
+
+  e.it('init', async (e) => {
+    const ctx = Dev.ctx(e);
+    ctx.subject
+      .backgroundColor(1)
+      .display('grid')
+      .render((e) => {
+        const text = doc.current.text.toString();
+        const style: t.CssValue = {
+          fontSize: 18,
+          padding: 10,
+          opacity: text ? 1 : 0.5,
+        };
+
+        return <div {...css(style)}>{text || 'empty'}</div>;
+      });
+  });
+
+  e.it('ui:debug', async (e) => {
+    const dev = Dev.tools(e);
+    const state = await dev.ctx.state<T>(initial);
+    const redraw = () => state.change((d) => d.count++);
+
+    dev.textbox((txt) =>
+      txt
+        .label('Text')
+        .value(() => doc.current.text.toString())
+        .onChange((e) => {
+          doc.change((d) => {
+            e.next.diff.forEach((change) => {
+              if (change.kind === 'Added') {
+                d.text.insertAt(change.index, change.value);
+              }
+              if (change.kind === 'Deleted') {
+                d.text.deleteAt(change.index, change.value.length);
+              }
+            });
+          });
+          redraw();
+        }),
+    );
+
+    /**
+     * Footer
+     */
+    dev.footer
+      .border(-0.1)
+      .render<D>((e) => <Dev.Object name={'Dev.Text'} data={e.state} expand={1} />);
+  });
+});
--- code/system/sys.data.crdt/src/test.ui/entry.Specs.mts
+++ code/system/sys.data.crdt/src/test.ui/entry.Specs.mts
@@ -2,6 +2,7 @@ export const Specs = {
   'sys.crdt.tests': () => import('./-TestRunner'),
   'sys.crdt.ui.Info': () => import('../ui/Crdt.Info/-SPEC'),
   'sys.crdt.ui.History': () => import('../ui/Crdt.History/-SPEC'),
+  'sys.crdt.types.Text': () => import('./-SPEC.Text'),
 };
 
 export default Specs;
