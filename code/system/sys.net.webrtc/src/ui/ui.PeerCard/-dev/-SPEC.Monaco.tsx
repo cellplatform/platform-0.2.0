b04d4ebbe3a17d2da8d3943b46df415d0b1dd8a7
Phil Cockfield <phil@cockfield.net>
2023-04-04 14:11:46 +1200

    Update -SPEC.Monaco.tsx

--- code/system/sys.net.webrtc/src/ui/ui.PeerCard/-dev/-SPEC.Monaco.tsx
+++ code/system/sys.net.webrtc/src/ui/ui.PeerCard/-dev/-SPEC.Monaco.tsx
@@ -1,37 +1,108 @@
+import { useEffect, useState } from 'react';
 import { MonacoCrdt, MonacoEditor } from 'sys.ui.react.monaco';
-import { Crdt, css, t } from './common';
+
+import { COLORS, Crdt, css, rx, t } from './common';
 
 import type { DocShared } from './Schema.mjs';
 
 type T = t.JsonMap & { code: t.AutomergeText; peers: t.EditorPeersState };
+type EditorCtx = { monaco: t.Monaco; editor: t.MonacoCodeEditor };
 
-export function SpecMonacoSync(self: t.Peer, doc: t.CrdtDocRef<DocShared>) {
+export type SpecMonacoSyncProps = {
+  self?: t.Peer;
+  doc?: t.CrdtDocRef<DocShared>;
+  style?: t.CssValue;
+};
+
+/**
+ * A shared syncing code-editor.
+ */
+export const SpecMonacoSync: React.FC<SpecMonacoSyncProps> = (props) => {
+  const { self, doc } = props;
+  const [indexCtx, setIndexCtx] = useState<EditorCtx>();
+  const [mainCtx, setMainCtx] = useState<EditorCtx>();
+
+  /**
+   * [Lifecycle]
+   */
+  useEffect(() => {
+    const { dispose, dispose$ } = rx.disposable();
+    if (self && mainCtx && doc) {
+      /**
+       * Ensure the code field exists.
+       */
+      if (!doc.current.tmp.code) {
+        doc.change((d) => ((d.tmp as T).code = Crdt.text()));
+      }
+      if (!doc.current.tmp.peers) {
+        doc.change((d) => ((d.tmp as T).peers = {}));
+      }
+
+      /**
+       * Start the syncer.
+       */
+      const { monaco, editor } = mainCtx;
+      const syncer = MonacoCrdt.syncer({
+        dispose$,
+        monaco,
+        editor,
+        data: { doc, getText: (d) => (d.tmp as T).code },
+        peers: { local: self.id, doc, getPeers: (d) => (d.tmp as T).peers },
+      });
+
+      console.log('syncer', syncer);
+
+      doc.$.pipe(rx.takeUntil(dispose$)).subscribe((e) => {
+        const tmp = e.doc.tmp as T;
+
+        const remotePeers = Object.keys(tmp.peers)
+          .filter((id) => id !== self.id)
+          .map((id) => tmp.peers[id]);
+
+        /**
+         * TODO 🐷
+         * - update
+         */
+        console.log('remotePeers', remotePeers);
+      });
+    }
+
+    return dispose;
+  }, [self, doc, mainCtx]);
+
+  /**
+   * [Render]
+   */
+  const styles = {
+    base: css({
+      backgroundColor: COLORS.WHITE,
+      height: 250,
+      display: 'grid',
+      gridTemplateColumns: '1fr 1.3fr',
+    }),
+  };
+
+  if (!self || !doc) return <div {...css(styles.base, props.style)} />;
+
+  const SAMPLE_INDEX = `
+  diagrams:
+    - slc/intro
+    - slc/macro
+    - slc/impact
+  `.substring(1);
+
   return (
-    <div {...css({ height: 200, display: 'grid' })}>
+    <div {...css(styles.base, props.style)}>
+      <MonacoEditor
+        language={'yaml'}
+        text={SAMPLE_INDEX}
+        onReady={({ editor, monaco }) => setIndexCtx({ editor, monaco })}
+      />
       <MonacoEditor
         focusOnLoad={true}
-        onReady={({ editor }) => {
-          /**
-           * Ensure the code field exists.
-           */
-          if (!doc.current.tmp.code) {
-            doc.change((d) => ((d.tmp as T).code = Crdt.text()));
-          }
-          if (!doc.current.tmp.peers) {
-            doc.change((d) => ((d.tmp as T).peers = {}));
-          }
-
-          /**
-           * Start the syncer.
-           */
-          const syncer = MonacoCrdt.syncer({
-            editor,
-            data: { doc, getText: (d) => (d.tmp as T).code },
-            peers: { local: self.id, doc, getPeers: (d) => (d.tmp as T).peers },
-          });
-          console.log('syncer', syncer);
-        }}
+        language={'yaml'}
+        onReady={({ editor, monaco }) => setMainCtx({ editor, monaco })}
       />
     </div>
   );
-}
+};
