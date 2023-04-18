ac1297538afbf3879cb9b4b3efb76239fac9a615
Phil Cockfield <phil@cockfield.net>
2023-04-18 14:03:17 +1200

    refactor

--- code/spikes/dev.000/src/test.ui/entry.tsx
+++ code/spikes/dev.000/src/test.ui/entry.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client';
 const params = new URL(location.href).searchParams;
 const isDev = params.has('dev') || params.has('d');
 
-const BADGE = {
+const badge = {
   image: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml/badge.svg',
   href: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml',
 };
@@ -22,14 +22,14 @@ const render = async (subject: SubjectMatter) => {
   if (subject === 'Dev') {
     const { Dev } = await import('sys.ui.react.common');
     const { Specs } = await import('./entry.Specs.mjs');
-    const el = await Dev.render(Pkg, Specs, { badge: BADGE, hrDepth: 3 });
-    return root.render(el);
+    const el = await Dev.render(Pkg, Specs, { badge, hrDepth: 3 });
+    root.render(el);
+    return;
   }
 
   if (subject === 'DefaultEntry') {
     const { Dev } = await import('sys.ui.react.common');
-    const pkg = Pkg.toString();
-    root.render(<Dev.Splash footer={pkg} />);
+    root.render(<Dev.Splash footer={Pkg.toString()} />);
     return;
   }
 
