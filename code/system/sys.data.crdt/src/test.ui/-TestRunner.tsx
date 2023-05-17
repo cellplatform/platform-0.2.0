3e0e8d43dec2608783416a6d47f7850d1f5e6c97
Phil Cockfield <phil@cockfield.net>
2023-05-18 11:25:38 +1200

    Update -TestRunner.tsx

--- code/system/sys.data.crdt/src/test.ui/-TestRunner.tsx
+++ code/system/sys.data.crdt/src/test.ui/-TestRunner.tsx
@@ -64,8 +64,8 @@ export default Dev.describe('Root', (e) => {
           await button(import('../crdt.DocFile/-TEST.mjs')),
           await button(import('../crdt.DocSync/-dev/-TEST.DocSync.mjs')),
           await button(import('../crdt.DocSync/-dev/-TEST.PeerSyncer.mjs')),
-          await button(import('../crdt.Schema/-dev/-TEST.mjs')),
           await button(import('../crdt.Lens/-dev/-TEST.mjs')),
+          await button(import('../crdt.Schema/-dev/-TEST.mjs')),
           await button(import('../crdt.helpers/-TEST.mjs')),
         ],
       );
