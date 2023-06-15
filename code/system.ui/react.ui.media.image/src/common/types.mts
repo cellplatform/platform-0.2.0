47f4530e68a48b8c9afab5190347ff19c14dddff
Phil Cockfield <phil@cockfield.net>
2023-06-16 09:54:45 +1200

    delete old

--- code/system.ui/react.ui.media.image/src/common/types.mts
+++
@@ -1,9 +0,0 @@
-/**
- * @system
- */
-export type { EventBus, Disposable } from 'sys.types/src/types.mjs';
-
-/**
- * @local
- */
-export * from '../types.mjs';
--- code/system.ui/react.ui.media.image/src/test/index.ts
+++
@@ -1,4 +0,0 @@
-export { expect, expectError } from 'sys.test';
-export { Test } from 'sys.test.spec';
-export { describe, it } from 'vitest';
-export * from '../common';
