5eb20e50c12144fedb198794171f0af43f2ab2ef
Phil Cockfield <phil@cockfield.net>
2023-06-20 13:24:22 +1200

    refactor

--- code/system/sys.data.crdt/src/test.ui/index.ts
+++ code/system/sys.data.crdt/src/test.ui/index.ts
@@ -1,9 +1,7 @@
-import { TestFilesystem } from 'sys.fs.indexeddb';
-
+export { TestFilesystem } from 'sys.fs.indexeddb';
 export { expect, expectError } from 'sys.test';
 export { Test, Tree } from 'sys.test.spec';
 export { Dev } from 'sys.ui.react.common';
-export { TestFilesystem };
 
 export { Automerge, Crdt, toObject } from '../index.mjs';
 export * from '../ui/common';
