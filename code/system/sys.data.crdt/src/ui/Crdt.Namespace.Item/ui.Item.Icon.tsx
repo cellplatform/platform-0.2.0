f1187a38e288cdc03617727fdd9cfb711fc92d03
Phil Cockfield <phil@cockfield.net>
2023-07-12 12:35:59 +1200

    underline when in edit state ← <Label>

--- code/system/sys.data.crdt/src/ui/Crdt.Namespace.Item/ui.Item.Icon.tsx
+++ code/system/sys.data.crdt/src/ui/Crdt.Namespace.Item/ui.Item.Icon.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
-import { Color, COLORS, css, DEFAULTS, FC, rx, type t, Icons } from './common';
+import { COLORS, Icons, css, type t } from './common';
 
 export type ItemIconProps = {
   kind: 'Repo' | 'Editing' | 'Json' | 'ObjectTree';
   color?: string | number;
   opacity?: number;
+  width?: number;
   style?: t.CssValue;
 };
 
 export const ItemIcon: React.FC<ItemIconProps> = (props) => {
-  const { opacity = 1 } = props;
+  const { opacity = 1, width } = props;
 
   /**
    * [Render]
@@ -19,6 +19,7 @@ export const ItemIcon: React.FC<ItemIconProps> = (props) => {
       display: 'grid',
       placeItems: 'center',
       opacity,
+      width,
     }),
   };
 
@@ -36,7 +37,7 @@ const Wrangle = {
       return <Icons.Repo size={18} color={color} offset={[0, 1]} />;
     }
     if (kind === 'Editing') {
-      return <Icons.Editing size={17} color={color} offset={[0, 3]} />;
+      return <Icons.Editing size={17} color={color} offset={[0, 4]} />;
     }
     if (kind === 'Json') {
       return <Icons.Json size={17} color={color} />;
--- code/system/sys.data.crdt/src/ui/Crdt.Namespace.Item/ui.Item.Label.tsx
+++ code/system/sys.data.crdt/src/ui/Crdt.Namespace.Item/ui.Item.Label.tsx
@@ -1,4 +1,4 @@
-import { DEFAULTS, TextInput, css, type t } from './common';
+import { Color, DEFAULTS, TextInput, css, type t } from './common';
 
 import { Wrangle } from './Wrangle';
 
@@ -6,34 +6,42 @@ export const ItemLabel: React.FC<t.CrdtNamespaceItemProps> = (props) => {
   const {
     enabled = DEFAULTS.enabled,
     editing = DEFAULTS.editing,
+    selected = DEFAULTS.selected,
     maxLength = DEFAULTS.maxLength,
   } = props;
-  const foreColor = Wrangle.foreColor(props);
   const { value } = Wrangle.value(props);
+  const color = Wrangle.foreColor(props);
+  const underlineColor = Color.alpha(color, selected ? 0.5 : 0.2);
 
   /**
    * [Render]
    */
   const styles = {
-    base: css({}),
+    base: css({ position: 'relative' }),
     textbox: css({}),
+    underline: css({
+      pointerEvents: 'none',
+      borderBottom: `dashed 1.2px ${underlineColor}`,
+      Absolute: [null, 0, 0, -3],
+    }),
   };
 
   return (
     <div {...css(styles.base, props.style)}>
+      {editing && <div {...styles.underline} />}
       <TextInput
         style={styles.textbox}
         placeholder={'namespace'}
         placeholderStyle={{
           opacity: 0.3,
-          color: foreColor,
-          disabledColor: foreColor,
+          color: color,
+          disabledColor: color,
         }}
         value={value}
         valueStyle={{
           fontSize: 13,
-          color: foreColor,
-          disabledColor: foreColor,
+          color: color,
+          disabledColor: color,
         }}
         maxLength={maxLength}
         spellCheck={false}
