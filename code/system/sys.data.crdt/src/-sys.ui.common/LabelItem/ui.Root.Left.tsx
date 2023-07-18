4223caa6b29c6a329a596ecf9968d1460b9ac857
Phil Cockfield <phil@cockfield.net>
2023-07-18 13:08:12 +1200

    clean

--- code/system/sys.data.crdt/src/-sys.ui.common/LabelItem/ui.Root.Left.tsx
+++ code/system/sys.data.crdt/src/-sys.ui.common/LabelItem/ui.Root.Left.tsx
@@ -3,8 +3,7 @@ import { DEFAULTS, type t } from './common';
 import { Actions } from './ui.Actions';
 
 export const Left: React.FC<t.LabelItemProps> = (props) => {
-
-  const flags = Wrangle.flagProps(props);
+  const flags = Wrangle.flagsOrDefault(props);
   const action = props.left ?? DEFAULTS.leftAction;
   return <Actions {...flags} edge={'Left'} action={action} />;
 };
