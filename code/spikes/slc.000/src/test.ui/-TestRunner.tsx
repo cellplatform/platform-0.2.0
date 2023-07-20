61d1911ecfc51ae80cefd3a5bfeaf633783c5a10
Phil Cockfield <phil@cockfield.net>
2023-07-20 15:03:08 +1200

    update namesaces

--- code/spikes/slc.000/src/test.ui/-TestRunner.tsx
+++ code/spikes/slc.000/src/test.ui/-TestRunner.tsx
@@ -36,7 +36,7 @@ export default Dev.describe('TestRunner', (e) => {
       runner
         .run({})
         .modules(async () => (await import('./-TestRunner.TESTS.mjs')).TESTS.all)
-        .localstore('dev:NAMESPACE')
+        .localstore('dev:slc.testrunner')
         .keyboard(true)
         .onChanged((e) => state.change((d) => (d.results = e.results))),
     );
--- code/spikes/slc.000/src/test.ui/entry.Specs.mts
+++ code/spikes/slc.000/src/test.ui/entry.Specs.mts
@@ -1,8 +1,8 @@
 export { Pkg } from '../index.pkg.mjs';
 
 export const Specs = {
-  'NAMESPACE.tests': () => import('./-TestRunner'),
-  'NAMESPACE.ui.Info': () => import('../ui/Info/-dev/-SPEC'),
+  'slc.tests': () => import('./-TestRunner'),
+  'slc.ui.Info': () => import('../ui/Info/-dev/-SPEC'),
 };
 
 export default Specs;
