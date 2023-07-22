75ecf0ee9556299696b9d81015e8a651f91854d4
Phil Cockfield <phil@cockfield.net>
2023-07-22 15:27:43 +1200

    ContainerQuery - sample CSS tested and working

--- code/system.ui/react.common/src/ui/ContainerQuery/-SPEC.tsx
+++ code/system.ui/react.common/src/ui/ContainerQuery/-SPEC.tsx
@@ -1,8 +1,14 @@
 import { Dev, type t } from '../../test.ui';
 import { ContainerQuery } from '.';
 
-type T = {};
-const initial: T = {};
+type T = {
+  props: t.ContainerQueryProps;
+  debug: { render?: boolean };
+};
+const initial: T = {
+  props: { displayName: 'foobar' },
+  debug: { render: true },
+};
 
 export default Dev.describe('ContainerQuery', (e) => {
   e.it('ui:init', async (e) => {
@@ -10,19 +16,32 @@ export default Dev.describe('ContainerQuery', (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await ctx.state<T>(initial);
     await state.change((d) => {});
+
+    ctx.debug.width(330);
     ctx.subject
       .backgroundColor(1)
       .size('fill')
       .display('grid')
       .render<T>((e) => {
-        return <ContainerQuery />;
+        const { debug } = e.state;
+        if (!debug.render) return <div />;
+        return <ContainerQuery {...e.state.props} />;
       });
   });
 
   e.it('ui:debug', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
-    dev.TODO();
+
+    dev.section('Debug', (dev) => {
+      dev.boolean((btn) => {
+        const value = (state: T) => Boolean(state.debug.render);
+        btn
+          .label((e) => `render`)
+          .value((e) => value(e.state))
+          .onClick((e) => e.change((d) => Dev.toggle(d.debug, 'render')));
+      });
+    });
   });
 
   e.it('ui:footer', async (e) => {
--- code/system.ui/react.common/src/ui/ContainerQuery/types.mts
+++ code/system.ui/react.common/src/ui/ContainerQuery/types.mts
@@ -1,5 +1,6 @@
 import { type t } from './common';
 
 export type ContainerQueryProps = {
+  displayName?: string; // NB: Optional name added as an attribute on the <style> element for debugging.
   style?: t.CssValue;
 };
