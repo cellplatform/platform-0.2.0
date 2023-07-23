9474e7b0d3eba5471239191854dee18fcdde63d0
Phil Cockfield <phil@cockfield.net>
2023-07-23 18:46:00 +1200

    sample Image URL

--- code/spikes/slc.000/src/ui/ui.Concept.Slug/common.ts
+++ code/spikes/slc.000/src/ui/ui.Concept.Slug/common.ts
@@ -6,17 +6,20 @@ export { Position } from '../ui.Position';
  * Constants
  */
 const position: t.Pos = ['center', 'center'];
-
 const sample: t.ConceptSlug = {
   id: 'sample',
   video: {
     id: 499921561, //→ vimeo/tubes
     position: ['left', 'bottom'],
   },
-  image: {},
+  image: {
+    // NB: "group scale"
+    src: 'https://user-images.githubusercontent.com/185555/208217954-0427e91d-fcb3-4e9a-b5f1-1f86ed3500bf.png',
+  },
 };
 
 export const DEFAULTS = {
+  videoHeight: 200,
   position,
   sample,
 } as const;
