45222eee9bec6571a5aa49607ff2c1f77da22038
Phil Cockfield <phil@cockfield.net>
2023-07-26 15:52:50 +1200

    ProgressBar → transition on width

--- code/system.ui/react.common/src/ui/ProgressBar/Root.tsx
+++ code/system.ui/react.common/src/ui/ProgressBar/Root.tsx
@@ -23,7 +23,7 @@ const View: React.FC<t.ProgressBarProps> = (props) => {
    * [Render]
    */
   const trackHeight = mouse.isOver ? 10 : 5;
-  const transition = `height 0.15s, background-color 0.15s`;
+  const transition = `width 0.15s, height 0.15s, background-color 0.15s`;
   const styles = {
     base: css({
       height,
