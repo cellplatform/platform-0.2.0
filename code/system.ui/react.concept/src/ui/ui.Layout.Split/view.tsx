cb2618de293cdd25f13701db06448d6705561f13
Phil Cockfield <phil@cockfield.net>
2023-08-04 12:14:32 +1200

    <View> break out

---
+++ code/system.ui/react.concept/src/ui/ui.Layout.Split/view.tsx
@@ -0,0 +1,36 @@
+import { Wrangle } from './Wrangle.mjs';
+import { COLORS, Color, DEFAULTS, FC, css, type t } from './common';
+
+export const View: React.FC<t.SplitLayoutProps> = (props) => {
+  const { debug = false, axis = DEFAULTS.axis, children = [] } = props;
+
+  /**
+   * [Render]
+   */
+  const styles = {
+    base: css({
+      position: 'relative',
+      ...Wrangle.gridCss(props),
+    }),
+    container: css({ position: 'relative', display: 'grid' }),
+    top: css({}),
+    bottom: css({}),
+    debug: css({
+      Absolute: axis === 'x' ? [0, 0, null, 0] : [0, null, 0, 0],
+      width: axis === 'y' ? 1 : undefined,
+      height: axis === 'x' ? 1 : undefined,
+      pointerEvents: 'none',
+      backgroundColor: Color.alpha(COLORS.RED, 0.1),
+    }),
+  };
+
+  return (
+    <div {...css(styles.base, props.style)}>
+      <div {...css(styles.container, styles.top)}>{children[0]}</div>
+      <div {...css(styles.container, styles.bottom)}>
+        {children[1]}
+        {debug && <div {...styles.debug} />}
+      </div>
+    </div>
+  );
+};
