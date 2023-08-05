22f7832f4a42e5cb45baa780f1929a6441cbc1b6
Phil Cockfield <phil@cockfield.net>
2023-08-06 11:53:03 +1200

    index with CRDT in spec

--- code/system.ui/react.concept/src/ui/ui.Index/-SPEC.tsx
+++ code/system.ui/react.concept/src/ui/ui.Index/-SPEC.tsx
@@ -5,14 +5,14 @@ type T = { props: t.IndexProps };
 const initial: T = { props: {} };
 const name = Root.displayName ?? '';
 
-type Doc = { name: string };
+type Doc = { count: number };
 
 export default Dev.describe(name, async (e) => {
   const dir = 'dev/concept/index.spec';
   const fs = (await TestFilesystem.client()).fs.dir(dir);
 
   const docid = 'dev-index';
-  const doc = Crdt.ref<Doc>(docid, { name: 'foo' });
+  const doc = Crdt.ref<Doc>(docid, { count: 0 });
   const file = await Crdt.file<Doc>(fs, doc, { autosave: true });
 
   e.it('ui:init', async (e) => {
@@ -36,27 +36,32 @@ export default Dev.describe(name, async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
 
-    dev.row((e) => {
-      return (
-        <CrdtViews.Info
-          card={true}
-          title={'CRDT'}
-          fields={[
-            'Module',
-            'Module.Verify',
-            'History',
-            'History.Item',
-            'History.Item.Message',
-            'File',
-            'File.Driver',
-          ]}
-          data={{
-            file: { doc: file, path: dir },
-            history: { data: doc.history },
-            url: { href: '?dev=sys.data.crdt.tests' },
-          }}
-        />
-      );
+    dev.section('File', (dev) => {
+      dev.button('increment (+)', () => file.doc.change((d) => d.count++));
+      dev.button('delete file', () => file.delete());
+
+      dev.hr(0, 6);
+
+      dev.row((e) => {
+        return (
+          <CrdtViews.Info
+            card={true}
+            title={'CRDT'}
+            fields={[
+              'Module',
+              'History',
+              'History.Item',
+              'History.Item.Message',
+              'File',
+              'File.Driver',
+            ]}
+            data={{
+              file: { doc: file, path: dir },
+              history: { data: doc.history },
+            }}
+          />
+        );
+      });
     });
 
     dev.hr(0, 10);
