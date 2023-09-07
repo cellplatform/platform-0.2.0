9f459db3c8595a9835de2ec032f78462d0f3ac93
Phil Cockfield <phil@cockfield.net>
2023-09-07 17:10:18 +1200

    delete sample

--- code/ext/ext.driver.auth.privy/src/ui/ui.Info/ui.tsx
+++ code/ext/ext.driver.auth.privy/src/ui/ui.Info/ui.tsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../ui.Auth/Auth.Provider';
-import { DEFAULTS, t } from './common';
+import { DEFAULTS, type t } from './common';
 import { Builder } from './ui.Builder';
 
 export const View: React.FC<t.InfoProps> = (props) => {
