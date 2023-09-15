f1c16c03c36a5992bd763ad8a054facee6e303e5
Phil Cockfield <phil@cockfield.net>
2023-09-16 09:36:27 +1200

    stub in sample spec

--- code/ext/ext.ipfs.pinning/src/test.ui/entry.Specs.mts
+++ code/ext/ext.ipfs.pinning/src/test.ui/entry.Specs.mts
@@ -3,6 +3,7 @@ export { Pkg } from '../index.pkg.mjs';
 export const Specs = {
   'ext.ipfs.pinning.tests': () => import('./-TestRunner'),
   'ext.ipfs.pinning.ui.Info': () => import('../ui/ui.Info/-SPEC'),
+  'ext.ipfs.pinning.ui.Sample': () => import('../ui/ui.Sample/-SPEC'),
 };
 
 export default Specs;
---
+++ code/ext/ext.ipfs.pinning/src/ui/ui.Sample/-SPEC.tsx
@@ -0,0 +1,43 @@
+import { Dev, type t } from '../../test.ui';
+
+type T = {};
+const initial: T = {};
+
+/**
+ * Spec
+ */
+const name = 'Sample';
+
+export default Dev.describe(name, (e) => {
+  e.it('ui:init', async (e) => {
+    const ctx = Dev.ctx(e);
+    const dev = Dev.tools<T>(e, initial);
+
+    const state = await ctx.state<T>(initial);
+    await state.change((d) => {});
+
+    ctx.debug.width(330);
+    ctx.subject
+      .backgroundColor(1)
+      .size([250, null])
+      .display('grid')
+      .render<T>((e) => {
+        return <div>{`🐷 ${name}`}</div>;
+      });
+  });
+
+  e.it('ui:debug', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    const state = await dev.state();
+    dev.TODO();
+  });
+
+  e.it('ui:footer', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    const state = await dev.state();
+    dev.footer.border(-0.1).render<T>((e) => {
+      const data = e.state;
+      return <Dev.Object name={name} data={data} expand={1} />;
+    });
+  });
+});
