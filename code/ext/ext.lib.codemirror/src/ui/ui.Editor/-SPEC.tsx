7d8ebb3443d18abe92d36ff3b1c78689294aecc6
Phil Cockfield <phil@cockfield.net>
2023-09-26 08:15:15 +1300

    refs to YAML language (code editor)

--- code/ext/ext.lib.codemirror/src/ui/ui.Editor/-SPEC.tsx
+++ code/ext/ext.lib.codemirror/src/ui/ui.Editor/-SPEC.tsx
@@ -5,6 +5,14 @@ type T = { props: t.RootProps };
 const initial: T = { props: {} };
 const name = Root.displayName ?? '';
 
+/**
+ * Spec:
+ *
+ * - YAML
+ *   https://discuss.codemirror.net/t/yaml-linter-for-uiw-react-codemirror-or-codemirror-v6/4976/8
+ *   https://codesandbox.io/s/clever-aryabhata-o1yx07?file=/package.json
+ */
+
 export default Dev.describe(name, (e) => {
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
