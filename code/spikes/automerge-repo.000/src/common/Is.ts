fe15fb56e2c2db884a7b32675ee89fbd4149a991
Phil Cockfield <phil@cockfield.net>
2023-09-26 17:24:16 +1300

    Is flags

---
+++ code/spikes/automerge-repo.000/src/common/Is.ts
@@ -0,0 +1,37 @@
+import type * as t from './types';
+
+export const Is = {
+  repo(input: any): input is t.Repo {
+    if (!isObject(input)) return false;
+    const { networkSubsystem, storageSubsystem } = input;
+    if (!Is.networkSubsystem(networkSubsystem)) return false;
+    if (storageSubsystem && !Is.storageSubsystem(storageSubsystem)) return false;
+    return true;
+  },
+
+  networkSubsystem(input: any): input is t.NetworkSubsystem {
+    if (!isObject(input)) return false;
+    return (
+      typeof input.peerId === 'string' &&
+      typeof input.isReady === 'function' &&
+      typeof input.whenReady === 'function' &&
+      typeof input.send === 'function'
+    );
+  },
+
+  storageSubsystem(input: any): input is t.StorageSubsystem {
+    if (!isObject(input)) return false;
+    return (
+      typeof input.loadDoc === 'function' &&
+      typeof input.saveDoc === 'function' &&
+      typeof input.remove === 'function'
+    );
+  },
+} as const;
+
+/**
+ * Helpers
+ */
+export function isObject(input: any): input is Object {
+  return typeof input === 'object' && input !== null;
+}
