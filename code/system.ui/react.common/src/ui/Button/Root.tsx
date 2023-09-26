90ca7a822dadac22b01b9983ad47f439fcb984ee
Phil Cockfield <phil@cockfield.net>
2023-09-26 19:22:08 +1300

    <Button.Blue>

--- code/system.ui/react.common/src/ui/Button/Root.tsx
+++ code/system.ui/react.common/src/ui/Button/Root.tsx
@@ -1,13 +1,15 @@
 import { DEFAULTS, FC, type t } from './common';
+import { Blue } from './Root.Blue';
 import { View } from './ui.Button';
 import { CopyButton as Copy } from './ui.Button.Copy';
 
 type Fields = {
   DEFAULTS: typeof DEFAULTS;
+  Blue: typeof Blue;
   Copy: typeof Copy;
 };
 export const Button = FC.decorate<t.ButtonProps, Fields>(
   View,
-  { DEFAULTS, Copy },
+  { DEFAULTS, Copy, Blue },
   { displayName: 'Button' },
 );
