88573c3e87df4131112ff9e9f0f0302da55c370a
Phil Cockfield <phil@cockfield.net>
2023-10-04 09:09:22 +1300

    fix absolute position wrangle

--- code/system.ui/react.common/src/ui/RenderCount/RenderCount.tsx
+++ code/system.ui/react.common/src/ui/RenderCount/RenderCount.tsx
@@ -40,6 +40,6 @@ const Wrangle = {
   absolute(input: RenderCountProps['absolute']): t.CssValue['Absolute'] {
     if (!input || !Array.isArray(input)) return [3, 4, null, null];
     if (input.length === 2) return [input[0], input[1], null, null];
-    return input.slice(0, 3) as t.CssValue['Absolute'];
+    return input.slice(0, 4) as t.CssValue['Absolute'];
   },
 };
