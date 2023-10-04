9fa15b4efb1f5dcffc4fb1e5e60d2883ede3ff0c
Phil Cockfield <phil@cockfield.net>
2023-10-05 12:08:19 +1300

    sample upload using w3up

--- code/ext/ext.driver.ipfs.w3s/src/ui/ui.Sample.w3up/-SPEC.tsx
+++ code/ext/ext.driver.ipfs.w3s/src/ui/ui.Sample.w3up/-SPEC.tsx
@@ -1,4 +1,6 @@
 import { Dev, type t } from '../../test.ui';
+import { create } from '@web3-storage/w3up-client';
+import { Wrangle } from '../ui.Sample.web3storage';
 
 type T = {};
 const initial: T = {};
@@ -33,24 +35,21 @@ export default Dev.describe(name, (e) => {
     /**
      * NOTE:
      *    This is the "new" client, which is still in development.
-     *    https://github.com/web3-storage/w3up/tree/main/packages/w3up-client#basic-usage
+     *    - https://web3-storage.github.io/w3up-client/
+     *    - https://github.com/web3-storage/w3up/tree/main/packages/w3up-client#basic-usage
      */
     dev.section('debug', (dev) => {
       dev.button((btn) => {
         const github = 'https://github.com/web3-storage/w3up/tree/main/packages/w3up-client';
         btn
-          .label(`lib`)
+          .label(`basic usage`)
           .right((e) => (
             <a href={github} target={'_blank'}>
               {'github (pre-release)'}
             </a>
           ))
           .enabled((e) => true)
-          .onClick(async (e) => {
-            const { create } = await import('@web3-storage/w3up-client');
-            const client = await create();
-            console.log('client', client);
-          });
+          .onClick(sampleBasicUsage);
       });
     });
 
@@ -67,3 +66,60 @@ export default Dev.describe(name, (e) => {
     });
   });
 });
+
+/**
+ * Sample
+ * - https://web3-storage.github.io/w3up-client/#basic-usage
+ * - https://github.com/web3-storage/w3up/tree/main/packages/w3up-client#basic-usage
+ */
+async function sampleBasicUsage() {
+  const client = await create();
+  console.log('client', client);
+
+  /**
+   * Create and register space.
+   */
+  const name = 'my-sample-space';
+  if (client.spaces().length === 0) {
+    await client.createSpace(name);
+  }
+
+  const space = client.spaces().find((s) => s.name() === name)!;
+  await client.setCurrentSpace(space.did());
+
+  if (!space.registered()) {
+    try {
+      const email = 'phil@cockfield.net';
+      console.info(`check for email sent to: ${email}`);
+      await client.registerSpace(email, { provider: 'did:web:web3.storage' });
+    } catch (err) {
+      console.error('registration failed: ', err);
+    }
+  }
+
+  /**
+   * Upload file.
+   */
+  const binary = new TextEncoder().encode('boom');
+  const files = [
+    new File(['# Hello World 👋'], 'README.md'),
+    new File(['console.info("🌳")'], 'src/main.ts'),
+    new File([binary], 'data/binary.dat'),
+  ];
+
+  console.log('files', files);
+  const directoryCid = await client.uploadDirectory(files);
+
+  console.log('directoryCid', directoryCid);
+  console.log('directoryCid', directoryCid.toString());
+  const cid = directoryCid.toString();
+
+  // bafybeiaqylpxvbovgpw6owvsgba4grfdv4hlpekxu7wwfwqvwpu4kduj7y
+  const url = Wrangle.Url.cid(cid);
+  console.log('url', url);
+
+  console.log('directoryCid.link', directoryCid.link());
+
+  const list = await client.capability.store.list();
+  console.log('list', list);
+}
