727947a72e562be851f804652a7d58808b7c4c18
Phil Cockfield <phil@cockfield.net>
2023-10-05 19:24:55 +1300

    refactor

--- code/ext/ext.lib.ipfs.w3s/src/ui/ui.Sample.w3up/-SPEC.tsx
+++ code/ext/ext.lib.ipfs.w3s/src/ui/ui.Sample.w3up/-SPEC.tsx
@@ -1,6 +1,5 @@
-import { Dev, type t } from '../../test.ui';
-import { create } from '@web3-storage/w3up-client';
-import { Wrangle } from '../ui.Sample.old.w3storage';
+import { Dev } from '../../test.ui';
+import { sampleBasicUsage } from './-SPEC.sample.BasicUsage';
 
 type T = {};
 const initial: T = {};
@@ -32,12 +31,6 @@ export default Dev.describe(name, (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
 
-    /**
-     * NOTE:
-     *    This is the "new" client, which is still in development.
-     *    - https://web3-storage.github.io/w3up-client/
-     *    - https://github.com/web3-storage/w3up/tree/main/packages/w3up-client#basic-usage
-     */
     dev.section('debug', (dev) => {
       dev.button((btn) => {
         const github = 'https://github.com/web3-storage/w3up/tree/main/packages/w3up-client';
@@ -66,60 +59,3 @@ export default Dev.describe(name, (e) => {
     });
   });
 });
-
-/**
- * Sample
- * - https://web3-storage.github.io/w3up-client/#basic-usage
- * - https://github.com/web3-storage/w3up/tree/main/packages/w3up-client#basic-usage
- */
-async function sampleBasicUsage() {
-  const client = await create();
-  console.log('client', client);
-
-  /**
-   * Create and register space.
-   */
-  const name = 'my-sample-space';
-  if (client.spaces().length === 0) {
-    await client.createSpace(name);
-  }
-
-  const space = client.spaces().find((s) => s.name() === name)!;
-  await client.setCurrentSpace(space.did());
-
-  if (!space.registered()) {
-    try {
-      const email = 'phil@cockfield.net';
-      console.info(`check for email sent to: ${email}`);
-      await client.registerSpace(email, { provider: 'did:web:web3.storage' });
-    } catch (err) {
-      console.error('registration failed: ', err);
-    }
-  }
-
-  /**
-   * Upload file.
-   */
-  const binary = new TextEncoder().encode('boom');
-  const files = [
-    new File(['# Hello World 👋'], 'README.md'),
-    new File(['console.info("🌳")'], 'src/main.ts'),
-    new File([binary], 'data/binary.dat'),
-  ];
-
-  console.log('files', files);
-  const directoryCid = await client.uploadDirectory(files);
-
-  console.log('directoryCid', directoryCid);
-  console.log('directoryCid', directoryCid.toString());
-  const cid = directoryCid.toString();
-
-  // bafybeiaqylpxvbovgpw6owvsgba4grfdv4hlpekxu7wwfwqvwpu4kduj7y
-  const url = Wrangle.Url.cid(cid);
-  console.log('url', url);
-
-  console.log('directoryCid.link', directoryCid.link());
-
-  const list = await client.capability.store.list();
-  console.log('list', list);
-}
