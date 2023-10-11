90be64ed3f2d05bd96ec28b88ff95c1a85fee3ff
Phil Cockfield <phil@cockfield.net>
2023-10-12 09:14:08 +1300

    Update types.List.ts

--- code/system.ui/react.common/src/ui/Item.LabelItem.Stateful/types.List.ts
+++ code/system.ui/react.common/src/ui/Item.LabelItem.Stateful/types.List.ts
@@ -20,7 +20,6 @@ export type LabelListState = t.PatchState<t.LabeList>;
 /**
  * Controller API's
  */
-
 export type LabelListController<Kind extends string, H extends HTMLElement> = {
   readonly kind: Kind;
   readonly ref: React.RefObject<H>;
