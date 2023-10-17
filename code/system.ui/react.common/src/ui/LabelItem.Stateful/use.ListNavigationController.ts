331b1461ac2472762b27a7ba45612633d4096628
Phil Cockfield <phil@cockfield.net>
2023-10-17 14:02:04 +1300

    update after selection model change

--- code/system.ui/react.common/src/ui/LabelItem.Stateful/use.ListNavigationController.ts
+++ code/system.ui/react.common/src/ui/LabelItem.Stateful/use.ListNavigationController.ts
@@ -1,17 +1,17 @@
 import { useEffect } from 'react';
-import { Keyboard, rx, type t } from './common';
+import { Keyboard, type t } from './common';
+
 type Args<H extends HTMLElement = HTMLDivElement> = {
   ref: React.RefObject<H>;
   enabled?: boolean;
   list?: t.LabelListState;
-  items?: t.LabelItemState[];
 };
 
 /**
  * HOOK: Selection behavior for a <List> of <Items>.
  */
 export function useListNavigationController<H extends HTMLElement = HTMLDivElement>(args: Args<H>) {
-  const { ref, enabled = true, list, items = [] } = args;
+  const { ref, list, enabled = true } = args;
 
   /**
    * Keyboard.
@@ -23,27 +23,28 @@ export function useListNavigationController<H extends HTMLElement = HTMLDivEleme
           return Boolean(list?.current.focused);
         },
         get editing() {
-          return items.some((item) => item.current.editing);
+          return Boolean(list?.current.editing);
         },
       },
       index: {
         get selected() {
           const selected = list?.current.selected;
-          return items.findIndex((item) => item.instance === selected);
+          return selected ? list?.current.getItem?.(selected)[1] ?? -1 : -1;
         },
         get last() {
-          return items.length - 1;
+          return (list?.current.total ?? 0) - 1;
         },
       },
       select(index: number) {
         if (!list || !List.is.focused || List.is.editing) return;
-        index = Math.max(0, Math.min(index, items.length - 1));
-        const item = items[index];
+        index = Math.max(0, Math.min(index, list.current.total - 1));
+        const item = list.current.getItem?.(index)[0];
         list.change((d) => (d.selected = item?.instance));
       },
     } as const;
 
     const { dispose } = Keyboard.onKeydown((e) => {
+      if (List.is.editing) return;
       const index = List.index;
       if (e.key === 'ArrowUp') List.select(e.metaKey ? 0 : index.selected - 1);
       if (e.key === 'ArrowDown') List.select(e.metaKey ? index.last : index.selected + 1);
@@ -53,10 +54,13 @@ export function useListNavigationController<H extends HTMLElement = HTMLDivEleme
 
     if (!enabled) dispose();
     return dispose;
-  }, [enabled, items, list]);
+  }, [enabled, list?.instance]);
 
   /**
    * API
    */
-  return { ref, enabled } as const;
+  return {
+    ref,
+    enabled,
+  } as const;
 }
