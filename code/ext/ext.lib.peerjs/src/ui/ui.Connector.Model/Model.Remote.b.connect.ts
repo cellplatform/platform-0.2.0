6d4f76930a833dd965174197d5bbbf51b333d7a0
Phil Cockfield <phil@cockfield.net>
2023-10-23 19:21:32 +1300

    WIP

--- code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/Model.Remote.b.connect.ts
+++ code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/Model.Remote.b.connect.ts
@@ -1,5 +1,5 @@
 import { Data } from './Data';
-import { Model, PeerUri, Time, slug, type t } from './common';
+import { Webrtc, Model, PeerUri, Time, slug, type t } from './common';
 
 export function openConnectionBehavior(args: {
   ctx: t.GetConnectorCtx;
@@ -16,7 +16,41 @@ export function openConnectionBehavior(args: {
   events.cmd.action.on('remote:right').subscribe((e) => {
     /**
      * TODO 🐷
+     * - listen for [ENTER] key (as ell as button click)
+     * - start spinning
+     * - connect to remote
+     * - stop spinning, update visual state with connection.
+     *
      */
-    console.log('connect', e);
+    console.log('💥 connect', e);
+
+    console.log('WebRtc', Webrtc);
+    const peer = Webrtc.Peer.create();
+
+    // peer.connect()
+
+    const data = Data.remote(state.current);
+    console.log('data', data);
+
+    const remoteid = data.remoteid ?? '';
+    if (!remoteid) {
+      // TODO: signal problem
+    }
+
+    const conn = peer.connect(remoteid);
+
+    console.log('conn', conn);
+    conn.on('open', async () => {
+      console.log('on open:', conn);
+      // console.log('open', conn);
+      // conn.send(`hi from ${local}!`);
+      // connections.push(conn);
+      // dev.redraw();
+      // await e.change((d) => (d.debug.connectingData = false));
+
+      /**
+       * TODO 🐷
+       */
+    });
   });
 }
