846b75b068fd438b004034c7b7bb804cbb523ab1
Phil Cockfield <phil@cockfield.net>
2023-10-24 07:55:14 +1300

    delete files

--- code/system/sys.util/src/Dispose/Dispose.ts
+++
@@ -1,9 +0,0 @@
-import { create } from './Dispose.create';
-import { done } from './Dispose.done';
-import { until } from './Dispose.until';
-
-export const Dispose = {
-  create,
-  until,
-  done,
-} as const;
