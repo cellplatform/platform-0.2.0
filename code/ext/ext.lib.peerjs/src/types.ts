5ad81997dde71fed5ef48c826c39dda674e72b1f
Phil Cockfield <phil@cockfield.net>
2023-10-24 12:33:35 +1300

    move types

--- code/ext/ext.lib.peerjs/src/types.ts
+++ code/ext/ext.lib.peerjs/src/types.ts
@@ -19,5 +19,6 @@ export type * from './Webrtc.PeerModel/types';
 export type * from './Webrtc/types';
 
 export type * from './ui/ui.Connector/types';
+export type * from './ui/ui.Connector.Model/types';
 export type * from './ui/ui.Dev/types';
 export type * from './ui/ui.Info/types';
