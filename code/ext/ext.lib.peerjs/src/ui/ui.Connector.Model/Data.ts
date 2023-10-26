8c4a2265a499614cef42c264572fa147fc6bcfad
Phil Cockfield <phil@cockfield.net>
2023-10-27 07:47:29 +1300

    clean / docs

--- code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/Data.ts
+++ code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/Data.ts
@@ -2,11 +2,20 @@ import { Model, type t } from './common';
 
 type Input = t.LabelItem | t.LabelItemState;
 
+/**
+ * Helpers for retreiving the typed {data} on the list/item models.
+ */
 export const Data = {
+  /**
+   * Item
+   */
   kind: (item: Input) => Model.data<t.ConnectorData>(item).kind,
   self: (item: Input) => Model.data<t.ConnectorDataSelf>(item),
   remote: (item: Input) => Model.data<t.ConnectorDataRemote>(item),
 
+  /**
+   * List
+   */
   list(input: t.ConnectorListState) {
     const list = {
       get items() {
@@ -17,9 +26,10 @@ export const Data = {
       },
       remotes: {
         get items() {
+          type R = t.ConnectorItemRemote;
           return list.items
             .filter((item) => item.data?.kind === 'peer:remote')
-            .map((item) => item as t.ConnectorItemRemote);
+            .map((item) => item as R);
         },
         get data() {
           return list.remotes.items.map((item) => item.data!);
