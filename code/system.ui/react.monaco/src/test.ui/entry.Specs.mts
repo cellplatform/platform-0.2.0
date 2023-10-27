3c20902941e858e2f7a57e08c304a8ff72cce3be
Phil Cockfield <phil@cockfield.net>
2023-10-27 19:27:22 +1300

    stub in <Bitext> component

--- code/system.ui/react.monaco/src/test.ui/entry.Specs.mts
+++ code/system.ui/react.monaco/src/test.ui/entry.Specs.mts
@@ -3,6 +3,7 @@ export { Pkg } from '../index.pkg.mjs';
 export const Specs = {
   'sys.ui.code.ui.MonacoEditor': () => import('../ui/ui.MonacoEditor/-SPEC'),
   'sys.ui.code.ui.MonacoCrdt': () => import('../ui.logic/MonacoCrdt/-dev/-SPEC'),
+  'sys.ui.code.ui.Bitext': () => import('../ui/ui.Bitext/-SPEC'),
   'sys.ui.code.ui.Sample': () => import('../ui/ui.Sample/-SPEC'),
 };
 
---
+++ code/system.ui/react.monaco/src/ui/ui.Bitext/-SPEC.tsx
@@ -0,0 +1,40 @@
+import { Dev, type t } from '../../test.ui';
+import { Bitext } from '.';
+
+type T = { props: t.BitextProps };
+const initial: T = { props: {} };
+const name = Bitext.displayName ?? '';
+
+export default Dev.describe(name, (e) => {
+  e.it('ui:init', async (e) => {
+    const ctx = Dev.ctx(e);
+    const dev = Dev.tools<T>(e, initial);
+
+    const state = await ctx.state<T>(initial);
+    await state.change((d) => {});
+
+    ctx.debug.width(330);
+    ctx.subject
+      .backgroundColor(1)
+      .size('fill')
+      .display('grid')
+      .render<T>((e) => {
+        return <Bitext {...e.state.props} />;
+      });
+  });
+
+  e.it('ui:debug', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    const state = await dev.state();
+    dev.TODO();
+  });
+
+  e.it('ui:footer', async (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    const state = await dev.state();
+    dev.footer.border(-0.1).render<T>((e) => {
+      const data = e.state;
+      return <Dev.Object name={name} data={data} expand={1} />;
+    });
+  });
+});
