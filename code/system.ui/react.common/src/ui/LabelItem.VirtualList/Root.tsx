bdd34aef967f4872d87b8550909216ee20cf65d1
Phil Cockfield <phil@cockfield.net>
2023-11-07 06:21:16 +1300

    Update Root.tsx

--- code/system.ui/react.common/src/ui/LabelItem.VirtualList/Root.tsx
+++ code/system.ui/react.common/src/ui/LabelItem.VirtualList/Root.tsx
@@ -5,7 +5,7 @@ import { VirtualListRef } from './Ref';
 import { View } from './ui';
 
 /**
- * A "virtual" (infinite) scrolling list
+ * A "virtual" (infinite) scrolling list.
  */
 export const VirtualList = forwardRef<t.VirtualListRef, t.VirtualListProps>((props, ref) => {
   const { list } = props;
