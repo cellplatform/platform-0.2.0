e44d2b4f2d17ae83861b3e2df347f706fbda512b
Phil Cockfield <phil@cockfield.net>
2023-11-09 19:30:48 +1300

    tweak

--- code/compiler/template.esm/src/ui/ui.Info/-SPEC.tsx
+++ code/compiler/template.esm/src/ui/ui.Info/-SPEC.tsx
@@ -25,7 +25,7 @@ export default Dev.describe(name, (e) => {
       d.props.margin = 10;
     });
 
-    ctx.debug.width(330);
+    ctx.debug.width(300);
     ctx.subject
       .backgroundColor(1)
       .size([320, null])
@@ -44,7 +44,6 @@ export default Dev.describe(name, (e) => {
         const props = e.state.props;
         return (
           <Dev.FieldSelector
-            style={{ Margin: [10, 40, 10, 30] }}
             all={DEFAULTS.fields.all}
             selected={props.fields}
             onClick={(ev) => {
