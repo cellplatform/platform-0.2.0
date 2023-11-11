7cd17189071e01d2a6be724b69b703c9922821fd
Phil Cockfield <phil@cockfield.net>
2023-11-11 16:15:28 +1300

    return array

--- code/ext/ext.lib.automerge/src/common/u.TEST.v.ts
+++ code/ext/ext.lib.automerge/src/common/u.TEST.v.ts
@@ -9,8 +9,16 @@ describe('Util', () => {
     expect(res).to.not.equal(obj);
   });
 
+  it('toObject ← array', () => {
+    const array = [1, [2, 3, { count: 4 }]];
+    const res = toObject(array);
+    expect(res).to.eql(array);
+    expect(res).to.not.equal(array);
+    expect(Array.isArray(res)).to.eql(true);
+  });
+
   it('toObject ← non {} param', () => {
-    [undefined, null, 123, [], ''].forEach((value) => {
+    [undefined, null, 123, ''].forEach((value) => {
       expect(toObject(value)).to.eql({}, `${value}`);
     });
   });
--- code/ext/ext.lib.automerge/src/common/u.ts
+++ code/ext/ext.lib.automerge/src/common/u.ts
@@ -3,9 +3,8 @@ import { R } from './libs';
 /**
  * Convert the given input to a simple object (deep).
  */
-export function toObject<T>(input: any): T {
+export function toObject<T>(input: T): T {
   if (input === null) return {} as T;
   if (typeof input !== 'object') return {} as T;
-  if (Array.isArray(input)) return {} as T;
   return R.clone(input);
 }
