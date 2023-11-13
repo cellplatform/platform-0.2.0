61d6aee5509342df5e544eba3b32b88665e32541
Phil Cockfield <phil@cockfield.net>
2023-11-13 18:24:58 +1300

    delete video ref

--- code/system.ui/react.media.video/src/ui/ui.VideoPlayer/-SPEC.tsx
+++ code/system.ui/react.media.video/src/ui/ui.VideoPlayer/-SPEC.tsx
@@ -169,7 +169,6 @@ export default Dev.describe('Player (Vime)', (e) => {
       def(SAMPLE.YOUTUBE.AlanKay, 'Alan Kay');
       def(SAMPLE.YOUTUBE.LocalFirst, 'Local First @pvh');
       dev.hr(-1, 5);
-      def(SAMPLE.VIDEO.GmFarcaster, 'IPFS: GM Farcaster');
       def(SAMPLE.VIDEO.GroupScale, 'IPFS: Group Scale');
       def(SAMPLE.VIDEO.GroupScaleFrench, 'IPFS: Group Scale (French)');
       dev.hr(-1, 5);
