13405838b52084cb7a2891695bc30104da9ade10
Phil Cockfield <phil@cockfield.net>
2023-11-29 08:45:34 +1300

    slow down redraw loop on new item creation

--- code/ext/ext.lib.automerge/src/ui/ui.RepoList.Model/Model.List.b.ts
+++ code/ext/ext.lib.automerge/src/ui/ui.RepoList.Model/Model.List.b.ts
@@ -14,10 +14,10 @@ export function listBehavior(args: { ctx: t.RepoListCtxGet }) {
 
   // NB: After a new document is added, simply append 1 to the length of the list.
   //     The [getItem] data generator takes care of the rest.
-  indexEvents.added$.pipe(rx.filter((e) => e.total !== currentTotal() + 1)).subscribe((e) => {
+  indexEvents.added$.pipe(rx.filter((e) => e.total !== currentTotal() + 1)).subscribe(async (e) => {
     const total = currentTotal();
     list.state.change((d) => (d.total = total + 1)); //    (1) Append new item to list.
-    list.dispatch.redraw();
-    Time.delay(0, () => list.dispatch.edit(total - 1)); // (2) Start edit.
+    await Time.delay(0, () => list.dispatch.redraw());
+    await Time.delay(0, () => list.dispatch.edit(total - 1)); // (2) Start edit.
   });
 }
