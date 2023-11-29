19ac337f15264c0deac97d21295ff9b3fdddfc3f
Phil Cockfield <phil@cockfield.net>
2023-11-30 07:13:40 +1300

    stub in connector UI

--- code/ext/ext.lib.automerge.webrtc/src/ui/ui.Connection/-SPEC.tsx
+++ code/ext/ext.lib.automerge.webrtc/src/ui/ui.Connection/-SPEC.tsx
@@ -1,11 +1,24 @@
-import { Dev, type t } from '../../test.ui';
 import { Root } from '.';
+import { Crdt, Dev, TestDb, Webrtc, type t } from '../../test.ui';
 
 type T = { props: t.ConnectionProps };
 const initial: T = { props: {} };
 const name = Root.displayName ?? '';
 
-export default Dev.describe(name, (e) => {
+export default Dev.describe(name, async (e) => {
+  const create = async (storage: string) => {
+    const peer = Webrtc.peer();
+    const store = Crdt.WebStore.init({
+      storage,
+      network: [], // NB: ensure the local "BroadcastNetworkAdapter" is not used so we actually test WebRTC.
+    });
+    const index = await Crdt.WebStore.index(store);
+    return { peer, store, index } as const;
+  };
+
+  const left = await create(TestDb.EdgeSample.left.name);
+  const right = await create(TestDb.EdgeSample.right.name);
+
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
     const dev = Dev.tools<T>(e, initial);
@@ -23,18 +36,27 @@ export default Dev.describe(name, (e) => {
       });
   });
 
+  e.it('ui:header', (e) => {
+    const dev = Dev.tools<T>(e, initial);
+    dev.header
+      .padding(0)
+      .border(-0.1)
+      .render((e) => <Webrtc.Connector peer={left.peer} />);
+  });
+
   e.it('ui:debug', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
-    dev.TODO();
+
+    dev.button('connect', (e) => left.peer.connect.data(right.peer.id));
+    dev.button('disconnect', (e) => left.peer.disconnect());
   });
 
-  e.it('ui:footer', async (e) => {
+  e.it('ui:footer', (e) => {
     const dev = Dev.tools<T>(e, initial);
-    const state = await dev.state();
-    dev.footer.border(-0.1).render<T>((e) => {
-      const data = e.state;
-      return <Dev.Object name={name} data={data} expand={1} />;
-    });
+    dev.footer
+      .padding(0)
+      .border(-0.1)
+      .render((e) => <Webrtc.Connector peer={right.peer} />);
   });
 });
