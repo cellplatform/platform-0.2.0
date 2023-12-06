45db00fa9338c46731601f3ef9c2c46b984672c2
Phil Cockfield <phil@cockfield.net>
2023-12-06 14:47:29 +1300

    Patch.apply ← pass in a docRef<T>

--- code/ext/ext.lib.automerge/src/Store.Doc/-TEST.Patch.v.ts
+++ code/ext/ext.lib.automerge/src/Store.Doc/-TEST.Patch.v.ts
@@ -19,7 +19,6 @@ describe('Doc.Patch', async () => {
       const doc1 = await generator();
       const doc2 = await generator();
       const events = doc1.events();
-
       let patches: t.Patch[] = [];
       events.changed$.subscribe((e) => (patches = e.patches));
 
@@ -39,7 +38,6 @@ describe('Doc.Patch', async () => {
       const doc1 = await generator();
       const doc2 = await generator();
       const events = doc1.events();
-
       let patches: t.Patch[] = [];
       events.changed$.subscribe((e) => (patches = e.patches));
 
@@ -52,6 +50,28 @@ describe('Doc.Patch', async () => {
       expect(doc2.current).to.eql({ count: 0 });
 
       doc2.change((d) => Doc.Patch.apply(d, patches));
+      expect(doc1.current).to.eql({ count: 1, msg: 'hello' });
+      expect(doc2.current).to.eql({ count: 1, msg: 'hello' });
+
+      events.dispose();
+    });
+
+    it('pass docRef', async () => {
+      const doc1 = await generator();
+      const doc2 = await generator();
+      const events = doc1.events();
+      let patches: t.Patch[] = [];
+      events.changed$.subscribe((e) => (patches = e.patches));
+
+      doc1.change((d) => {
+        d.count++;
+        d.msg = 'hello';
+      });
+      expect(patches.length).to.eql(3);
+      expect(doc1.current).to.eql({ count: 1, msg: 'hello' });
+      expect(doc2.current).to.eql({ count: 0 });
+
+      Doc.Patch.apply(doc2, patches);
       expect(doc1.current).to.eql({ count: 1, msg: 'hello' });
       expect(doc2.current).to.eql({ count: 1, msg: 'hello' });
 
