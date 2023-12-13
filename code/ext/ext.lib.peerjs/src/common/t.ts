df23fa6b07e0b1ac36392f70d9124418af1864c4
Phil Cockfield <phil@cockfield.net>
2023-12-14 11:17:38 +1300

    refactor

--- code/ext/ext.lib.peerjs/src/common/t.ts
+++ code/ext/ext.lib.peerjs/src/common/t.ts
@@ -13,14 +13,7 @@ export type {
   PatchStateEvents,
 } from 'sys.data.json/src/types';
 export type { SpecImport, TestSuiteRunResponse } from 'sys.test.spec/src/types.mjs';
-export type {
-  Disposable,
-  EventBus,
-  Lifecycle,
-  Milliseconds,
-  UntilObservable,
-} from 'sys.types/src/types';
-
+export type { Disposable, EventBus, Lifecycle, Msecs, UntilObservable } from 'sys.types/src/types';
 export type { TimeDelayPromise } from 'sys.util/src/types.mjs';
 
 /**
--- code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/u.Timer.ts
+++ code/ext/ext.lib.peerjs/src/ui/ui.Connector.Model/u.Timer.ts
@@ -1,6 +1,6 @@
 import { Time, type t } from './common';
 
-export function ResetTimer(delay: t.Milliseconds, action: () => void) {
+export function ResetTimer(delay: t.Msecs, action: () => void) {
   let _timer: t.TimeDelayPromise | undefined;
   const clear = () => _timer?.cancel();
   const start = () => {
