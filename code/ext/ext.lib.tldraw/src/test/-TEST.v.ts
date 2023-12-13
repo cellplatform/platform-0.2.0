2934259c9c649fdb2ad49651af34b3a6df71fef8
Phil Cockfield <phil@cockfield.net>
2023-12-13 13:49:24 +1300

    Update -TEST.v.ts

--- code/ext/ext.lib.tldraw/src/test/-TEST.v.ts
+++ code/ext/ext.lib.tldraw/src/test/-TEST.v.ts
@@ -4,14 +4,6 @@ import { TESTS } from '../test.ui/-TestRunner.TESTS';
 /**
  * Test Harness (UI)
  */
-describe('visual specs', () => {
-  it('run', async () => {
-    const { Dev, expect } = await import('../test.ui');
-    const { Specs } = await import('../test.ui/entry.Specs.mjs');
-    const res = await Dev.headless(Specs);
-    expect(res.ok).to.eql(true);
-  });
-});
 
 /**
  * Run tests within CI (server-side).
