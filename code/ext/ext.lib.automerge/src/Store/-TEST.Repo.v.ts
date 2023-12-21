8faa07b7cac375ef42d0b2662b31c58db9885b50
Phil Cockfield <phil@cockfield.net>
2023-12-21 16:58:30 +1300

    Update -TEST.Repo.v.ts

--- code/ext/ext.lib.automerge/src/Store/-TEST.Repo.v.ts
+++ code/ext/ext.lib.automerge/src/Store/-TEST.Repo.v.ts
@@ -11,9 +11,34 @@ describe('Store.repo', () => {
   it('repo.handles', async () => {
     const repo = store.repo;
     expect(repo.handles).to.eql({});
-
     const doc = await generator();
     const keys = Object.keys(repo.handles);
     expect(keys).to.eql([Store.Doc.Uri.id(doc.uri)]);
+  });
+
+  describe('repo.on("delete-document")', () => {
+    it('fires on [repo.delete]', async () => {
+      let fired = 0;
+      const handleDelete = () => fired++;
+      store.repo.on('delete-document', handleDelete);
+
+      const doc = await generator();
+      store.repo.delete(doc.uri);
+      expect(fired).to.eql(1);
+
+      store.repo.off('delete-document', handleDelete);
+    });
+
+    it('does not fire on [doc.handle.delete]', async () => {
+      let fired = 0;
+      const handleDelete = () => fired++;
+      store.repo.on('delete-document', handleDelete);
+
+      const doc = await generator();
+      doc.handle.delete(); // NB: This will  not fire the document event.
+
+      expect(fired).to.eql(0);
+      store.repo.off('delete-document', handleDelete);
+    });
   });
 });
