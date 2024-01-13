90c87d974bbd96f9a5a52f86cfff903512dac3a1
Phil Cockfield <phil@cockfield.net>
2024-01-14 08:51:03 +1300

    rename folder

--- code/ext/ext.lib.automerge/src/test.ui/common.ts
+++ code/ext/ext.lib.automerge/src/test.ui/common.ts
@@ -3,7 +3,7 @@ export { Test, Tree } from 'sys.test.spec';
 export { Dev } from 'sys.ui.react.common';
 export { DevReload } from 'sys.data.indexeddb';
 
-export * from '../Store.Doc';
+export * from '../Doc';
 export * from '../Store.Web';
 export * from '../Store.Web.IndexDb';
 export * from '../ui/common';
