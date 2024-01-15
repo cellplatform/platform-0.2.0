124ee1586a05dae4d7de26f2479d6aae7c924459
Phil Cockfield <phil@cockfield.net>
2024-01-15 16:54:08 +1300

    Sample updates

--- code/compiler.samples/deno.hono/src/deps.ts
+++ code/compiler.samples/deno.hono/src/deps.ts
@@ -1,4 +1,4 @@
 export type * as t from './t.ts';
 
-// export * as dotenv from 'https://deno.land/std@0.212.0/dotenv/mod.ts';
 export { Hono } from 'https://deno.land/x/hono@v3.12.3/mod.ts';
+export { assertEquals } from 'https://deno.land/std@0.212.0/assert/mod.ts';
--- code/compiler.samples/deno.hono/src/main.ts
+++ code/compiler.samples/deno.hono/src/main.ts
@@ -1,10 +1,25 @@
 import { Hono, type t } from './deps.ts';
-const app = new Hono();
 
+/**
+ * Sample: --env flag
+ */
 console.log(`.env: FOO=${Deno.env.get('FOO')}`);
 
+/**
+ * Hono
+ *    Tiny web framework for the edge (Web Standards)
+ *    https://hono.dev/top
+ */
+const app = new Hono();
+
+/**
+ * Routes
+ */
 app.get('/', (c: t.Context) => {
   return c.text('Hello Deno/Hono 👋!!');
 });
 
+/**
+ * Start
+ */
 Deno.serve({ port: 1234 }, app.fetch);
