53826a0a1e58de7c596c982df71affcd4018dcab
Phil Cockfield <phil@cockfield.net>
2024-01-19 17:45:13 +1300

    Update URLs (after CI name/file refactoring)

--- code/spikes/dev.000/src/test.ui/ci.badge.ts
+++ code/spikes/dev.000/src/test.ui/ci.badge.ts
@@ -1,4 +1,4 @@
 export const badge = {
-  image: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml/badge.svg',
-  href: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/node.esm.yml',
+  image: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/ci.node.yml/badge.svg',
+  href: 'https://github.com/cellplatform/platform-0.2.0/actions/workflows/ci.node.yml',
 };
