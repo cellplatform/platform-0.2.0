049f8beae6fadc65109fe88191a02b285e9e0ebc
Phil Cockfield <phil@cockfield.net>
2024-01-21 19:54:08 +1300

    Update test imports and names

--- code/system/sys.fn/src/test.ui/-TEST.ts
+++ code/system/sys.fn/src/test.ui/-TEST.ts
@@ -1,7 +1,7 @@
 import { Pkg } from '../index.pkg.mjs';
 import { expect, Test } from '../test.ui';
 
-export default Test.describe('Module', (e) => {
+export default Test.describe(`Module: ${Pkg.toString()}`, (e) => {
   e.it('has a package ModuleDef', (e) => {
     expect(typeof Pkg.name).to.eql('string');
     expect(typeof Pkg.version).to.eql('string');
