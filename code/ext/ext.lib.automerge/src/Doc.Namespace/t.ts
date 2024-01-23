a4bb40439c95ca51355fbe276f46599e53e7e889
Phil Cockfield <phil@cockfield.net>
2024-01-24 07:51:57 +1300

    refactor

--- code/ext/ext.lib.automerge/src/Doc.Namespace/t.ts
+++ code/ext/ext.lib.automerge/src/Doc.Namespace/t.ts
@@ -2,16 +2,16 @@ import type { t } from '../common';
 
 export type NamespaceMap<N extends string = string> = Record<N, {}>;
 export type NamespaceMapGetLens<R extends {}> = t.LensGetDescendent<R, NamespaceMap>;
+export type Namespace<L extends {}, N extends string = string> = { namespace: N; lens: t.Lens<L> };
 
-export type Namespace<L extends {}, N extends string = string> = {
-  namespace: N;
-  lens: t.Lens<L>;
-};
-
+/**
+ * Manager for working with [Lens] objects attached to a {map}
+ * where the "keys" are the namespace "names".
+ */
 export type NamespaceManager<N extends string = string> = t.Lifecycle & {
   readonly kind: 'crdt:namespace';
   readonly container: Readonly<NamespaceMap<N>>;
-  lens<L extends {}>(namespace: N, initial: L, options?: { type?: string }): t.Lens<L>;
   list<L extends {}>(): Namespace<L, N>[];
+  lens<L extends {}>(namespace: N, initial: L, options?: { type?: string }): t.Lens<L>;
   events(dispose$?: t.UntilObservable): t.LensEvents<t.NamespaceMap<N>>;
 };
