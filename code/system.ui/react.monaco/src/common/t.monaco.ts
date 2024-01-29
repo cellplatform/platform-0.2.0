40e7b523277b82a751d02acec0d89e0effd2f99f
Phil Cockfield <phil@cockfield.net>
2024-01-29 17:00:15 +1300

    types

--- code/system.ui/react.monaco/src/common/t.monaco.ts
+++ code/system.ui/react.monaco/src/common/t.monaco.ts
@@ -4,4 +4,4 @@ import type * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
  * @external
  */
 export type { ISelection, Selection } from 'monaco-editor';
-export { monaco };
+export type { monaco };
