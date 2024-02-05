cdcb51bd3e71f22bf08854a75f4f2a0c9999dd6f
Phil Cockfield <phil@cockfield.net>
2024-02-05 17:59:17 +1300

    make copy icon smaller

--- code/system.ui/react.common/src/ui/PropList/ui.Item/CopyIcon.tsx
+++ code/system.ui/react.common/src/ui/PropList/ui.Item/CopyIcon.tsx
@@ -6,9 +6,9 @@ import { COLORS, css, Icons } from './common';
 export const CopyIcon: React.FC = () => {
   const styles = {
     base: css({
-      Absolute: [-2, -20, null, null],
+      Absolute: [-1, -16, null, null],
       opacity: 0.8,
     }),
   };
-  return <Icons.Copy style={styles.base} color={COLORS.BLUE} size={18} />;
+  return <Icons.Copy style={styles.base} color={COLORS.BLUE} size={14} />;
 };
