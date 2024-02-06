e1d8fedf7d32c9d31e4363bcd32a20d18f88ed1d
Phil Cockfield <phil@cockfield.net>
2024-02-07 12:59:53 +1300

    tmp

--- code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-loaders/_CmdHost.tsx
+++ code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-loaders/_CmdHost.tsx
@@ -5,6 +5,7 @@ import { specs } from './CmdHost.imports';
 export type CmdHostLoaderProps = {
   store: t.Store;
   shared: t.Lens<t.SampleSharedMain>;
+  factory: t.LoadFactory<any>;
   style?: t.CssValue;
 };
 
@@ -12,24 +13,33 @@ export const CmdHostLoader: React.FC<CmdHostLoaderProps> = (props) => {
   const { store, shared } = props;
   const badge = CmdHost.DEFAULTS.badge;
 
-  console.log('CmdHost (loader)', shared); // TEMP 🐷
+  console.log('CmdHost (loader)', shared.toObject()); // TEMP 🐷
 
   /**
    * Render
    */
   const styles = {
-    base: css({ backgroundColor: COLORS.WHITE, display: 'grid' }),
+    base: css({
+      position: 'relative',
+      backgroundColor: COLORS.WHITE,
+      display: 'grid',
+    }),
+    list: css({ Absolute: 0 }),
   };
 
   return (
     <div {...css(styles.base, props.style)}>
       <CmdHost.Stateful
+        style={styles.list}
         pkg={Pkg}
         specs={specs}
         badge={badge}
         mutateUrl={false}
         showDevParam={false}
         commandPlaceholder={'namespace'}
+        onItemClick={(e) => {
+          console.log('onItemClick', e);
+        }}
       />
     </div>
   );
