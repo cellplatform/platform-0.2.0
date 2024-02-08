935a3e6d1d1c48e8acce250a3e6946e0fcfecfc7
Phil Cockfield <phil@cockfield.net>
2024-02-08 17:27:00 +1300

    delete ModuleHost (initial copy, abandon this idea)

--- code/system.ui/react.common/src/ui/ModuleHost.Stateful/t.ts
+++
@@ -1,5 +0,0 @@
-import type { t } from './common';
-
-export type ModuleHostStatefulProps<T = t.SpecModule> = t.ModuleHostProps<T> & {
-  mutateUrl?: boolean;
-};
--- code/system.ui/react.common/src/ui/ModuleHost.Stateful/u.ts
+++
@@ -1,2 +0,0 @@
-export * from './u.Url';
-export * from './u.Wrangle';
