8280b41ad1fce11411931faa15d59b0ea74cc4dd
Phil Cockfield <phil@cockfield.net>
2024-02-08 18:32:39 +1300

    tmpl: break out /ui → <View>

--- code/tmpl/ui.Component/Root.tsx
+++ code/tmpl/ui.Component/Root.tsx
@@ -1,23 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { Color, COLORS, css, DEFAULTS, FC, rx, type t } from './common';
-
-const View: React.FC<t.RootProps> = (props) => {
-  /**
-   * Render
-   */
-  const styles = {
-    base: css({
-      backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
-      padding: 5,
-    }),
-  };
-
-  return (
-    <div {...css(styles.base, props.style)}>
-      <div>{`🐷 ${Root.displayName}`}</div>
-    </div>
-  );
-};
+import { View } from './ui';
 
 /**
  * Export
