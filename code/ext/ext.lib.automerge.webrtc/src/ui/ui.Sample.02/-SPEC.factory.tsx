26978304bb340e826fcb0348ea03db007badef4a
Phil Cockfield <phil@cockfield.net>
2024-02-15 08:42:20 +1300

    refactor

--- code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-SPEC.factory.tsx
+++ code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-SPEC.factory.tsx
@@ -1,4 +1,4 @@
-import { ModuleLoader, type t } from '../common';
+import { ModuleLoader, type t } from './common';
 
 type TName = t.SampleFactoryTypename;
 
@@ -8,14 +8,19 @@ type TName = t.SampleFactoryTypename;
 export const loadFactory: t.LoadFactory<TName> = async (e) => {
   const { typename, store, docuri, shared } = e;
 
+  /**
+   * TODO 🐷
+   * - collapse to single "factory" generator, which takes a {ctx} type
+   */
+
   const loader = ModuleLoader.factory<TName>(async (e) => {
     if (e.name === 'Auth') {
-      const { AuthLoader } = await import('./Auth');
+      const { AuthLoader } = await import('./-loaders/Auth');
       return <AuthLoader store={store} docuri={docuri} />;
     }
 
     if (e.name === 'CodeEditor') {
-      const { CodeEditorLoader } = await import('./CodeEditor'); // NB: dynamic code-splitting here.
+      const { CodeEditorLoader } = await import('./-loaders/CodeEditor'); // NB: dynamic code-splitting here.
       return <CodeEditorLoader store={store} docuri={docuri} />;
     }
 
@@ -27,7 +32,7 @@ export const loadFactory: t.LoadFactory<TName> = async (e) => {
     }
 
     if (e.name === 'CmdHost') {
-      const { CmdHostLoader } = await import('./CmdHost');
+      const { CmdHostLoader } = await import('./-loaders/CmdHost');
       return <CmdHostLoader store={store} shared={shared} factory={loadFactory} />;
     }
 
