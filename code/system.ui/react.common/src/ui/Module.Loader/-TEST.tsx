b868843cfb6bfb1e9510515e0d1213ca2d9b110e
Phil Cockfield <phil@cockfield.net>
2024-02-14 19:19:59 +1300

    refactor (naming)

--- code/system.ui/react.common/src/ui/Module.Loader/-TEST.tsx
+++ code/system.ui/react.common/src/ui/Module.Loader/-TEST.tsx
@@ -25,8 +25,8 @@ export default Test.describe('Module.Loader', (e) => {
 
     e.it('factory', async (e) => {
       const { fn, invoked } = testFactory();
-      const res = ModuleLoader.factory(fn);
-      expect(res.factory).to.equal(fn);
+      const loader = ModuleLoader.factory(fn);
+      expect(loader.factory).to.equal(fn);
       expect(invoked.length).to.eql(0);
 
       type A = t.ModuleLoaderFactoryArgs<TName>;
@@ -43,8 +43,8 @@ export default Test.describe('Module.Loader', (e) => {
         is: { front: false, back: true, light: true, dark: false },
       };
 
-      const res1 = await res.factory(args1);
-      const res2 = await res.factory(args2);
+      const res1 = await loader.factory(args1);
+      const res2 = await loader.factory(args2);
       expect(isValidElement(res1)).to.eql(true);
       expect(res2).to.eql(null);
 
@@ -55,10 +55,10 @@ export default Test.describe('Module.Loader', (e) => {
 
     e.it('factory.render( "typename" )', (e) => {
       const { fn, assertModuleLoader } = testFactory();
-      const factory = ModuleLoader.factory(fn);
-      const res1 = factory.render('foo');
-      const res2 = factory.render('bar');
-      const res3 = factory.render('404');
+      const loader = ModuleLoader.factory(fn);
+      const res1 = loader.render('foo');
+      const res2 = loader.render('bar');
+      const res3 = loader.render('404');
 
       assertModuleLoader(res1, 'foo');
       assertModuleLoader(res2, 'bar');
