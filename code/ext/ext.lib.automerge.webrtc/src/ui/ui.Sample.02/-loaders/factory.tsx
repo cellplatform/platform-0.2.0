786a579d4e8f506108c3555e4fe6810cfb9f9f76
Phil Cockfield <phil@cockfield.net>
2024-02-14 19:19:52 +1300

    refactor: clean up call-site

--- code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-loaders/factory.tsx
+++ code/ext/ext.lib.automerge.webrtc/src/ui/ui.Sample.02/-loaders/factory.tsx
@@ -1,46 +1,43 @@
 import { ModuleLoader, type t } from '../common';
 
+type TName = t.SampleFactoryTypename;
+
 /**
  * A factory for code-split (dynamicly loaded) ESM module.
  */
-export const loadFactory: t.LoadFactory<t.SampleFactoryTypename> = async (e) => {
+export const loadFactory: t.LoadFactory<TName> = async (e) => {
   const { typename, store, docuri, shared } = e;
 
-  type TName = t.SampleFactoryTypename;
-  const factory = ModuleLoader.factory<TName>(async (e) => {
+  const loader = ModuleLoader.factory<TName>(async (e) => {
     if (e.name === 'Auth') {
       const { AuthLoader } = await import('./Auth');
       return <AuthLoader store={store} docuri={docuri} />;
     }
+
+    if (e.name === 'CodeEditor') {
+      const { CodeEditorLoader } = await import('./CodeEditor'); // NB: dynamic code-splitting here.
+      return <CodeEditorLoader store={store} docuri={docuri} />;
+    }
+
+    if (e.name === 'DiagramEditor') {
+      // @ts-ignore
+      await import('@tldraw/tldraw/tldraw.css');
+      const { Canvas } = await import('ext.lib.tldraw');
+      return <Canvas style={{ opacity: 0.9 }} />;
+    }
+
+    if (e.name === 'CmdHost') {
+      const { CmdHostLoader } = await import('./CmdHost');
+      return <CmdHostLoader store={store} shared={shared} factory={loadFactory} />;
+    }
+
+    if (e.name === 'ModuleNamespace') {
+      const { ModuleNamespace } = await import('sys.ui.react.common');
+      return <ModuleNamespace />;
+    }
+
     return null;
   });
 
-  if (typename === 'CodeEditor') {
-    const { CodeEditorLoader } = await import('./CodeEditor'); // NB: dynamic code-splitting here.
-    return <CodeEditorLoader store={store} docuri={docuri} />;
-  }
-
-  if (typename === 'DiagramEditor') {
-    // @ts-ignore
-    await import('@tldraw/tldraw/tldraw.css');
-    const { Canvas } = await import('ext.lib.tldraw');
-    return <Canvas style={{ opacity: 0.9 }} />;
-  }
-
-  if (typename === 'Auth') {
-    // return factory.render(typename);
-    return factory.type('Auth').render();
-  }
-
-  if (typename === 'CmdHost') {
-    const { CmdHostLoader } = await import('./CmdHost');
-    return <CmdHostLoader store={store} shared={shared} factory={loadFactory} />;
-  }
-
-  if (typename === 'ModuleNamespace') {
-    const { ModuleNamespace } = await import('sys.ui.react.common');
-    return <ModuleNamespace />;
-  }
-
-  return;
+  return loader.render(typename);
 };
