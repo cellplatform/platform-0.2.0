99665d1f0bd2a2d1972e919fb5ab7264db81cc65
Phil Cockfield <phil@cockfield.net>
2024-02-17 11:35:26 +1300

    tmp rename

--- code/system.ui/react.common/src/ui.dev/DevTools/_Helpers.theme.ts
+++ code/system.ui/react.common/src/ui.dev/DevTools/_Helpers.theme.ts
@@ -3,15 +3,20 @@ import { COLORS, type t } from '../common';
 /**
  * Helpers for working with common themes within the harness.
  */
-export const theme = {
+export const Theme = {
+  is(theme: t.CommonTheme) {
+    return { dark: theme === 'Dark', light: theme === 'Light' } as const;
+  },
+
   /**
    * Adjust the theme of the DevHarness.
    */
-  background(ctx: t.DevCtx, theme?: t.CommonTheme) {
-    if (theme === 'Light') {
+  background(ctx: t.DevCtx, value: t.CommonTheme = 'Light') {
+    const is = Theme.is(value);
+    if (value === 'Light') {
       ctx.host.backgroundColor(null).tracelineColor(null);
     }
-    if (theme === 'Dark') {
+    if (value === 'Dark') {
       ctx.host.backgroundColor(COLORS.DARK).tracelineColor(0.08);
     }
   },
