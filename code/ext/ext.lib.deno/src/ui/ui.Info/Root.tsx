dc95746c243b9be418db540c6b00ba6d0957b7b2
Phil Cockfield <phil@cockfield.net>
2024-02-17 14:26:59 +1300

    stub in module

---
+++ code/ext/ext.lib.deno/src/ui/ui.Info/Root.tsx
@@ -0,0 +1,63 @@
+import { DEFAULTS, FC, PropList, type t } from './common';
+import { Field } from './field';
+
+export type InfoProps = {
+  title?: t.PropListProps['title'];
+  width?: t.PropListProps['width'];
+  fields?: t.InfoField[];
+  data?: t.InfoData;
+  margin?: t.CssEdgesInput;
+  card?: boolean;
+  flipped?: boolean;
+  style?: t.CssValue;
+};
+
+/**
+ * Component
+ */
+const View: React.FC<InfoProps> = (props) => {
+  const { fields = DEFAULTS.fields.default, data = {} } = props;
+
+  const items = PropList.builder<t.InfoField>()
+    .field('Module', () => Field.module())
+    .field('Module.Verify', () => Field.moduleVerify())
+    .field('Component', () => Field.component(data.component))
+    .items(fields);
+
+  return (
+    <PropList
+      title={Wrangle.title(props)}
+      items={items}
+      width={props.width ?? { min: 230 }}
+      defaults={{ clipboard: false }}
+      card={props.card}
+      flipped={props.flipped}
+      padding={props.card ? [20, 25, 30, 25] : undefined}
+      margin={props.margin}
+      style={props.style}
+    />
+  );
+};
+
+/**
+ * Helpers
+ */
+const Wrangle = {
+  title(props: InfoProps) {
+    const title = PropList.Wrangle.title(props.title);
+    if (!title.margin && props.card) title.margin = [0, 0, 15, 0];
+    return title;
+  },
+};
+
+/**
+ * Export
+ */
+type Fields = {
+  DEFAULTS: typeof DEFAULTS;
+};
+export const Info = FC.decorate<InfoProps, Fields>(
+  View,
+  { DEFAULTS },
+  { displayName: DEFAULTS.displayName },
+);
