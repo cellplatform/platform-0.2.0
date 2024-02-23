f196ccf998828f9847b22a207c609b2d540b4821
Phil Cockfield <phil@cockfield.net>
2024-02-23 21:28:46 +1300

    Update -SPEC.sample.ts

--- code/ext/ext.lib.deno/src/ui/ui.Sample.01/-SPEC.sample.ts
+++ code/ext/ext.lib.deno/src/ui/ui.Sample.01/-SPEC.sample.ts
@@ -1,13 +1,5 @@
 export const code = `
-const handler = (req: Request): Response => {
-  const body = 'hello world 👋';
-  return new Response(body, { status: 200 });
-};
-
-console.log('HTTP server running at: http://localhost:8080/');
-
-const port = 8080;
-Deno.serve({ port }, handler);    
+Deno.serve((req) => new Response("👋 Hello!"));
 `.substring(1);
 
 export const SAMPLE = { code } as const;
