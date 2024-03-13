287ce821fbde58f537244a70dbf6940decc700d0
Phil Cockfield <phil@cockfield.net>
2024-03-14 10:58:15 +1300

    Update libs.ts

--- code/system.ui/dom/src/common/libs.ts
+++ code/system.ui/dom/src/common/libs.ts
@@ -1,9 +1,10 @@
 /**
  * @external
  */
+export { Subject } from 'rxjs';
+
 import { clone, uniq } from 'ramda';
 export const R = { clone, uniq } as const;
-export { Subject } from 'rxjs';
 
 /**
  * @system
