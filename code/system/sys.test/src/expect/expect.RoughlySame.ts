da5c30438a1f5507a828d0d8bdb8e894be193adb
Phil Cockfield <phil@cockfield.net>
2024-03-19 15:14:08 +1300

    loosen input params

--- code/system/sys.test/src/expect/expect.RoughlySame.ts
+++ code/system/sys.test/src/expect/expect.RoughlySame.ts
@@ -4,7 +4,13 @@ import { expect } from './expect';
 /**
  * Determines if two numbers are approximately equal within a specified tolerance.
  */
-export function areRoughlySame(left: number, right: number, tolerance: t.Percent): boolean {
+export function areRoughlySame(
+  left: number | undefined,
+  right: number | undefined,
+  tolerance: t.Percent,
+): boolean {
+  if (typeof left !== 'number') throw new Error('The left number was undefined');
+  if (typeof right !== 'number') throw new Error('The right number was undefined');
   const average = (left + right) / 2;
   const difference = Math.abs(left - right);
   const allowedDifference = average * tolerance;
@@ -16,11 +22,14 @@ export function areRoughlySame(left: number, right: number, tolerance: t.Percent
  * Throws an error with a custom message if the assertion fails.
  */
 export function expectRoughlySame(
-  left: number,
-  right: number,
+  left: number | undefined,
+  right: number | undefined,
   tolerance: t.Percent,
   message?: string,
 ) {
   const res = areRoughlySame(left, right, tolerance);
-  expect(res).to.eql(true, message);
+  const within = `within ${tolerance * 100}%`;
+  const suffix = `should be roughly the same numbers ${left}, ${right} (${within})`;
+  let msg = `${message ?? ''} - ${suffix}`;
+  expect(res).to.eql(true, msg);
 }
