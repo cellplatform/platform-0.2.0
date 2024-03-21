283df1a67682c9b000b620f4b8379c468b867ea0
Phil Cockfield <phil@cockfield.net>
2024-03-22 10:33:58 +1300

    instrument with: Common Theme

--- code/system/sys.data.indexeddb/src/ui/ui.Dev.Reload/t.ts
+++ code/system/sys.data.indexeddb/src/ui/ui.Dev.Reload/t.ts
@@ -6,6 +6,7 @@ import type { t } from './common';
 export type DevReloadProps = {
   isCloseable?: boolean;
   isReloadRequired?: boolean;
+  theme?: t.CommonTheme;
   style?: t.CssValue;
   onCloseClick?: () => void;
   onReloadClick?: () => void;
