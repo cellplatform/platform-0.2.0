54daa44047c5da0d06ca882409299735920570bd
Phil Cockfield <phil@cockfield.net>
2024-03-22 09:35:09 +1300

    Sort order

--- code/system/sys.types/src/types.sys/index.ts
+++ code/system/sys.types/src/types.sys/index.ts
@@ -9,6 +9,7 @@ export type * from './t.hash';
 export type * from './t.immutable';
 export type * from './t.io';
 export type * from './t.json';
+export type * from './t.list';
 export type * from './t.module';
 export type * from './t.npm';
 export type * from './t.number';
