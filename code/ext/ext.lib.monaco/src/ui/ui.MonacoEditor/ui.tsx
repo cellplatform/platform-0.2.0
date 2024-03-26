358537a55b0545048e6747aebeabec68ded506b2
Phil Cockfield <phil@cockfield.net>
2024-03-27 12:41:09 +1300

    refactor view module

---
+++ code/ext/ext.lib.monaco/src/ui/ui.MonacoEditor/ui.tsx
@@ -0,0 +1,75 @@
+import type { OnChange, OnMount } from '@monaco-editor/react';
+
+import EditorReact from '@monaco-editor/react';
+import { useEffect, useRef } from 'react';
+import { DEFAULTS, Wrangle, css, t } from './common';
+import { Theme } from './u.Theme';
+
+export const View: React.FC<t.MonacoEditorProps> = (props) => {
+  const { text, language = DEFAULTS.language, tabSize = DEFAULTS.tabSize, placeholder } = props;
+  const theme = Theme.toName(props.theme);
+
+  const monacoRef = useRef<t.Monaco>();
+  const editorRef = useRef<t.MonacoCodeEditor>();
+  const editor = editorRef.current;
+
+  /**
+   * [Lifecycle]
+   */
+  useEffect(() => {
+    if (!editor) return;
+    if (text !== editor.getValue()) editor.setValue(text ?? '');
+  }, [text]);
+
+  useEffect(() => {
+    editorRef.current?.getModel()?.updateOptions({ tabSize });
+  }, [tabSize]);
+
+  useEffect(() => {
+    return () => {
+      const editor = editorRef.current!;
+      const monaco = monacoRef.current!;
+      props.onDispose?.({ editor, monaco });
+    };
+  }, []);
+
+  /**
+   * [Handlers]
+   */
+  const handleEditorDidMount: OnMount = (ed, monaco) => {
+    Theme.init(monaco);
+    monacoRef.current = monaco;
+    const editor = (editorRef.current = ed as unknown as t.MonacoCodeEditor);
+    editor.updateOptions({ theme });
+    editor.getModel()?.updateOptions({ tabSize });
+    if (props.focusOnLoad) editor.focus();
+    props.onReady?.({ editor, monaco });
+  };
+
+  const handleChange: OnChange = (text = '', event) => {
+    props.onChange?.({ text, event });
+  };
+
+  /**
+   * [Render]
+   */
+  const styles = {
+    base: css({ position: 'relative' }),
+    inner: css({ Absolute: 0 }),
+  };
+
+  return (
+    <div {...css(styles.base, props.style)} className={Wrangle.editorClassName(editor)}>
+      <div {...styles.inner}>
+        <EditorReact
+          defaultLanguage={language}
+          language={language}
+          defaultValue={text}
+          theme={theme}
+          onMount={handleEditorDidMount}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
