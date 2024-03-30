39f832bade7b7aa7de8d62f9825cc0cfce98b070
Phil Cockfield <phil@cockfield.net>
2024-03-30 17:42:32 +1300

    green on copy (clipboard)

--- code/ext/ext.lib.automerge/src/ui/ui.History.Grid/ui.MonoHash.tsx
+++ code/ext/ext.lib.automerge/src/ui/ui.History.Grid/ui.MonoHash.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Color, DEFAULTS, Hash, Time, css, type t } from './common';
+import { Button, COLORS, Color, DEFAULTS, Hash, Time, css, type t } from './common';
 
 export type MonoHashProps = {
   hash?: string;
@@ -11,15 +11,15 @@ export type MonoHashProps = {
 export const MonoHash: React.FC<MonoHashProps> = (props) => {
   const { hash = '', theme, length = DEFAULTS.hash.length } = props;
   const short = Hash.shorten(hash, [0, length]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState<JSX.Element | undefined>();
 
   /**
    * Handlers
    */
   const handleClick = () => {
     navigator.clipboard.writeText(hash);
-    setMessage('Copied');
-    Time.delay(2000, () => setMessage(''));
+    setMessage(<div {...styles.copied}>{'Copied'}</div>);
+    Time.delay(2000, () => setMessage(undefined));
   };
 
   /**
@@ -30,6 +30,9 @@ export const MonoHash: React.FC<MonoHashProps> = (props) => {
     base: css({ color, Flex: 'x-center-center' }),
     mono: css(DEFAULTS.mono),
     pound: css({ color, opacity: 0.4, marginRight: 3 }),
+    copied: css({
+      color: COLORS.GREEN,
+    }),
   };
 
   return (
