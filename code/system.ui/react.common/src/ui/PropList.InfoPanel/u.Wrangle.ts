228bc69ef403d44e51a5d9ee299577ec6b15b0c6
Phil Cockfield <phil@cockfield.net>
2024-04-04 13:17:19 +1300

    Update u.Wrangle.ts

--- code/system.ui/react.common/src/ui/PropList.InfoPanel/u.Wrangle.ts
+++ code/system.ui/react.common/src/ui/PropList.InfoPanel/u.Wrangle.ts
@@ -8,7 +8,6 @@ export const Wrangle = {
   title(props: t.CommonInfoProps) {
     const title = PropListWrangle.title(props.title);
     if (!title.margin && props.card) title.margin = [0, 0, 15, 0];
-    console.log('title|||||', title);
     return title;
   },
 } as const;
