cce0265d70b49d3b655e7c7cca9ccfaa2a81f9c1
Phil Cockfield <phil@cockfield.net>
2024-04-04 13:17:14 +1300

    Is.methods

--- code/system/sys.net.http/src/http/-TEST.v.ts
+++ code/system/sys.net.http/src/http/-TEST.v.ts
@@ -2,13 +2,20 @@ import { Http } from '.';
 import { describe, expect, it } from '../test';
 
 describe('Http', () => {
-  describe('Http.Is', () => {
+  describe('Http.Is (flags)', () => {
     it('Is.nativeHeaders', () => {
       const test = (input: any, expected: boolean) => {
-        const res = Http.Is.nativeHeaders(input);
-        expect(res).to.eql(expected, input);
+        expect(Http.Is.nativeHeaders(input)).to.eql(expected, input);
       };
       test(new Headers(), true);
+      [null, 0, [], {}, true, ''].forEach((v) => test(v, false));
+    });
+
+    it('Is.methods', async () => {
+      const test = (input: any, expected: boolean) => {
+        expect(Http.Is.methods(input)).to.eql(expected, input);
+      };
+      test(Http.methods(), true);
       [null, 0, [], {}, true, ''].forEach((v) => test(v, false));
     });
   });
