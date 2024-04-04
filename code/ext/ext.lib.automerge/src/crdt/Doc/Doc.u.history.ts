1d48c5cb1dff6cbf0c846f609844b6c0bb762b1a
Phil Cockfield <phil@cockfield.net>
2024-04-05 12:31:36 +1300

    undefined guard

--- code/ext/ext.lib.automerge/src/crdt/Doc/Doc.u.history.ts
+++ code/ext/ext.lib.automerge/src/crdt/Doc/Doc.u.history.ts
@@ -62,7 +62,7 @@ export function history<T>(doc?: t.DocRef<T>): t.DocHistory<T> {
  * Retrieve the list of hashes for the current HEAD.
  */
 export function heads<T>(doc: t.DocRef<T> | undefined): t.HashString[] {
-  return doc ? A.getHeads(doc.current as A.Doc<T>) : [];
+  return doc?.current ? A.getHeads(doc.current as A.Doc<T>) : [];
 }
 
 /**
