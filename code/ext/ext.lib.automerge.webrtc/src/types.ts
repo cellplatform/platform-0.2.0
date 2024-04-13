4ebbda7babff6d783860d60b0b9ce65988e619b0
Phil Cockfield <phil@cockfield.net>
2024-04-13 18:17:28 +1200

    rename module

--- code/ext/ext.lib.automerge.webrtc/src/types.ts
+++ code/ext/ext.lib.automerge.webrtc/src/types.ts
@@ -4,4 +4,4 @@ export type * from './ui/ui.Info/t';
 export type * from './ui/ui.NetworkConnection/t';
 export type * from './ui/ui.PeerRepoList/t';
 
-export type * from './ui/ui.Sample.CmdHost/t';
+export type * from './ui/ui.CmdHosts/t';
