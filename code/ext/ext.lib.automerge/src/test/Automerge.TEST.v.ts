7ca2f14678a826228e59fe009da701220a81b354
Phil Cockfield <phil@cockfield.net>
2024-04-23 15:06:37 +1200

    update refs

--- code/ext/ext.lib.automerge/src/test/Automerge.TEST.v.ts
+++ code/ext/ext.lib.automerge/src/test/Automerge.TEST.v.ts
@@ -4,22 +4,25 @@ import { A, describe, expect, it } from '../test';
  * Tests of the Automerge library itself.
  */
 describe('Automerge', () => {
+  type D = { count: number };
+
   /**
    * Hard coded byte array hack.
    * https://automerge.org/docs/cookbook/modeling-data/#setting-up-an-initial-document-structure
    */
-  it('hard coded byte array hack', () => {
-    type TDoc = { count: number };
-    let doc1 = A.change(A.init<TDoc>(), (d) => (d.count = 123));
-    const bytes = A.save(doc1);
+  describe('hard coded byte array hack', () => {
+    it('init → Uint8Array → merge', () => {
+      let doc1 = A.change(A.init<D>(), (d) => (d.count = 123));
+      const bytes = A.save(doc1);
 
-    let doc2 = A.load<TDoc>(bytes);
-    expect(doc1).to.eql({ count: 123 });
-    expect(doc2).to.eql({ count: 123 });
+      let doc2 = A.load<D>(bytes);
+      expect(doc1).to.eql({ count: 123 });
+      expect(doc2).to.eql({ count: 123 });
 
-    doc1 = A.change(doc1, (d) => (d.count = 456));
-    doc2 = A.merge(doc2, doc1); // NB: merged two documents that were independently initialized.
-    expect(doc1).to.eql({ count: 456 });
-    expect(doc2).to.eql({ count: 456 });
+      doc1 = A.change(doc1, (d) => (d.count = 456));
+      doc2 = A.merge(doc2, doc1); // NB: merged two documents that were independently initialized.
+      expect(doc1).to.eql({ count: 456 });
+      expect(doc2).to.eql({ count: 456 });
+    });
   });
 });
--- code/ext/ext.lib.tldraw/src/index.pkg.mts
+++ code/ext/ext.lib.tldraw/src/index.pkg.mts
@@ -13,7 +13,7 @@ export const Pkg: ModuleDef = {
   name: 'ext.lib.tldraw',
   version: '0.0.0',
   dependencies: {
-    '@tldraw/tldraw': '2.0.1',
+    '@tldraw/tldraw': '2.0.2',
     'ext.lib.automerge': '0.0.0',
     'automerge-tldraw': '0.2.0',
     'react': '18.2.0',
