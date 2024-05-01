fac01dfc2c7c68b50ad7f234b7711e5ea4c07588
Phil Cockfield <phil@cockfield.net>
2024-05-02 09:16:15 +1200

    remote connected event

--- code/ext/ext.lib.automerge.webrtc/src/Store.Network/t.Shared.Events.ts
+++ code/ext/ext.lib.automerge.webrtc/src/Store.Network/t.Shared.Events.ts
@@ -6,18 +6,34 @@ import type { t } from './common';
 export type CrdtSharedEvents = t.Lifecycle & {
   readonly $: t.Observable<t.CrdtSharedEvent>;
   readonly changed$: t.Observable<t.CrdtSharedChanged>;
+  readonly remoteConnected$: t.Observable<t.CrdtSharedRemoteConnected>;
 };
 
 /**
  * Events
  */
-export type CrdtSharedEvent = CrdtSharedChangedEvent;
+export type CrdtSharedEvent = CrdtSharedChangedEvent | CrdtSharedRemoteConnectedEvent;
 
 /**
  * Fired when the shared doc changes.
  */
 export type CrdtSharedChangedEvent = {
-  type: 'crdt:webrtc:shared/Changed';
+  type: 'crdt:net:shared/Changed';
   payload: t.CrdtSharedChanged;
 };
-export type CrdtSharedChanged = t.DocChanged<t.CrdtShared>;
+export type CrdtSharedChanged = {
+  uri: t.UriString;
+  before: t.CrdtShared;
+  after: t.CrdtShared;
+};
+
+/**
+ * Ephemeral messages.
+ */
+export type CrdtSharedRemoteConnectedEvent = {
+  type: 'crdt:net:shared/RemoteConnected';
+  payload: CrdtSharedRemoteConnected;
+};
+export type CrdtSharedRemoteConnected = {
+  shared: { uri: t.UriString };
+};
