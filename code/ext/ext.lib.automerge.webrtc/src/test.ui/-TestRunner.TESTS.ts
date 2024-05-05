b9d0b2254cc5cefef420bb8a612e10cfc77b25de
Phil Cockfield <phil@cockfield.net>
2024-05-06 11:36:39 +1200

    reorder

--- code/ext/ext.lib.automerge.webrtc/src/test.ui/-TestRunner.TESTS.ts
+++ code/ext/ext.lib.automerge.webrtc/src/test.ui/-TestRunner.TESTS.ts
@@ -1,11 +1,12 @@
 export const TESTS = {
   get all() {
     return [
-      import('./-TEST'),
-      import('../Store.Network/-TEST'),
-
       'Integration Tests',
       import('../Store.Network/-TEST.integration'),
+
+      'Unit Tests',
+      import('./-TEST'),
+      import('../Store.Network/-TEST'),
     ];
   },
 };
