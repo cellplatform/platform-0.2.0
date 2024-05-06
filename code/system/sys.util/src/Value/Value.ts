9bc00a7f9eb625b1a947aa07b6150a4314b4e0b8
Phil Cockfield <phil@cockfield.net>
2024-05-07 07:26:00 +1200

    rename

--- code/system/sys.util/src/Value/Value.ts
+++ code/system/sys.util/src/Value/Value.ts
@@ -1,13 +1,11 @@
+import { Is } from '../Is';
 import * as Array from './Value.Array';
 import * as Hash from './Value.Hash';
 import * as Math from './Value.Math';
+import * as Object from './Value.Object';
 import * as Random from './Value.Random';
 import * as To from './Value.To';
-import * as Util from './Value.Util';
-
-import { Is } from '../Is';
-
-import * as Object from './Value.Object';
+import * as Util from './Value.u';
 
 /**
  * Value conversion and interpretation helpers.
@@ -19,6 +17,6 @@ export const Value = {
   ...Array,
   ...Math,
   ...Random,
-  ...Util,
   ...Hash,
+  ...Util,
 };
