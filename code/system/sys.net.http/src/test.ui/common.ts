1bd4f4bd863555f444cd9f381e44f126164fcbfe
Phil Cockfield <phil@cockfield.net>
2024-05-10 12:02:00 +1200

    remove module refs to: sys.fs.indexeddb

--- code/system/sys.net.http/src/test.ui/common.ts
+++ code/system/sys.net.http/src/test.ui/common.ts
@@ -1,7 +1,6 @@
 /**
  * @libs
  */
-export { TestFilesystem } from 'sys.fs.indexeddb';
 export { expect, expectError } from 'sys.test';
 export { Test, Tree } from 'sys.test.spec';
 export { Dev } from 'sys.ui.react.common';
