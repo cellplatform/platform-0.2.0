a5ebc037e720516f58cd62ec9c06d036ba63bb24
Phil Cockfield <phil@cockfield.net>
2024-05-10 13:25:05 +1200

    Update libs.ts

--- code/sys.ui/react.dev/src/common/libs.ts
+++ code/sys.ui/react.dev/src/common/libs.ts
@@ -1,13 +1,13 @@
 export { ErrorBoundary } from 'react-error-boundary';
-import { mergeDeepRight, clone, equals, prop, sortBy } from 'ramda';
+import { clone, equals, mergeDeepRight, prop, sortBy } from 'ramda';
 export const R = { mergeDeepRight, clone, equals, prop, sortBy } as const;
 
 /**
  * @system
  */
-export { rx, Time, slug, Path, asArray, maybeWait } from 'sys.util';
-export { FC, useMouse } from 'sys.ui.react.util';
-export { css, Color } from 'sys.ui.react.css';
+export { Fuzzy } from 'sys.data.text';
 export { Test } from 'sys.test.spec';
 export { Keyboard } from 'sys.ui.dom';
-export { Fuzzy } from 'sys.data.text';
+export { Color, css } from 'sys.ui.react.css';
+export { FC, useMouse } from 'sys.ui.react.util';
+export { Path, Time, asArray, maybeWait, rx, slug } from 'sys.util';
