650076d0aa57d85b1aaab59922621bf95fd60fcb
Phil Cockfield <phil@cockfield.net>
2024-05-17 14:28:30 +1200

    update spec

--- code/sys.ui/react.common/src/ui/PropList.InfoPanel/-SPEC.tsx
+++ code/sys.ui/react.common/src/ui/PropList.InfoPanel/-SPEC.tsx
@@ -1,5 +1,5 @@
 import { css, Dev, Pkg } from '../../test.ui';
-import { Info } from './-SPEC.ui.Info';
+import { Info, DEFAULTS } from './-SPEC.ui.Info';
 import type * as t from './-SPEC.t';
 
 type P = t.InfoProps;
@@ -12,16 +12,24 @@ const initial: T = { props: {} };
 const name = 'PropList.InfoCommon';
 
 export default Dev.describe(name, (e) => {
-  type LocalStore = Pick<P, 'theme'>;
+  type LocalStore = Pick<P, 'theme' | 'fields' | 'margin'>;
   const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
-  const local = localstore.object({ theme: undefined });
+  const local = localstore.object({
+    theme: 'Dark',
+    fields: DEFAULTS.fields.default,
+    margin: 10,
+  });
 
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
     const dev = Dev.tools<T>(e, initial);
 
     const state = await ctx.state<T>(initial);
-    await state.change((d) => {});
+    await state.change((d) => {
+      d.props.fields = local.fields;
+      d.props.theme = local.theme;
+      d.props.margin = local.margin;
+    });
 
     ctx.debug.width(330);
     ctx.subject
@@ -38,11 +46,57 @@ export default Dev.describe(name, (e) => {
   e.it('ui:debug', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
-    const link = Dev.Link.pkg(Pkg, dev);
-    dev.TODO();
+
+    dev.section('Fields', (dev) => {
+      const update = (fields: t.InfoField[]) => {
+        dev.change((d) => (d.props.fields = fields));
+        local.fields = fields?.length === 0 ? undefined : fields;
+      };
+
+      dev.row((e) => {
+        const props = e.state.props;
+        return (
+          <Dev.FieldSelector
+            all={DEFAULTS.fields.all}
+            selected={props.fields}
+            onClick={(ev) => {
+              const fields =
+                ev.action === 'Reset:Default'
+                  ? DEFAULTS.fields.default
+                  : (ev.next as t.InfoField[]);
+              update(fields);
+            }}
+          />
+        );
+      });
+
+      dev.hr(0, 5);
+
+      const common = (label: string, fields: t.InfoField[]) => {
+        dev.button(label, (e) => update(fields));
+      };
+
+      common('single', ['Module']);
+      common('all', DEFAULTS.fields.all);
+    });
+
+    dev.hr(5, 20);
 
     dev.section('Properties', (dev) => {
       Dev.Theme.switch(dev, ['props', 'theme'], (next) => (local.theme = next));
+
+      dev.boolean((btn) => {
+        const value = (state: T) => state.props.margin ?? 0;
+        btn
+          .label((e) => `margin`)
+          .value((e) => value(e.state) !== 0)
+          .onClick((e) =>
+            e.change((d) => {
+              const current = d.props.margin ?? 0;
+              local.margin = d.props.margin = current === 0 ? 10 : 0;
+            }),
+          );
+      });
     });
   });
 
