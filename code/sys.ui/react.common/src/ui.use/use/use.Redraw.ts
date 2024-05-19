4cf35b1149c3dabec069473cb66cb134e145ca28
Phil Cockfield <phil@cockfield.net>
2024-05-20 10:30:10 +1200

    useRedraw: expand to take optional observable

--- code/sys.ui/react.common/src/ui.use/use/use.Redraw.ts
+++ code/sys.ui/react.common/src/ui.use/use/use.Redraw.ts
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { rx, type t } from '../common';
 
 /**
  * HOOK: provide simple counter incrementing component "redraw" API.
  */
-export function useRedraw() {
+export function useRedraw(redraw$?: t.Observable<any>) {
   const [, setCount] = useState(0);
-  return () => setCount((prev) => prev + 1);
+  const redraw = () => setCount((n) => n + 1);
+
+  useEffect(() => {
+    const life = rx.disposable();
+    const $ = redraw$?.pipe(
+      rx.takeUntil(life.dispose$),
+      rx.debounceTime(10, rx.animationFrameScheduler),
+    );
+    $?.subscribe(redraw);
+    return life.dispose;
+  }, [redraw$]);
+
+  return redraw;
 }
