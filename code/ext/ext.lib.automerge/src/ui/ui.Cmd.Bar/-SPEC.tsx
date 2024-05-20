bb74d02f31fc490cb7390b0dcab82ad818c86eb2
Phil Cockfield <phil@cockfield.net>
2024-05-20 13:50:48 +1200

    CmdBar: spec loading docs from selector list

--- code/ext/ext.lib.automerge/src/ui/ui.Cmd.Bar/-SPEC.tsx
+++ code/ext/ext.lib.automerge/src/ui/ui.Cmd.Bar/-SPEC.tsx
@@ -1,21 +1,40 @@
-import { DEFAULTS, CmdBar } from '.';
-import { css, Color, Dev, Pkg } from '../../test.ui';
+import { CmdBar, DEFAULTS } from '.';
+import { Time, Color, Dev, Doc, Pkg, TestDb, WebStore, css } from '../../test.ui';
+import { Info } from '../ui.Info';
+import { RepoList } from '../ui.RepoList';
 import { type t } from './common';
 
 type P = t.CmdBarProps;
-type T = { props: P; debug: {} };
+type T = { props: P; debug: { docuri?: t.UriString } };
 const initial: T = { props: {}, debug: {} };
+
+/**
+ * Sample CRDT Store and Document Setup
+ */
+async function createStore(local: { docuri?: t.UriString }) {
+  const db = TestDb.Spec;
+  const store = WebStore.init({ storage: db.name });
+  const index = await WebStore.index(store);
+  return { store, index } as const;
+}
 
 /**
  * Spec
  */
 const name = DEFAULTS.displayName;
-export default Dev.describe(name, (e) => {
+export default Dev.describe(name, async (e) => {
   type LocalStore = T['debug'] & Pick<P, 'theme'>;
   const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
   const local = localstore.object({
     theme: 'Dark',
+    docuri: undefined,
   });
+
+  const db = await createStore(local);
+  const store = db.store;
+  const index = db.index;
+  let model: t.RepoListModel;
+  let ref: t.RepoListRef;
 
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
@@ -24,11 +43,21 @@ export default Dev.describe(name, (e) => {
     const state = await ctx.state<T>(initial);
     await state.change((d) => {
       d.props.theme = local.theme;
+      d.debug.docuri = local.docuri;
+    });
+
+    model = await RepoList.model(db.store, {
+      behaviors: ['Copyable', 'Deletable'],
+      onReady: (e) => (ref = e.ref),
+      onActiveChanged(e) {
+        console.info(`⚡️ onActiveChanged`, e);
+        state.change((d) => (local.docuri = d.debug.docuri = e.item.uri));
+      },
     });
 
     ctx.debug.width(330);
     ctx.subject
-      .size('fill')
+      .size('fill-x')
       .display('grid')
       .render<T>((e) => {
         const { props, debug } = e.state;
@@ -40,8 +69,20 @@ export default Dev.describe(name, (e) => {
   e.it('ui:debug', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
-    const link = Dev.Link.pkg(Pkg, dev);
-    dev.TODO();
+
+    dev.header.border(-0.1).render((e) => {
+      const ref = state.current.debug.docuri;
+      return (
+        <Info
+          stateful={true}
+          fields={['Repo', 'Doc', 'Doc.URI', 'Doc.Object']}
+          data={{
+            repo: { store, index },
+            document: { ref, object: { visible: true } },
+          }}
+        />
+      );
+    });
 
     dev.section('Properties', (dev) => {
       Dev.Theme.switch(dev, ['props', 'theme'], (next) => (local.theme = next));
@@ -57,9 +98,40 @@ export default Dev.describe(name, (e) => {
   e.it('ui:footer', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     const state = await dev.state();
-    dev.footer.border(-0.1).render<T>((e) => {
-      const data = e.state;
-      return <Dev.Object name={name} data={data} expand={1} />;
-    });
+    dev.footer
+      .border(-0.1)
+      .padding(0)
+      .render<T>((e) => {
+        const { props, debug } = e.state;
+        const data = {
+          docuri: Doc.Uri.id(debug.docuri, { shorten: 5 }),
+          props,
+        };
+
+        const styles = {
+          base: css({ position: 'relative' }),
+          object: css({ padding: 10 }),
+          list: css({ border: `solid 1px ${Color.alpha(Color.DARK, 0.15)}` }),
+        };
+
+        const elObject = (
+          <div {...styles.object}>
+            <Dev.Object name={name} data={data} expand={1} fontSize={11} />
+          </div>
+        );
+
+        const elList = (
+          <div {...styles.list}>
+            <RepoList model={model} onReady={(e) => e.ref.select(0)} style={{}} />
+          </div>
+        );
+
+        return (
+          <div {...styles.base}>
+            {elObject}
+            {elList}
+          </div>
+        );
+      });
   });
 });
