23c4d79e262aef37b6231740a2e88f67197c8069
Phil Cockfield <phil@cockfield.net>
2024-06-03 20:11:09 +1200

    rename files: index.mts → index.ts

--- script.deno/tmp.ts
+++ script.deno/tmp.ts
@@ -8,13 +8,21 @@ const exclude = [
   '**/*.d.mts',
 ];
 
-const pattern = 'code/**/index.pkg.mts';
+const pattern = 'code/**/src/index.mts';
 const dir = fs.resolve(import.meta.dirname || '', '..');
 const paths = await fs.glob(dir).find(pattern, { exclude });
 
 for (const file of paths) {
   const path = file.path.substring(dir.length + 1);
   console.log(c.green('•'), path);
+
+  const from = file.path;
+  const to = fs.join(fs.dirname(from), 'index.ts');
+  console.log('-------------------------------------------');
+  console.log('from', from);
+  console.log('to  ', to);
+  // console.log('file', file);
+  await Deno.rename(from, to);
 }
 
 console.log(`↑ dir: ${c.green(dir)}`);
