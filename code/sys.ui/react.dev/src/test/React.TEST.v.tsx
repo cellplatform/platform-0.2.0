214b2b378546747de08c6fd307e824d6443d0ffe
Phil Cockfield <phil@cockfield.net>
2024-06-04 14:25:42 +1200

    update refs

--- code/sys.ui/react.dev/src/test/React.TEST.v.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
-
-describe('@testing-library/react', () => {
-  it('sample (simple)', () => {
-    render(<div>Hello</div>);
-    expect(screen.getByText(/Hello/i)).toBeDefined();
-  });
-});
