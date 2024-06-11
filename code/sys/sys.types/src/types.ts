95b8ffc9410079cad5b85505286d9eb8d86f331c
Phil Cockfield <phil@cockfield.net>
2024-06-11 16:37:45 +1200

    type: Immutable<T> change taking patches callback

--- code/sys/sys.types/src/types.ts
+++ code/sys/sys.types/src/types.ts
@@ -1,4 +1,6 @@
-export * from './t.sys';
-export * from './t.sys.bus';
-export * from './t.sys.manifest';
-export * from './t.w3c';
+export type * from './t.sys';
+export type * from './t.sys.bus';
+export type * from './t.sys.manifest';
+export type * from './t.w3c';
+
+export type { Patch } from './common/t';
