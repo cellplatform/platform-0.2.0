4e56d051f4abf02223e776897adc8e3ca1fbe37c
Phil Cockfield <phil@cockfield.net>
2024-06-14 11:55:24 +1200

    rename folder

--- code/sys/sys.types/src/types.ts
+++ code/sys/sys.types/src/types.ts
@@ -1,6 +1,6 @@
 export type * from './t.sys';
 export type * from './t.sys.bus';
 export type * from './t.sys.manifest';
-export type * from './t.w3c';
+export type * from './t.www';
 
 export type { Patch } from './common/t';
