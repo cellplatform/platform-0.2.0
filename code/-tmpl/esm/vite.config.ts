160f8b04710e0b50056c1427acef7e7805154355
Phil Cockfield <phil@cockfield.net>
2024-06-14 15:08:57 +1200

    template: single line

--- code/-tmpl/esm/vite.config.ts
+++ code/-tmpl/esm/vite.config.ts
@@ -1,8 +1,6 @@
 import { Config } from '../../../config';
 
-export const tsconfig = Config.ts((e) => {
-  e.env('web', 'web:react');
-});
+export const tsconfig = Config.ts((e) => e.env('web', 'web:react'));
 
 export default Config.vite(import.meta.url, (e) => {
   e.lib({ entry: { index: 'src/index.ts' } });
