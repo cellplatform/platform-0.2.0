a9ad9eeb839043a8f8132a387b4b5dac0f0bdf6d
Phil Cockfield <phil@cockfield.net>
2024-06-19 16:36:14 +1200

    remove unused type references

--- code/sys/sys.cmd/src/common/t.ts
+++ code/sys/sys.cmd/src/common/t.ts
@@ -18,7 +18,6 @@ export type {
   UntilObservable,
 } from 'sys.types/src/types';
 
-export type { SpecImport, SpecImports, TestSuiteRunResponse } from 'sys.test.spec/src/types';
 export type { Describe, Expect, It } from 'sys.test/src/types';
 export type { PatchOperation } from 'sys.util/src/types';
 
