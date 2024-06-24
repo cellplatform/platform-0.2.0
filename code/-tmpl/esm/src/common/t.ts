9878913b952874e8e685e19bfa88566a7100d649
Phil Cockfield <phil@cockfield.net>
2024-06-24 20:36:01 +1200

    Update t.ts

--- code/-tmpl/esm/src/common/t.ts
+++ code/-tmpl/esm/src/common/t.ts
@@ -6,6 +6,7 @@ export type { Observable } from 'rxjs';
 /**
  * @system
  */
+export type { Cmd, CmdType } from 'sys.cmd/src/types';
 export type { SpecImport, SpecImports, TestSuiteRunResponse } from 'sys.test.spec/src/types';
 export type { Disposable, EventBus, Lifecycle, UntilObservable } from 'sys.types/src/types';
 
