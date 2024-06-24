f782cef948c54bb194a65ce91ddb49e66bc2019a
Phil Cockfield <phil@cockfield.net>
2024-06-25 10:40:51 +1200

    refactor

--- code/sys.ui/dom/src/dom.Keyboard/Keyboard.until.ts
+++ code/sys.ui/dom/src/dom.Keyboard/Keyboard.until.ts
@@ -9,13 +9,8 @@ export function until(until$?: t.UntilObservable) {
   const life = rx.lifecycle(until$);
   const { dispose, dispose$ } = life;
 
-  const on: t.KeyboardMonitor['on'] = (...args: any) => {
-    return handlerOnOverloaded(args, { dispose$ });
-  };
-
-  const filter: t.KeyboardMonitor['filter'] = (fn) => {
-    return handlerFiltered(fn, { dispose$ });
-  };
+  const on: t.KeyboardMonitor['on'] = (...args: any) => handlerOnOverloaded(args, { dispose$ });
+  const filter: t.KeyboardMonitor['filter'] = (fn) => handlerFiltered(fn, { dispose$ });
 
   const $ = KeyboardMonitor.$.pipe(rx.takeUntil(dispose$));
   const down$ = $.pipe(rx.filter((e) => e.last?.stage === 'Down'));
@@ -25,8 +20,8 @@ export function until(until$?: t.UntilObservable) {
     $,
     up$,
     down$,
-    on,
     filter,
+    on,
 
     /**
      * Lifecycle
