764671f02f700cf10641b5c4a647023d3ecaa725
Phil Cockfield <phil@cockfield.net>
2024-06-25 13:30:41 +1200

    stub in: slc.000

---
+++ code/spikes/slc.000/src/ui/common/t.ts
@@ -0,0 +1,24 @@
+/**
+ * @external
+ */
+export type { CSSProperties } from 'react';
+
+/**
+ * @system
+ */
+export type {
+  CommonInfoProps,
+  CommonTheme,
+  CssEdgesInput,
+  CssValue,
+  DevCtxState,
+  MarginInput,
+  PropListItem,
+  PropListProps,
+  RenderCountProps,
+} from 'sys.ui.react.common/src/types';
+
+/**
+ * @local
+ */
+export type * from '../../common/t';
