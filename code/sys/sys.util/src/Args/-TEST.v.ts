7fbdb3077b9039dabe21634c6b3bd4dee4d2acf7
Phil Cockfield <phil@cockfield.net>
2024-06-26 11:02:25 +1200

    refactor test

--- code/sys/sys.util/src/Args/-TEST.v.ts
+++ code/sys/sys.util/src/Args/-TEST.v.ts
@@ -7,12 +7,12 @@ describe('parseArgs (minimist)', () => {
    *       https://github.com/minimistjs/minimist
    */
   const sample1 = {
-    text: '-a beep -b boop',
-    result: { _: [], a: 'beep', b: 'boop' },
+    argv: '-a beep -b boop',
+    parsed: { _: [], a: 'beep', b: 'boop' },
   };
   const sample2 = {
-    text: '-x 3 -y 4 -n5 -abc --beep=boop --no-ding foo bar baz',
-    result: {
+    argv: '-x 3 -y 4 -n5 -abc --beep=boop --no-ding foo bar baz',
+    parsed: {
       _: ['foo', 'bar', 'baz'],
       x: 3,
       y: 4,
@@ -25,19 +25,21 @@ describe('parseArgs (minimist)', () => {
     },
   };
 
-  it('parse string', () => {
-    const res1 = Args.parse(sample1.text);
-    const res2 = Args.parse(sample2.text);
-    expect(res1).to.eql(sample1.result);
-    expect(res2).to.eql(sample2.result);
-  });
+  describe('parse', () => {
+    it('parse: string', () => {
+      const res1 = Args.parse(sample1.argv);
+      const res2 = Args.parse(sample2.argv);
+      expect(res1).to.eql(sample1.parsed);
+      expect(res2).to.eql(sample2.parsed);
+    });
 
-  it('parse array', () => {
-    const args1 = Args.asArray(sample1.text);
-    const args2 = Args.asArray(sample2.text);
-    const res1 = Args.parse(args1);
-    const res2 = Args.parse(args2);
-    expect(res1).to.eql(sample1.result);
-    expect(res2).to.eql(sample2.result);
+    it('parse: array', () => {
+      const args1 = Args.asArray(sample1.argv);
+      const args2 = Args.asArray(sample2.argv);
+      const res1 = Args.parse(args1);
+      const res2 = Args.parse(args2);
+      expect(res1).to.eql(sample1.parsed);
+      expect(res2).to.eql(sample2.parsed);
+    });
   });
 });
