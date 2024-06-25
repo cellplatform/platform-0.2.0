11bc3dafe920e74e5c0f6bfcc8aa894bc73a5c85
Phil Cockfield <phil@cockfield.net>
2024-06-26 10:36:54 +1200

    file rename

--- code/spikes/dev.000/src/ui.main.000/-SPEC.tsx
+++ code/spikes/dev.000/src/ui.main.000/-SPEC.tsx
@@ -8,9 +8,9 @@ import { Buffer } from 'buffer';
 if (!window.Buffer) window.Buffer = Buffer;
 
 import { Color, Dev, css, type t } from '../test.ui';
-import { SampleLayout } from './-SPEC.ui';
-import { Footer } from './-SPEC.ui.CmdBar';
-import { DebugFooter } from './-SPEC.ui.debug.footer';
+import { SampleLayout } from './ui';
+import { Footer } from './ui.CmdBar';
+import { DebugFooter } from './ui.debug';
 import { Cmd, Immutable, Peer, PeerRepoList, RepoList, WebStore, WebrtcStore } from './common';
 
 type T = {
