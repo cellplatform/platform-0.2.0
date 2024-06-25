71da5fb62e8a0a2ea3ad820c52a1a831405376a3
Phil Cockfield <phil@cockfield.net>
2024-06-25 13:48:44 +1200

    update refs

--- code/sys.driver/ext.lib.privy/src/index.pkg.ts
+++ code/sys.driver/ext.lib.privy/src/index.pkg.ts
@@ -20,7 +20,7 @@ export const Pkg: ModuleDef = {
     'sys.cmd': '0.0.0',
     'sys.util': '0.0.0',
     'sys.ui.react.common': '0.0.0',
-    'viem': '2.16.1',
+    'viem': '2.16.2',
   },
   toString() {
     return `${Pkg.name}@${Pkg.version}`;
