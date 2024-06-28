7748c6fc1211131222e132041079e5041b17840b
Phil Cockfield <phil@cockfield.net>
2024-06-28 17:57:42 +1200

    shorten hash

--- code/sys.driver/ext.lib.automerge/src/ui/ui.Info/common.ts
+++ code/sys.driver/ext.lib.automerge/src/ui/ui.Info/common.ts
@@ -40,7 +40,7 @@ const visibleFilter: t.InfoDataVisible<t.InfoField>['filter'] = (e) => {
 const uri: Required<t.InfoDataDocUri> = {
   shorten: [4, 4],
   prefix: 'crdt',
-  head: 3,
+  head: 2,
   clipboard: (uri) => Doc.Uri.id(uri),
 };
 
