32e8dde5cea30d5384901f676c4edc04182cd0f5
Phil Cockfield <phil@cockfield.net>
2024-07-02 10:16:13 +1200

    wasmer config → ssl / headers (cross-origin-isolation)

--- code/sys.driver/ext.lib.wasmer/vite.config.ts
+++ code/sys.driver/ext.lib.wasmer/vite.config.ts
@@ -6,4 +6,10 @@ export default Config.vite(import.meta.url, (e) => {
   e.lib({ entry: { index: 'src/index.ts' } });
   e.target('web');
   e.externalDependency(e.ctx.deps.map((d) => d.name));
+
+  e.plugin('ssl').headers({
+    // REF: https://docs.wasmer.io/sdk/wasmer-js/explainers/troubleshooting#sharedarraybuffer-and-cross-origin-isolation
+    'Cross-Origin-Opener-Policy': 'same-origin',
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+  });
 });
