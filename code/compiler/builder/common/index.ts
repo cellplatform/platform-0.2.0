bede0415349f664c4f868861d3dde4b82280b910
Phil Cockfield <phil@cockfield.net>
2024-07-02 10:56:44 +1200

    Update index.ts

--- code/compiler/builder/common/index.ts
+++ code/compiler/builder/common/index.ts
@@ -3,7 +3,7 @@ import { fs } from './fs';
 export type * as t from '../t';
 export * from './libs';
 
-export { Time, Util, asArray } from './u';
+export { Util, asArray } from './u';
 export { Find as FindUtil } from './u.Find';
 export { Json as JsonUtil, PackageJson as PackageJsonUtil } from './u.Json';
 export { LogTable } from './u.LogTable';
