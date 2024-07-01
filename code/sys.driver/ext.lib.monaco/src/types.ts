a3169909645727b0db1d63988a1117b8d31c19af
Phil Cockfield <phil@cockfield.net>
2024-07-02 11:30:14 +1200

    file rename

--- code/sys.driver/ext.lib.monaco/src/types.ts
+++ code/sys.driver/ext.lib.monaco/src/types.ts
@@ -2,5 +2,5 @@ export type * from './ui/logic.Editor.Carets/t';
 export type * from './ui/ui.Info/t';
 export type * from './ui/ui.MonacoEditor/t';
 
-export type * from './types.def';
-export type * from './types.def.monaco';
+export type * from './t.def';
+export type * from './t.def.monaco';
