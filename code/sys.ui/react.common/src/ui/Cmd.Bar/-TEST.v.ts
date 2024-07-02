3ef332d48178eaf22d335bfabb9363577f4be34d
Phil Cockfield <phil@cockfield.net>
2024-07-03 11:43:17 +1200

    CmdBar.Ctrl.methods

--- code/sys.ui/react.common/src/ui/Cmd.Bar/-TEST.v.ts
+++ code/sys.ui/react.common/src/ui/Cmd.Bar/-TEST.v.ts
@@ -1,12 +1,14 @@
-import { Time, describe, it, expect, type t, Immutable } from '../../test';
 import { CmdBar } from '.';
+import { Immutable, Time, describe, expect, it } from '../../test';
 
 describe('CmdBar', () => {
   describe('CmdBar.Ctrl', () => {
+    const Ctrl = CmdBar.Ctrl;
+
     it('creation (from simple Immutable<T>)', async () => {
       const transport = Immutable.clonerRef({});
-      const ctrl1 = CmdBar.Ctrl.create(transport);
-      const ctrl2 = CmdBar.Ctrl.create(); // NB: auto transport
+      const ctrl1 = Ctrl.create(transport);
+      const ctrl2 = Ctrl.create(); // NB: auto immutable "transport" construction.
       const expectCounter = (value: number) =>
         expect((transport.current as any).counter.value).to.eql(value);
 
@@ -20,6 +22,21 @@ describe('CmdBar', () => {
       await Time.wait(0);
       expectCounter(1);
       expect(fired).to.eql(2);
+    });
+
+    it('CmdBar.Ctrl.methods', () => {
+      const ctrl = Ctrl.create();
+      const cmd = Ctrl.cmd();
+
+      const methods1 = Ctrl.methods(cmd); // From raw command.
+      const methods2 = Ctrl.methods(cmd); // From raw command (new instance)
+      const methods3 = Ctrl.methods(methods1);
+      const methods4 = Ctrl.methods(ctrl);
+
+      expect(CmdBar.Is.methods(methods1)).to.eql(true);
+      expect(methods1).to.not.equal(methods2);
+      expect(methods3).to.equal(methods1); // NB: same instance reused.
+      expect(methods4).to.equal(ctrl);
     });
   });
 
--- code/sys.ui/react.common/src/ui/Cmd.Bar/Root.ctrl.ts
+++ code/sys.ui/react.common/src/ui/Cmd.Bar/Root.ctrl.ts
@@ -1,24 +1,32 @@
 import { Cmd, Immutable, type t } from './common';
+import { Is } from './u.Is';
+
+type C = t.CmdBarCtrlCmdType;
 
 /**
  * Command API for controlling the <CmdBar> component.
  */
 export const Ctrl = {
   create(transport?: t.CmdImmutable): t.CmdBarCtrlMethods {
-    type C = t.CmdBarCtrlCmdType;
-    const doc = transport ?? Immutable.clonerRef({});
-    const cmd = Cmd.create<C>(doc) as t.CmdBarCtrl;
+    const cmd = Ctrl.cmd(transport);
     return { cmd, ...Ctrl.methods(cmd) };
   },
 
-  methods(cmd: t.CmdBarCtrl): t.CmdBarMethods {
+  methods(input: t.CmdBarCtrl | t.CmdBarMethods): t.CmdBarMethods {
+    if (Is.methods(input)) return input;
+    const method = input.method;
     return {
-      focus: cmd.method('Focus'),
-      blur: cmd.method('Blur'),
-      selectAll: cmd.method('SelectAll'),
-      caretToStart: cmd.method('CaretToStart'),
-      caretToEnd: cmd.method('CaretToEnd'),
-      invoke: cmd.method('Invoke'),
+      focus: method('Focus'),
+      blur: method('Blur'),
+      selectAll: method('SelectAll'),
+      caretToStart: method('CaretToStart'),
+      caretToEnd: method('CaretToEnd'),
+      invoke: method('Invoke'),
     };
+  },
+
+  cmd(transport?: t.CmdImmutable) {
+    const doc = transport ?? Immutable.clonerRef({});
+    return Cmd.create<C>(doc) as t.CmdBarCtrl;
   },
 } as const;
