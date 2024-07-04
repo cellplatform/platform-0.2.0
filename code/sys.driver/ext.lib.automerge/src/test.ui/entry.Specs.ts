1e79fd22020e190566bcf41afbb17246349c05f4
Phil Cockfield <phil@cockfield.net>
2024-07-04 13:06:40 +1200

    delete old code

--- code/sys.driver/ext.lib.automerge/src/test.ui/entry.Specs.ts
+++ code/sys.driver/ext.lib.automerge/src/test.ui/entry.Specs.ts
@@ -15,7 +15,6 @@ export const Specs = {
   [`${ns}.ui.Nav.Paging`]: () => import('../ui/ui.Nav.Paging/-SPEC'),
   [`${ns}.ui.Cmd.Prefix`]: () => import('../ui/ui.Cmd.Prefix/-SPEC'),
   [`${ns}.ui.Cmd.Bar`]: () => import('../ui/ui.Cmd.Bar/-SPEC'),
-  [`${ns}.ui.Cmd.Host`]: () => import('../ui/ui.Cmd.Host/-SPEC'),
   [`${ns}.ui.useDocs`]: () => import('../ui/ui.use/-SPEC.useDocs'),
 } as t.SpecImports;
 
--- code/sys.driver/ext.lib.automerge/src/ui/ui.Cmd.Host/common.ts
+++
@@ -1,9 +0,0 @@
-import { Pkg } from './common';
-export * from '../common';
-
-/**
- * Constants
- */
-export const DEFAULTS = {
-  displayName: `${Pkg.name}.CmdHost`,
-} as const;
