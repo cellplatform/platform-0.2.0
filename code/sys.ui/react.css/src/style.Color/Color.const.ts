c219bb22e66dc5754153c83b5332a5f070769caa
Phil Cockfield <phil@cockfield.net>
2024-07-10 18:20:58 +1200

    add BLUE

--- code/sys.ui/react.css/src/style.Color/Color.const.ts
+++ code/sys.ui/react.css/src/style.Color/Color.const.ts
@@ -1,3 +1,4 @@
 export const RED = `rgba(255, 0, 0, 0.1)`; // Standard "ruby" RED color (semi-transparent).
 export const WHITE = '#ffffff';
 export const DARK = '#293042';
+export const BLUE = '#4D7EF7';
