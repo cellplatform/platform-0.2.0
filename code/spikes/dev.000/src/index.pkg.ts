c48049f730c9a00facdbfe79b86a58936af7a687
Phil Cockfield <phil@cockfield.net>
2024-07-16 15:12:53 +1200

    version

--- code/spikes/dev.000/src/index.pkg.ts
+++ code/spikes/dev.000/src/index.pkg.ts
@@ -11,7 +11,7 @@
 
 export const Pkg: ModuleDef = {
   name: 'dev.000',
-  version: '0.6.179',
+  version: '0.6.180',
   dependencies: {
     '@automerge/automerge': '2.2.4',
     '@automerge/automerge-repo': '1.2.0',
