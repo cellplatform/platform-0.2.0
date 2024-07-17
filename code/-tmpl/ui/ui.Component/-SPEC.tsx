2e0f073eea8e465a485cc77a3a14d361672a03e1
Phil Cockfield <phil@cockfield.net>
2024-07-17 18:17:51 +1200

    tmpl: Immutable props

--- code/-tmpl/ui/ui.Component/-SPEC.tsx
+++ code/-tmpl/ui/ui.Component/-SPEC.tsx
@@ -1,10 +1,10 @@
 import { DEFAULTS, Root } from '.';
-import { css, Color, Dev, Pkg } from '../../test.ui';
+import { css, Color, rx, Dev, Immutable, Pkg } from '../../test.ui';
 import { type t } from './common';
 
 type P = t.RootProps;
-type T = { props: P; debug: {} };
-const initial: T = { props: {}, debug: {} };
+type T = { debug: {} };
+const initial: T = { debug: {} };
 
 /**
  * Spec
@@ -12,19 +12,20 @@ const initial: T = { props: {}, debug: {} };
 const name = DEFAULTS.displayName;
 
 export default Dev.describe(name, (e) => {
-  type LocalStore = T['debug'] & Pick<P, 'theme'>;
+  type LocalStore = T['debug'] & { props?: t.JsonString };
   const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
-  const local = localstore.object({
-    theme: 'Dark',
-  });
+  const local = localstore.object({ props: undefined });
+
+  const propState = Immutable.clonerRef<P>(local.props ? JSON.parse(local.props) : {});
 
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
     const dev = Dev.tools<T>(e, initial);
 
-    const state = await ctx.state<T>(initial);
-    await state.change((d) => {
-      d.props.theme = local.theme;
+    const props$ = propState.events().changed$;
+    props$.pipe(rx.debounceTime(100)).subscribe((e) => {
+      dev.redraw();
+      local.props = JSON.stringify(propState.current);
     });
 
     ctx.debug.width(330);
@@ -32,20 +33,19 @@ export default Dev.describe(name, (e) => {
       .size('fill')
       .display('grid')
       .render<T>((e) => {
-        const { props, debug } = e.state;
-        Dev.Theme.background(dev, props.theme, 1);
+        const { debug } = e.state;
+        const props = propState.current;
         return <Root {...props} />;
       });
   });
 
   e.it('ui:debug', async (e) => {
     const dev = Dev.tools<T>(e, initial);
-    const state = await dev.state();
     const link = Dev.Link.pkg(Pkg, dev);
     dev.TODO();
 
     dev.section('Properties', (dev) => {
-      Dev.Theme.switch(dev, ['props', 'theme'], (next) => (local.theme = next));
+      Dev.Theme.immutable(dev, propState);
     });
 
     dev.hr(5, 20);
@@ -57,9 +57,8 @@ export default Dev.describe(name, (e) => {
 
   e.it('ui:footer', async (e) => {
     const dev = Dev.tools<T>(e, initial);
-    const state = await dev.state();
     dev.footer.border(-0.1).render<T>((e) => {
-      const data = e.state;
+      const data = { props: propState.current };
       return <Dev.Object name={name} data={data} expand={1} fontSize={11} />;
     });
   });
