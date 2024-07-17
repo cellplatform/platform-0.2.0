516e695ec97ebb199191d8afcd0535eb6c880d90
Phil Cockfield <phil@cockfield.net>
2024-07-17 18:36:38 +1200

    using Immutable

--- code/-tmpl/ui/ui.Component/-SPEC.tsx
+++ code/-tmpl/ui/ui.Component/-SPEC.tsx
@@ -3,8 +3,8 @@ import { css, Color, rx, Dev, Immutable, Pkg } from '../../test.ui';
 import { type t } from './common';
 
 type P = t.RootProps;
-type T = { debug: {} };
-const initial: T = { debug: {} };
+type T = {};
+const initial: T = {};
 
 /**
  * Spec
@@ -12,29 +12,39 @@ const initial: T = { debug: {} };
 const name = DEFAULTS.displayName;
 
 export default Dev.describe(name, (e) => {
-  type LocalStore = T['debug'] & { props?: t.JsonString };
+  type LocalStore = { props?: t.JsonString; debug?: t.JsonString };
   const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
-  const local = localstore.object({ props: undefined });
-
-  const propState = Immutable.clonerRef<P>(local.props ? JSON.parse(local.props) : {});
+  const local = localstore.object({ props: undefined, debug: undefined });
+  const state = {
+    props: Immutable.clonerRef<P>(local.props ? JSON.parse(local.props) : {}),
+    debug: Immutable.clonerRef<T>(local.debug ? JSON.parse(local.debug) : {}),
+  } as const;
 
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
     const dev = Dev.tools<T>(e, initial);
 
-    const props$ = propState.events().changed$;
-    props$.pipe(rx.debounceTime(100)).subscribe((e) => {
-      dev.redraw();
-      local.props = JSON.stringify(propState.current);
-    });
+    const props$ = state.props.events().changed$;
+    const debug$ = state.debug.events().changed$;
+
+    rx.merge(props$, debug$)
+      .pipe(rx.debounceTime(1000))
+      .subscribe(() => {
+        local.props = JSON.stringify(state.props.current);
+        local.debug = JSON.stringify(state.debug.current);
+      });
+
+    rx.merge(props$, debug$)
+      .pipe(rx.debounceTime(100))
+      .subscribe(() => dev.redraw());
 
     ctx.debug.width(330);
     ctx.subject
       .size('fill')
       .display('grid')
       .render<T>((e) => {
-        const { debug } = e.state;
-        const props = propState.current;
+        const props = state.props.current;
+        Dev.Theme.background(dev, props.theme, 1);
         return <Root {...props} />;
       });
   });
@@ -45,7 +55,7 @@ export default Dev.describe(name, (e) => {
     dev.TODO();
 
     dev.section('Properties', (dev) => {
-      Dev.Theme.immutable(dev, propState);
+      Dev.Theme.immutable(dev, state.props);
     });
 
     dev.hr(5, 20);
@@ -58,7 +68,7 @@ export default Dev.describe(name, (e) => {
   e.it('ui:footer', async (e) => {
     const dev = Dev.tools<T>(e, initial);
     dev.footer.border(-0.1).render<T>((e) => {
-      const data = { props: propState.current };
+      const data = { props: state.props.current };
       return <Dev.Object name={name} data={data} expand={1} fontSize={11} />;
     });
   });
