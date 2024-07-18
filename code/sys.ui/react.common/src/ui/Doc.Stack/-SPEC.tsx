e0a26fdb5a8eb706614888cf53c0253f24eafc66
Phil Cockfield <phil@cockfield.net>
2024-07-18 12:04:41 +1200

    setup spec

--- code/sys.ui/react.common/src/ui/Doc.Stack/-SPEC.tsx
+++ code/sys.ui/react.common/src/ui/Doc.Stack/-SPEC.tsx
@@ -1,9 +1,16 @@
 import { DEFAULTS, DocStack } from '.';
-import { Dev, Immutable, Pkg, rx } from '../../test.ui';
+import { COLORS, css, Dev, Immutable, Json, Pkg, rx, slug } from '../../test.ui';
 import { type t } from './common';
 
-type P = t.DocStack;
-type D = {};
+type P = t.DocStackProps;
+type D = { ids: string[]; start: t.Index };
+
+const defaultDebug = (): D => {
+  return {
+    ids: Array.from({ length: 20 }).map(() => slug()),
+    start: 0,
+  };
+};
 
 /**
  * Spec
@@ -13,11 +20,40 @@ const name = DEFAULTS.displayName;
 export default Dev.describe(name, (e) => {
   type LocalStore = { props?: t.JsonString; debug?: t.JsonString };
   const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
-  const local = localstore.object({ props: undefined });
+  const local = localstore.object({ props: undefined, debug: undefined });
 
   const state = {
-    props: Immutable.clonerRef<P>(local.props ? JSON.parse(local.props) : {}),
-    debug: Immutable.clonerRef<D>(local.debug ? JSON.parse(local.debug) : {}),
+    props: Immutable.clonerRef<P>(Json.parse<P>(local.props, DEFAULTS.props)),
+    debug: Immutable.clonerRef<D>(Json.parse<D>(local.debug, defaultDebug)),
+  } as const;
+
+  const Props = {
+    get current() {
+      return {
+        ...state.props.current,
+        ids: Props.ids.current,
+      };
+    },
+    ids: {
+      get all() {
+        return state.debug.current.ids;
+      },
+      get current() {
+        const { ids, start } = state.debug.current;
+        const { total = DEFAULTS.props.total } = state.props.current;
+        return ids.slice(start, start + total);
+      },
+      get range() {
+        const { start } = state.debug.current;
+        const { total = DEFAULTS.props.total } = state.props.current;
+        const end = start + total - 1;
+        return { start, end, total } as const;
+      },
+      withinRange(index: t.Index) {
+        const { start, end } = Props.ids.range;
+        return !(index < start || index > end);
+      },
+    },
   } as const;
 
   e.it('ui:init', (e) => {
@@ -30,8 +66,8 @@ export default Dev.describe(name, (e) => {
     rx.merge(props$, debug$)
       .pipe(rx.debounceTime(1000))
       .subscribe(() => {
-        local.props = JSON.stringify(state.props.current);
-        local.debug = JSON.stringify(state.debug.current);
+        local.props = Json.stringify(state.props.current);
+        local.debug = Json.stringify(state.debug.current);
       });
 
     rx.merge(props$, debug$)
@@ -40,12 +76,13 @@ export default Dev.describe(name, (e) => {
 
     ctx.debug.width(330);
     ctx.subject
-      .size('fill-x', 180)
+      .size('fill-x', 100)
       .display('grid')
       .render<D>((e) => {
-        const props = state.props.current;
+        const debug = state.debug.current;
+        const props = Props.current;
         Dev.Theme.background(dev, props.theme, 1);
-        return <DocStack {...props} style={{ height: 250 }} />;
+        return <DocStack {...props} />;
       });
   });
 
@@ -54,6 +91,58 @@ export default Dev.describe(name, (e) => {
 
     dev.section('Properties', (dev) => {
       Dev.Theme.immutable(dev, state.props, 1);
+    });
+
+    dev.hr(5, 20);
+
+    dev.section('Ids', (dev) => {
+      dev.row((e) => {
+        const ids = Props.ids.all;
+        const styles = {
+          base: css({
+            lineHeight: 1.5,
+            marginLeft: 30,
+            marginBottom: 20,
+            display: 'grid',
+            gridTemplateColumns: `repeat(4, 1fr)`,
+          }),
+          id: css({ fontFamily: 'monospace', fontSize: 11, fontWeight: 600, opacity: 0.1 }),
+          current: css({ opacity: 1 }),
+          first: css({ color: COLORS.MAGENTA }),
+        };
+
+        const elList = ids.map((id, i) => {
+          const style = Props.ids.withinRange(i) ? styles.current : undefined;
+          const isFirst = Props.ids.range.start === i;
+          return (
+            <div key={id} {...css(styles.id, style, isFirst && styles.first)}>
+              {id}
+            </div>
+          );
+        });
+
+        return <div {...styles.base}>{elList}</div>;
+      });
+
+      const increment = (by: number) => {
+        const { ids } = state.debug.current;
+        state.debug.change((d) => {
+          const next = d.start + by;
+          d.start = Math.max(0, Math.min(ids.length - 1, next));
+        });
+      };
+
+      dev.button('start:(next)', (e) => increment(1));
+      dev.button('start:(prev)', (e) => increment(-1));
+
+      dev.hr(-1, 5);
+      dev.button('reset', (e) => {
+        state.debug.change((d) => {
+          const next = defaultDebug();
+          d.start = next.start;
+          d.ids = next.ids;
+        });
+      });
     });
 
     dev.hr(5, 20);
@@ -67,7 +156,8 @@ export default Dev.describe(name, (e) => {
     const dev = Dev.tools<D>(e);
     dev.footer.border(-0.1).render<D>((e) => {
       const data = {
-        props: state.props.current,
+        props: Props.current,
+        debug: state.debug.current,
       };
       return <Dev.Object name={name} data={data} expand={1} fontSize={11} />;
     });
