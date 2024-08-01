26d4b80f029c72d554fa8dc60c9d4fa86c3b2468
Phil Cockfield <phil@cockfield.net>
2024-08-02 07:33:53 +1200

    Update t.Editor.ts

--- code/sys.driver/ext.lib.monaco.crdt/src/ui/u.Syncer/t.Editor.ts
+++ code/sys.driver/ext.lib.monaco.crdt/src/ui/u.Syncer/t.Editor.ts
@@ -2,12 +2,18 @@ import type { t } from './common';
 
 export type EditorUpdateStrategy = 'Splice' | 'Overwrite';
 
+/**
+ * Definition of paths to the editor state object.
+ */
 export type EditorPaths = {
   text: t.ObjectPath;
   identity: t.ObjectPath;
   cmd: t.ObjectPath;
 };
 
+/**
+ * The transient editor state of a single identity (aka. "user").
+ */
 export type EditorIdentityState = {
   selection?: t.Selection;
 };
