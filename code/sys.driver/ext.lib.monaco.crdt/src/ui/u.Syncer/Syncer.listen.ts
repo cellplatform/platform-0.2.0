647b0b8abddd85e84b35eae2e97393030269da37
Phil Cockfield <phil@cockfield.net>
2024-08-05 16:49:16 +1200

    rename file

--- code/sys.driver/ext.lib.monaco.crdt/src/ui/u.Syncer/Syncer.listen.ts
+++ code/sys.driver/ext.lib.monaco.crdt/src/ui/u.Syncer/Syncer.listen.ts
@@ -1,3 +1,108 @@
+import { calculateDiff } from './-tmp.diff';
+
+import { Monaco, DEFAULTS, ObjectPath, Path, Pkg, rx, type t } from './common';
+import { SyncerCmd } from './Syncer.Cmd';
+import { Util } from './u';
+
+type Options = {
+  debugLabel?: string;
+  strategy?: t.EditorUpdateStrategy | (() => t.EditorUpdateStrategy | undefined);
+  paths?: t.EditorPaths | t.ObjectPath;
+  identity?: string; // Unique identifier (URI) representing the user/editor.
+  dispose$?: t.UntilObservable;
+};
+
+/**
+ * An adapter for managing 2-way binding between a code-editor UI
+ * component and a CRDT (Automerge.Text) data-structure.
+ */
+export function listen(
+  monaco: t.Monaco,
+  editor: t.MonacoCodeEditor,
+  lens: t.Lens | t.Doc,
+  options: Options = {},
+): t.SyncListener {
+  const { debugLabel } = options;
+  const paths = Util.Path.wrangle(options.paths);
+  const identity = Util.Identity.format(options.identity);
+  const Mutate = ObjectPath.Mutate;
+
+  /**
+   * Lifecycle.
+   */
+  const life = rx.lifecycle(options.dispose$);
+  const { dispose, dispose$ } = life;
+  dispose$.subscribe(() => {
+    textChangedHandler.dispose();
+    selectionChangedHandler.dispose();
+    editor.setValue('');
+  });
+
+  const cmd = Util.Cmd.create(lens, { paths });
+  SyncerCmd.listen(cmd, { identity, paths, dispose$ });
+
+  /**
+   * Observables.
+   */
+  const events = lens.events(dispose$);
+  const changed$ = events.changed$.pipe(rx.filter(() => !_ignoreChange));
+  const identity$ = changed$.pipe(
+    rx.filter((e) => Util.Patch.Includes.identity(e.patches, { paths })),
+    rx.map((e) => {
+      type T = t.EditorIdentityState;
+      const identity = Util.Patch.extractIdentity(e.patches);
+      const path = [...paths.identity, identity];
+      const before = ObjectPath.resolve<T>(e.before, path)!;
+      const after = ObjectPath.resolve<T>(e.after, path)!;
+      const res: t.EditorIdentityStateChange = { identity, before, after };
+      return res;
+    }),
+  );
+
+  /**
+   * Helpers.
+   */
+  const patchMonaco = Util.Patch.monaco(monaco, editor);
+  const Text = Util.Lens.text(lens, paths);
+
+  /**
+   * Editor change.
+   */
+  let _ignoreChange = false;
+  const changeEditorText = (text: string) => {
+    _ignoreChange = true;
+    const before = editor.getSelections()!;
+    editor.setValue(text);
+    editor.setSelections(before);
+    _ignoreChange = false;
+  };
+
+  /**
+   * CRDT: text changed.
+   */
+  changed$
+    .pipe(rx.distinctWhile((p, n) => Text.resolve(p.after) === Text.resolve(n.after)))
+    .subscribe((e) => {
+      const before = editor.getValue();
+      const after = Text.resolve(e.after) ?? '';
+      if (after === before) return;
+
+      /**
+       * TODO 🐷
+       */
+      const diff = calculateDiff(Text.resolve(e.before) || '', Text.resolve(e.after) || '');
+      console.log(`TODO [${Pkg.name}] 🐷 diff:`, diff);
+
+      const source = 'crdt.sync';
+      const patches = e.patches.filter((patch) => startsWith(patch.path, paths.text));
+      patches.forEach((patch) => {
+        _ignoreChange = true;
+        if (patch.action === 'del') patchMonaco.delete(patch, `${source}:delete`);
+        if (patch.action === 'splice') patchMonaco.splice(patch, `${source}:update`);
+        _ignoreChange = false;
+      });
+    });
+
   /**
    * CRDT: Selection → Keep editor carets in sync.
    */
@@ -7,3 +112,96 @@
     const selections = e.after.selections;
     carets.identity(e.identity).change({ selections });
   });
+
+  /**
+   * Editor: text changed.
+   */
+  const textChangedHandler = editor.onDidChangeModelContent((e) => {
+    if (life.disposed) return;
+    if (_ignoreChange) return;
+
+    /**
+     * Apply each change to the CRDT text field.
+     * https://automerge.org/automerge/api-docs/js/functions/next.splice.html
+     */
+    e.changes.forEach((change) => {
+      const { startLineNumber, endLineNumber, startColumn, endColumn } = change.range;
+      const isReplace = startLineNumber !== endLineNumber || startColumn !== endColumn;
+      const index = change.rangeOffset;
+      const deleteCount = isReplace ? change.rangeLength : 0;
+
+      const strategy = api.strategy;
+      if (strategy === 'Splice') {
+        lens.change((d) => Text.splice(d, index, deleteCount, change.text));
+      }
+
+      if (strategy === 'Overwrite') {
+        lens.change((d) => Text.replace(d, editor.getValue()));
+      }
+    });
+  });
+
+  /**
+   * Editor: selection changed.
+   */
+  const selectionChangedHandler = editor.onDidChangeCursorSelection((e) => {
+    if (life.disposed) return;
+    lens.change((d) => {
+      const path = Util.Path.identity(identity);
+      const selections = editor.getSelections();
+      Mutate.value(d, path.selections, selections);
+    });
+  });
+
+  /**
+   * Initial state.
+   */
+  const initialText = Text.current;
+  if (initialText === undefined) {
+    lens.change((d) => Path.Object.Mutate.value(d, paths.text, ''));
+  } else if (typeof initialText === 'string') {
+    changeEditorText(initialText);
+  }
+
+  /**
+   * API
+   */
+  const api: t.SyncListener = {
+    cmd,
+    identity,
+    changed: { identity$ },
+
+    get strategy() {
+      const { strategy } = options;
+      if (typeof strategy === 'string') return strategy;
+      if (typeof strategy === 'function') strategy() ?? DEFAULTS.strategy;
+      return DEFAULTS.strategy;
+    },
+
+    /**
+     * Query the status of all identities, and purge dead ones.
+     */
+    async purge() {
+      const self = identity;
+      return Util.Cmd.purge({ cmd, lens, self, paths });
+    },
+
+    /**
+     * Lifecycle
+     */
+    dispose,
+    dispose$,
+    get disposed() {
+      return life.disposed;
+    },
+  };
+
+  return api;
+}
+
+/**
+ * Helpers
+ */
+function startsWith(list: any[], m: any[]): boolean {
+  return list.length >= m.length && m.every((value, index) => value === list[index]);
+}
