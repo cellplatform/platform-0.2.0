4f217ecfd683a40535110fde20d14964bb8cbfa0
Phil Cockfield <phil@cockfield.net>
2024-08-30 07:12:56 +1200

    refactor

--- code/sys.driver/ext.lib.automerge/src/ui/ui.Info/u.ts
+++ code/sys.driver/ext.lib.automerge/src/ui/ui.Info/u.ts
@@ -1,24 +1,29 @@
-import { DEFAULTS, PropList, type t } from './common';
+import { DEFAULTS, PropList, Value, type t } from './common';
 import { Data } from './u.data';
 import { Diff } from './u.diff';
 
 export { Data, Diff };
 
+type P = t.InfoProps | t.InfoStatefulProps;
 const DEF = DEFAULTS.props;
 
 export const Wrangle = {
   Data,
   Diff,
 
-  ctx(props: t.InfoProps | t.InfoStatefulProps): t.InfoCtx {
+  ctx(props: P): t.InfoCtx {
     const { repos = {}, theme = DEF.theme, enabled = DEF.enabled } = props;
     const fields = PropList.fields(props.fields);
     const handlers = Wrangle.handlers(props);
     return { repos, handlers, fields, theme, enabled };
   },
 
-  handlers(props: t.InfoProps | t.InfoStatefulProps): t.InfoHandlers {
-    const { onVisibleToggle, onDocToggleClick, onBeforeObjectRender } = props;
-    return { onVisibleToggle, onDocToggleClick, onBeforeObjectRender };
+  handlers(props: P): t.InfoHandlers {
+    return Value.Object.pick<t.InfoHandlers>(
+      props,
+      'onVisibleToggle',
+      'onDocToggleClick',
+      'onBeforeObjectRender',
+    );
   },
 } as const;
