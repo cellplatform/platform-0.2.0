26666f2baeea05675e418259aa1799596f106c2a
Phil Cockfield <phil@cockfield.net>
2024-09-04 12:02:05 +1200

    Update u.toObject.ts

--- code/sys.driver/ext.lib.automerge/src/common/u.toObject.ts
+++ code/sys.driver/ext.lib.automerge/src/common/u.toObject.ts
@@ -1,11 +1,9 @@
 import { R, Immutable } from './libs';
 
-type O = Record<string, unknown>;
-
 /**
  * Convert the given input to a simple object (deep).
  */
-export function toObject<T extends O>(input: T): T {
+export function toObject<T>(input: T): T {
   if (input === null) return {} as T;
   if (typeof input !== 'object') return {} as T;
   if (Immutable.Is.map(input) || Immutable.Is.proxy(input)) return Immutable.toObject<T>(input);
