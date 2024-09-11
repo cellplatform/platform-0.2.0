57d155476131f1245801d557cd933bf2a0fca879
Phil Cockfield <phil@cockfield.net>
2024-09-12 08:33:00 +1200

    refactor spec

--- code/sys.driver/ext.lib.automerge.webrtc/src/ui/ui.Sample.TextboxSync/-SPEC.tsx
+++ code/sys.driver/ext.lib.automerge.webrtc/src/ui/ui.Sample.TextboxSync/-SPEC.tsx
@@ -1,45 +1,60 @@
-import { Dev, Pkg, TestEdge, type t } from '../../test.ui';
+import { Dev, Immutable, Json, Pkg, rx, TestEdge, type t } from '../../test.ui';
 import { Layout } from './ui.Layout';
 
 type L = t.Lens;
-type T = { theme?: t.CommonTheme };
-const initial: T = {};
+type D = { theme?: t.CommonTheme };
 
 /**
  * Spec
  */
-const name = 'Sample.TextboxSync';
+const name = `${Pkg.name}:${'Sample.TextboxSync'}`;
+
 export default Dev.describe(name, async (e) => {
   const left = await TestEdge.createEdge('Left');
   const right = await TestEdge.createEdge('Right');
   const lenses: { left?: L | undefined; right?: L } = {};
 
-  type LocalStore = {};
-  const localstore = Dev.LocalStorage<LocalStore>(`dev:${Pkg.name}.${name}`);
-  const local = localstore.object({});
+  type LocalStore = { debug?: string };
+  const localstore = Dev.LocalStorage<LocalStore>(`dev:${name}`);
+  const local = localstore.object({ debug: undefined });
+
+  const State = {
+    debug: Immutable.clonerRef<D>(Json.parse<D>(local.debug, {})),
+  } as const;
 
   e.it('ui:init', async (e) => {
     const ctx = Dev.ctx(e);
-    const dev = Dev.tools<T>(e, initial);
-    const state = await ctx.state<T>(initial);
-    await state.change((d) => {});
 
     const toLens = (shared: t.NetworkStoreShared) => shared.ns.lens('foo', { text: '' });
     lenses.left = toLens(left.network.shared);
     lenses.right = toLens(right.network.shared);
 
+    const debug$ = State.debug.events().changed$;
+    rx.merge(debug$)
+      .pipe(rx.debounceTime(1000))
+      .subscribe(() => (local.debug = Json.stringify(State.debug.current)));
+    rx.merge(debug$)
+      .pipe(rx.debounceTime(100))
+      .subscribe(() => ctx.redraw());
+
     const theme: t.CommonTheme = 'Dark';
     Dev.Theme.background(ctx, theme);
     ctx.debug.width(330);
-    ctx.subject.display('grid').render<T>(async (e) => {
-      return <Layout left={lenses.left} right={lenses.right} path={['text']} theme={theme} />;
+    ctx.subject.display('grid').render<D>(() => {
+      return (
+        <Layout
+          //
+          left={lenses.left}
+          right={lenses.right}
+          path={['text']}
+          theme={theme}
+        />
+      );
     });
   });
 
   e.it('ui:debug', async (e) => {
-    const dev = Dev.tools<T>(e, initial);
-    const state = await dev.state();
-    const link = Dev.Link.pkg(Pkg, dev);
+    const dev = Dev.tools<D>(e);
 
     TestEdge.dev.headerFooterConnectors(dev, left.network, right.network);
     TestEdge.dev.peersSection(dev, left.network, right.network);
