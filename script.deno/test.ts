17c242345c4b402fff359b1473c0f1668ad77bca
Phil Cockfield <phil@cockfield.net>
2024-09-13 16:47:16 +1200

    Update test.ts

--- script.deno/test.ts
+++ script.deno/test.ts
@@ -1,4 +1,7 @@
 import { Cmd } from '@sys/std-server';
 
+/**
+ * Run all tests across the mono-repo.
+ */
 await Cmd.sh('cd code/deno/std', 'deno test -RW');
 await Cmd.sh('cd code/deno/std.server', 'deno test -RW');
