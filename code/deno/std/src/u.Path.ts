421c225e29ceb071c29bed6220e074012d8b9e34
Phil Cockfield <phil@cockfield.net>
2024-09-13 14:12:07 +1200

    update docs

--- code/deno/std/src/u.Path.ts
+++ code/deno/std/src/u.Path.ts
@@ -4,10 +4,21 @@ import { basename, dirname, fromFileUrl, join, resolve, toFileUrl } from '@std/p
  * Path helpers.
  */
 export const Path = {
+  /* Joins a sequence of paths, then normalizes the resulting path. */
   join,
+
+  /* Converts a file URL to a path string. */
   fromFileUrl,
+
+  /* Converts a path string to a file URL. */
   toFileUrl,
+
+  /* Resolves path segments into a path. */
   resolve,
+
+  /* Return the directory path of a path. */
   dirname,
+
+  /* Return the last portion of a path. */
   basename,
 } as const;
--- code/deno/std/src/u.Testing.ts
+++ code/deno/std/src/u.Testing.ts
@@ -9,10 +9,18 @@ import {
  * Assertion helpers.
  */
 export const Expect = {
+  /* Assert eqaulity. */
   eql: assertEquals,
+
+  /* Assert greater than. */
   greater: assertGreater,
+
+  /* Assert greater or equal than. */
   greaterOrEqual: assertGreaterOrEqual,
+
+  /* Helpers for asserting truth about arrays. */
   Array: {
+    /* Assert an item exists within the array. */
     includes: assertArrayIncludes,
   },
 } as const;
