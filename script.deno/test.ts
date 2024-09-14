c4f551b26debbbb38cb4a92c3c6507927d957c20
Phil Cockfield <phil@cockfield.net>
2024-09-14 22:02:38 +1200

    update ref paths

--- script.deno/test.ts
+++ script.deno/test.ts
@@ -1,7 +1,14 @@
 import { Cmd } from '@sys/std-server';
 
+const run = (path: string, args = '') => Cmd.sh(`cd ${path}`, `deno test ${args}`);
+
 /**
  * Run all tests across the mono-repo.
  */
-await Cmd.sh('cd code/deno/std', 'deno test -RW');
-await Cmd.sh('cd code/deno/std.server', 'deno test -RW');
+
+// Std Libs
+await run('code/deno/std.lib/std', '-RW');
+await run('code/deno/std.lib/std.server', '-RW');
+
+// Drivers.
+await run('code/deno/driver/driver.deno.cloud', '-RW');
