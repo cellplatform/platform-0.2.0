5b9ed922aa113729fe3b3fe57ffd34f4daf31a89
Phil Cockfield <phil@cockfield.net>
2024-09-17 12:49:35 +1200

    folder rename

--- code/deno/std.lib/std/src/mod.ts
+++ code/deno/std.lib/std/src/mod.ts
@@ -1,5 +1,5 @@
 export * from './DateTime/mod.ts';
 export * from './Fmt/mod.ts';
 export * from './Path/mod.ts';
-export * from './SemVer/mod.ts';
+export * from './Semver/mod.ts';
 export * from './Testing/mod.ts';
--- code/deno/std.lib/std/src/t.ts
+++ code/deno/std.lib/std/src/t.ts
@@ -2,5 +2,5 @@ export type * from './DateTime/t.ts';
 export type * from './Fmt/t.ts';
 export type * from './Http/t.ts';
 export type * from './Path/t.ts';
-export type * from './SemVer/t.ts';
+export type * from './Semver/t.ts';
 export type * from './Testing/t.ts';
