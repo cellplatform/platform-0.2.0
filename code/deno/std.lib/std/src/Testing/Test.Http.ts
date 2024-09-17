748fb571c00f869b3771ef42fd333b2910a850d9
Phil Cockfield <phil@cockfield.net>
2024-09-17 20:42:20 +1200

    Update Test.Http.ts

--- code/deno/std.lib/std/src/Testing/Test.Http.ts
+++ code/deno/std.lib/std/src/Testing/Test.Http.ts
@@ -10,10 +10,12 @@ export const TestHttp: t.TestHttp = {
    */
   server: TestServer.create,
 
+  /**
+   * Retrieve JSON response.
+   */
   json(...args: any[]) {
     const req = args.length === 2 ? args[0] : undefined;
     const body = args.length === 2 ? args[1] : args[0];
-
     const headers: HeadersInit = { 'Content-Type': 'application/json' };
     if (req?.url) headers['X-Request-URL'] = req?.url;
     return new Response(JSON.stringify(body), { headers });
