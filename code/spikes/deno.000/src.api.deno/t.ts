69ccced0e2c511da31b33b5661b77988b9c2e98c
Phil Cockfield <phil@cockfield.net>
2024-09-18 17:24:04 +1200

    path update

--- code/spikes/deno.000/src.api.deno/t.ts
+++ code/spikes/deno.000/src.api.deno/t.ts
@@ -1,3 +1,4 @@
-export type * from '../../../sys.driver/ext.lib.deno/src/t.deno.ts';
+export type * from '../../../deno/driver/driver.deno.cloud/src/t.deno.ts';
+
 export type * from '../src/t.ts';
 export type * from './r.subhosting/t.ts';
