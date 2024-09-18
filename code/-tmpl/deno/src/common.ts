6313ff49ecb598d90945c46e378eaac78ccd0bd3
Phil Cockfield <phil@cockfield.net>
2024-09-18 17:03:09 +1200

    Update common.ts

--- code/-tmpl/deno/src/common.ts
+++ code/-tmpl/deno/src/common.ts
@@ -1,2 +1,7 @@
+import { default as Pkg } from '../deno.json' with { type: 'json' };
+
 export { describe, expect, it } from '@sys/std/testing';
+
 export type * as t from './t.ts';
+export { Pkg };
+
