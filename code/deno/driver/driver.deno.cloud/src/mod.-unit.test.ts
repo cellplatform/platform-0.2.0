d2b02d88d182d6a0a9704fb2c35b57111046a198
Phil Cockfield <phil@cockfield.net>
2024-09-20 09:00:39 +1200

    refactor

---
+++ code/deno/driver/driver.deno.cloud/src/mod.-unit.test.ts
@@ -0,0 +1,66 @@
+import { describe, expect, it, Pkg } from './common/mod.ts';
+import { testSetup } from './u.Server/u.DenoCloud.-unit.test.ts';
+
+describe('DenoCloud (client)', () => {
+  describe('HTTP Server: routes', () => {
+    describe('/ (root)', () => {
+      it('/.info', async () => {
+        const { client, dispose } = testSetup();
+
+        const res = await client.info();
+        expect(res.ok).to.eql(true);
+        expect(res.error).to.eql(undefined);
+
+        if (res.ok) {
+          const data = res.data;
+          expect(data.module).to.eql({ name: Pkg.name, version: Pkg.version });
+        }
+
+        await dispose();
+      });
+    });
+
+    describe('/subhosting', () => {
+      it('/subhosting.info', async () => {
+        const { client, dispose } = testSetup();
+
+        const res = await client.subhosting.info();
+        expect(res.ok).to.eql(true);
+        expect(res.error).to.eql(undefined);
+
+        if (res.ok) {
+          const data = res.data;
+          expect(data.description).to.include('deno:subhosting™️');
+          expect(data.module).to.eql({ name: Pkg.name, version: Pkg.version });
+          expect(data.auth.identity).to.eql('');
+          expect(data.auth.verified).to.eql(false);
+        }
+
+        await dispose();
+      });
+
+      it('/subhosting.orgs', async () => {
+        const { client, dispose } = testSetup();
+
+        const res = await client.subhosting.projects();
+        expect(res.ok).to.eql(true);
+        expect(res.error).to.eql(undefined);
+
+        if (res.ok) {
+          const data = res.data;
+          expect(Array.isArray(data.projects)).to.be.true;
+          if (data.projects.length > 0) {
+            const project = data.projects[0];
+            expect(project.id).to.be.string;
+            expect(project.name).to.be.string;
+            expect(project.description).to.be.string;
+            expect(project.createdAt).to.be.string;
+            expect(project.updatedAt).to.be.string;
+          }
+        }
+
+        await dispose();
+      });
+    });
+  });
+});
