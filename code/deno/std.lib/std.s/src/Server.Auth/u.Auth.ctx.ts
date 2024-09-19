8908b0001a76b8c60b7eb3466368bad42497d2eb
Phil Cockfield <phil@cockfield.net>
2024-09-19 20:36:41 +1200

    refactor

---
+++ code/deno/std.lib/std.s/src/Server.Auth/u.Auth.ctx.ts
@@ -0,0 +1,36 @@
+import { PrivyClient, type t } from './common.ts';
+
+/**
+ * Factory: create a new Auth context.
+ */
+export const ctx: t.ServerAuth['ctx'] = (appId, appSecret) => {
+  const privy = new PrivyClient(appId, appSecret);
+  return {
+    /**
+     * Verify a user's access token (JWT)
+     * https://docs.privy.io/guide/server/authorization/verification#verifying-the-user-s-access-token
+     */
+    async verify(input): Promise<t.AuthVerification> {
+      const token = wrangle.accessToken(input);
+      if (!token) return { verified: false, user: '' };
+      try {
+        const claims = await privy.verifyAuthToken(token);
+        const user = claims.userId || '';
+        return { verified: true, user, claims };
+      } catch (err: unknown) {
+        const error = (err as Error).message;
+        return { verified: false, user: '', error };
+      }
+    },
+  };
+};
+
+/**
+ * Helpers
+ */
+const wrangle = {
+  accessToken(input: string | Request) {
+    const text = typeof input === 'string' ? input : input.headers.get('Authorization') ?? '';
+    return text.replace(/^Bearer /, '').trim();
+  },
+} as const;
--- code/deno/std.lib/std.s/src/Server.Auth/u.Auth.ts
+++ code/deno/std.lib/std.s/src/Server.Auth/u.Auth.ts
@@ -1,41 +1,9 @@
-import { PrivyClient, type t } from './common.ts';
+import { type t } from './common.ts';
+import { ctx } from './u.Auth.ctx.ts';
 
 /**
  * Helpers for auth (authentication and authorization).
  */
 export const Auth: t.ServerAuth = {
-  /**
-   * Factory: create an Auth context helper.
-   */
-  ctx(appId, appSecret) {
-    const privy = new PrivyClient(appId, appSecret);
-    return {
-      /**
-       * Verify a user's access token (JWT)
-       * https://docs.privy.io/guide/server/authorization/verification#verifying-the-user-s-access-token
-       */
-      async verify(input): Promise<t.AuthVerification> {
-        const token = wrangle.accessToken(input);
-        if (!token) return { verified: false, user: '' };
-        try {
-          const claims = await privy.verifyAuthToken(token);
-          const user = claims.userId || '';
-          return { verified: true, user, claims };
-        } catch (err: unknown) {
-          const error = (err as Error).message;
-          return { verified: false, user: '', error };
-        }
-      },
-    };
-  },
+  ctx,
 };
-
-/**
- * Helpers
- */
-const wrangle = {
-  accessToken(input: string | Request) {
-    const text = typeof input === 'string' ? input : input.headers.get('Authorization') ?? '';
-    return text.replace(/^Bearer /, '').trim();
-  },
-} as const;
