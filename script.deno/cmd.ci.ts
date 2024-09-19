bffc4715f5fcd9c98c04b83645fa3f6c1f833907
Phil Cockfield <phil@cockfield.net>
2024-09-19 18:47:43 +1200

    Update cmd.ci.ts

--- script.deno/cmd.ci.ts
+++ script.deno/cmd.ci.ts
@@ -2,6 +2,7 @@ import { Cmd } from './u.ts';
 
 const sh = Cmd.sh().run;
 
-await sh('deno task test');
+await sh('deno task info');
 await sh('deno task dry');
+await sh('deno task test');
 await sh('deno task info');
