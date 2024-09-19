c5ef5d6b332aa43fa84c6ef0fc7fd0c0155b555b
Phil Cockfield <phil@cockfield.net>
2024-09-20 09:29:32 +1200

    Update mod.-unit.test.ts

--- code/deno/driver/driver.deno.cloud/src/mod.-unit.test.ts
+++ code/deno/driver/driver.deno.cloud/src/mod.-unit.test.ts
@@ -1,66 +1,18 @@
-import { describe, expect, it, Pkg } from './common/mod.ts';
-import { testSetup } from './u.Server/u.DenoCloud.-unit.test.ts';
+import { describe, expect, it } from './common/mod.ts';
+import { env } from './env.ts';
 
 describe('DenoCloud (client)', () => {
-  describe('HTTP Server: routes', () => {
-    describe('/ (root)', () => {
-      it('/.info', async () => {
-        const { client, dispose } = testSetup();
+  it('.env contains values', () => {
+    const expectValue = <T>(obj: T, ns: string, key: keyof T) => {
+      const value = obj[key];
+      expect(value).to.be.string;
+      expect(String(value).length).to.be.greaterThan(10, `missing value for {${ns}.${String(key)}`);
+    };
 
-        const res = await client.info();
-        expect(res.ok).to.eql(true);
-        expect(res.error).to.eql(undefined);
+    expectValue(env.privy, 'privy', 'appId');
+    expectValue(env.privy, 'privy', 'appSecret');
 
-        if (res.ok) {
-          const data = res.data;
-          expect(data.module).to.eql({ name: Pkg.name, version: Pkg.version });
-        }
-
-        await dispose();
-      });
-    });
-
-    describe('/subhosting', () => {
-      it('/subhosting.info', async () => {
-        const { client, dispose } = testSetup();
-
-        const res = await client.subhosting.info();
-        expect(res.ok).to.eql(true);
-        expect(res.error).to.eql(undefined);
-
-        if (res.ok) {
-          const data = res.data;
-          expect(data.description).to.include('deno:subhosting™️');
-          expect(data.module).to.eql({ name: Pkg.name, version: Pkg.version });
-          expect(data.auth.identity).to.eql('');
-          expect(data.auth.verified).to.eql(false);
-        }
-
-        await dispose();
-      });
-
-      it('/subhosting.orgs', async () => {
-        const { client, dispose } = testSetup();
-
-        const res = await client.subhosting.projects();
-        expect(res.ok).to.eql(true);
-        expect(res.error).to.eql(undefined);
-
-        if (res.ok) {
-          const data = res.data;
-          expect(Array.isArray(data.projects)).to.be.true;
-          if (data.projects.length > 0) {
-            const project = data.projects[0];
-            expect(project.id).to.be.string;
-            expect(project.name).to.be.string;
-            expect(project.description).to.be.string;
-            expect(project.createdAt).to.be.string;
-            expect(project.updatedAt).to.be.string;
-          }
-        }
-
-        await dispose();
-      });
-    });
+    expectValue(env.deno, 'deno', 'accessToken');
+    expectValue(env.deno, 'deno', 'orgId');
   });
 });
