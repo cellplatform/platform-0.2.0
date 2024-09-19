3b0c89bcdcb559eb2b036d35cd43ef1afb8efb5a
Phil Cockfield <phil@cockfield.net>
2024-09-19 12:42:26 +1200

    Cmd: unit test (.sh)

--- code/deno/std.lib/std.s/src/Process/-Cmd.test.ts
+++ code/deno/std.lib/std.s/src/Process/-Cmd.test.ts
@@ -1,4 +1,13 @@
-import { describe, expect, Fs, it } from '../mod.ts';
+import { describe, expect, it } from '../mod.ts';
 import { Cmd } from './mod.ts';
 
-describe('Cmd', () => {});
+describe('Cmd', () => {
+  describe('Cmd.sh', () => {
+    it('run: "echo"', async () => {
+      const res = await Cmd.sh({ silent: true }).run('echo foo');
+      expect(res.code).to.eql(0);
+      expect(res.success).to.eql(true);
+      expect(res.text.stdout).to.eql('foo\n');
+    });
+  });
+});
