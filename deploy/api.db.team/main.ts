c5df9987eec100ee9c6dbe71e6ed34fa684d605b
Phil Cockfield <phil@cockfield.net>
2024-09-21 12:48:33 +1200

    refactor

--- deploy/api.db.team/main.ts
+++ deploy/api.db.team/main.ts
@@ -2,4 +2,5 @@ import { Server, DenoCloud, Pkg } from '@sys/driver-deno-cloud/server';
 
 const env = await DenoCloud.env();
 const app = DenoCloud.server({ env });
-Deno.serve(Server.options(8080, Pkg), app.fetch);
+const config = Server.options(8080, Pkg);
+Deno.serve(config, app.fetch);
