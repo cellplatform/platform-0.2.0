0a0577c0ef824794566c48e5b4d7f9cc6ce2aea9
Phil Cockfield <phil@cockfield.net>
2024-09-23 15:33:53 +1200

    Is.promise

--- code/deno/std.lib/std/src/u.Is/t.ts
+++ code/deno/std.lib/std/src/u.Is/t.ts
@@ -3,4 +3,5 @@ import type { t } from '../common.ts';
 export type CommonIsLib = {
   subject: t.RxIs['subject'];
   observable: t.RxIs['observable'];
+  promise<T = any>(value?: any): value is Promise<T>;
 };
--- code/deno/std.lib/std/src/u.Is/u.Is.ts
+++ code/deno/std.lib/std/src/u.Is/u.Is.ts
@@ -9,4 +9,13 @@ const { observable, subject } = RxIs;
 export const Is: t.CommonIsLib = {
   observable,
   subject,
+
+  /**
+   * Determines whether the given value is a Promise.
+   */
+  promise<T = any>(value?: any): value is Promise<T> {
+    return value !== null && value
+      ? typeof value === 'object' && typeof value.then === 'function'
+      : false;
+  },
 };
