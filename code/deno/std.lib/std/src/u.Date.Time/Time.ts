49cf7fd113ce31ba4ec40e4f2cb4dfa010def555
Phil Cockfield <phil@cockfield.net>
2024-09-23 15:27:18 +1200

    refactor

--- code/deno/std.lib/std/src/u.Date.Time/Time.ts
+++ code/deno/std.lib/std/src/u.Date.Time/Time.ts
@@ -1,59 +1,10 @@
-import { delay } from '@std/async';
 import type { t } from '../common.ts';
+import { delay } from './u.delay.ts';
 
 /**
  * Helpers for working with time/delays.
  */
 export const Time: t.TimeLib = {
-  delay(...args: any[]) {
-    type T = t.TimeDelayPromise;
-
-    const { msecs, fn } = wrangle.delayArgs(args);
-    const controller = new AbortController();
-    const { signal } = controller;
-
-    const cancel = () => controller.abort('delay cancelled');
-    const is: t.DeepMutable<T['is']> = { completed: false, cancelled: false, done: false };
-
-    const promise: any = new Promise<void>((resolve) => {
-      const done = () => {
-        is.done = true;
-        is.cancelled = signal.aborted;
-        resolve();
-      };
-
-      signal.onabort = done;
-      const complete = () => {
-        fn?.();
-        is.completed = true;
-        done();
-      };
-
-      delay(msecs, { signal }).then(complete).catch(done);
-    });
-
-    // Decorate the promise with extra time/delay controller fields.
-    promise.cancel = cancel;
-    promise.is = is;
-    promise.timeout = msecs;
-    return promise as T;
-  },
-
-  wait(msecs) {
-    return Time.delay(msecs);
-  },
+  delay,
+  wait: (msecs) => Time.delay(msecs),
 };
-
-/**
- * Helpers
- */
-const wrangle = {
-  delayArgs(input: any[]) {
-    let msecs = 0;
-    let fn: t.TimeDelayCallback | undefined;
-    if (typeof input[0] === 'number') msecs = input[0];
-    if (typeof input[0] === 'function') fn = input[0];
-    if (typeof input[1] === 'function') fn = input[1];
-    return { msecs, fn };
-  },
-} as const;
